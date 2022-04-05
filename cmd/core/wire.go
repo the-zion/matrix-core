66c2dbcdd5d820c738590ac82d2617386da1e3b0
liusiyuan <945212191@qq.com>
2022-04-05 16:56:24 +0800

    feat(cmd): 初始化cmd

---
+++ cmd/core/wire.go
@@ -0,0 +1,21 @@
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"core/internal/biz"
+	"core/internal/conf"
+	"core/internal/data"
+	"core/internal/server"
+	"core/internal/service"
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
