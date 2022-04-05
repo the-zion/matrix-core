e2310bc314243189133ff8d24c659a3e651a85c0
liusiyuan <945212191@qq.com>
2022-04-05 16:55:40 +0800

    feat(internal): 初始化internal

---
+++ internal/data/data.go
@@ -0,0 +1,23 @@
+package data
+
+import (
+	"core/internal/conf"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// ProviderSet is data providers.
+var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+
+// Data .
+type Data struct {
+	// TODO wrapped database client
+}
+
+// NewData .
+func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+	cleanup := func() {
+		log.NewHelper(logger).Info("closing the data resources")
+	}
+	return &Data{}, cleanup, nil
+}
