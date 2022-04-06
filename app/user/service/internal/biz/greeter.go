1cf99f927a11137092b2cd14e7f2debf1a061e0e
liusiyuan <945212191@qq.com>
2022-04-06 17:15:03 +0800

    refactor(biz): 修改import包名

--- app/user/service/internal/biz/greeter.go
+++ app/user/service/internal/biz/greeter.go
@@ -3,7 +3,7 @@ package biz
 import (
 	"context"
 
-	v1 "cube-core/api/helloworld/v1"
+	v1 "github.com/Cube-v2/cube-core/api/helloworld/v1"
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 )
