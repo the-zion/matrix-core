3e2a81eb585acefb7333a23860c90076b9979a94
liusiyuan <945212191@qq.com>
2022-04-06 17:19:37 +0800

    refactor(service): 新增DO对象User

--- app/user/service/internal/service/service.go
+++ app/user/service/internal/service/service.go
@@ -1,6 +1,27 @@
 package service
 
-import "github.com/google/wire"
+import (
+	"github.com/Cube-v2/cube-core/api/user/service/v1"
+	"github.com/Cube-v2/cube-core/app/user/service/internal/biz"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
 
-// ProviderSet is service providers.
 var ProviderSet = wire.NewSet(NewGreeterService)
+
+type User struct {
+	v1.UnimplementedUserServer
+
+	uc *biz.UserUseCase
+
+	log *log.Helper
+}
+
+func NewUser(
+	uc *biz.UserUseCase,
+	logger log.Logger) *User {
+	return &User{
+		log: log.NewHelper(log.With(logger, "module", "service/user")),
+		uc:  uc,
+	}
+}
