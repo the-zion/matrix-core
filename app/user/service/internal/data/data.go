0ec4d176d33f8c5d818bd73962044e2b500581a6
liusiyuan <945212191@qq.com>
2022-04-07 23:54:14 +0800

    refactor(data): 增加mysql启动逻辑，实现repo接口

--- app/user/service/internal/data/data.go
+++ app/user/service/internal/data/data.go
@@ -4,20 +4,40 @@ import (
 	"github.com/Cube-v2/cube-core/app/user/service/internal/conf"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewDB, NewUserRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	db *gorm.DB
 }
 
-// NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
+func NewDB(conf *conf.Data, logger log.Logger) *gorm.DB {
+	l := log.NewHelper(log.With(logger, "module", "user-service/data.mysql"))
+
+	db, err := gorm.Open(mysql.Open(conf.Database.Source), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		l.Fatalf("failed opening connection to db: %v", err)
 	}
-	return &Data{}, cleanup, nil
+	if err := db.AutoMigrate(&User{}); err != nil {
+		log.Fatalf("failed creat or update table resources: %v", err)
+	}
+	return db
+}
+
+func NewData(db *gorm.DB, logger log.Logger) (*Data, func(), error) {
+	l := log.NewHelper(log.With(logger, "module", "user-service/data"))
+
+	d := &Data{
+		db: db,
+	}
+	return d, func() {
+		l.Info("closing the data resources")
+	}, nil
 }
