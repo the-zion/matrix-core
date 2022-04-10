dd168a13e6a76befe43d2a7478c9439422a2c4e9
liusiyuan <945212191@qq.com>
2022-04-10 10:59:52 +0800

    chore(service): api接口实现

---
+++ app/user/service/internal/service/auth.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	"context"
+	v1 "github.com/Cube-v2/cube-core/api/user/service/v1"
+)
+
+func (s *UserService) Login(ctx context.Context, req *v1.LoginReq) (*v1.LoginReply, error) {
+	return s.ac.Login(ctx, req)
+}
--- app/user/service/internal/service/service.go
+++ app/user/service/internal/service/service.go
@@ -1,7 +1,7 @@
 package service
 
 import (
-	"github.com/Cube-v2/cube-core/api/user/service/v1"
+	v1 "github.com/Cube-v2/cube-core/api/user/service/v1"
 	"github.com/Cube-v2/cube-core/app/user/service/internal/biz"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
@@ -12,14 +12,14 @@ var ProviderSet = wire.NewSet(NewUserService)
 type UserService struct {
 	v1.UnimplementedUserServer
 	uc  *biz.UserUseCase
+	ac  *biz.AuthUseCase
 	log *log.Helper
 }
 
-func NewUserService(
-	uc *biz.UserUseCase,
-	logger log.Logger) *UserService {
+func NewUserService(uc *biz.UserUseCase, ac *biz.AuthUseCase, logger log.Logger) *UserService {
 	return &UserService{
 		log: log.NewHelper(log.With(logger, "module", "service/user")),
 		uc:  uc,
+		ac:  ac,
 	}
 }
