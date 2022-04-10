12c34014313e1277b6d7c237b82ae8f5c7ffb218
liusiyuan <945212191@qq.com>
2022-04-10 11:01:23 +0800

    refactor(biz): biz层更新注册与登录业务逻辑

--- app/user/service/internal/biz/auth.go
+++ app/user/service/internal/biz/auth.go
@@ -2,20 +2,49 @@ package biz
 
 import (
 	"context"
+	"errors"
 	v1 "github.com/Cube-v2/cube-core/api/user/service/v1"
 	"github.com/Cube-v2/cube-core/app/user/service/internal/conf"
+	"github.com/go-kratos/kratos/v2/log"
 	"github.com/golang-jwt/jwt/v4"
 )
 
-type AuthUseCase struct {
-	key      string
-	userRepo UserRepo
+var (
+	ErrUserNotFound       = errors.New("user not found")
+	ErrPasswordError      = errors.New("password error")
+	ErrCodeError          = errors.New("code error")
+	ErrUserRegisterFailed = errors.New("user register failed")
+)
+
+type Auth struct {
+	Id       int64
+	Username string
+	Password string
+	Phone    string
+	Email    string
+	Wechat   string
+	Github   string
 }
 
-func NewLoginUseCase(conf *conf.Auth, userRepo UserRepo) *AuthUseCase {
+type AuthRepo interface {
+	FindByUserPhone(ctx context.Context, phone string) (*Auth, error)
+	FindByUserEmail(ctx context.Context, email string) (*Auth, error)
+	VerifyPassword(ctx context.Context, id int64, password string) error
+	VerifyCode(ctx context.Context, key, code string) error
+	UserRegister(ctx context.Context, u *Auth, mode string) (*Auth, error)
+}
+
+type AuthUseCase struct {
+	key  string
+	repo AuthRepo
+	log  *log.Helper
+}
+
+func NewAuthUseCase(conf *conf.Auth, repo AuthRepo, logger log.Logger) *AuthUseCase {
 	return &AuthUseCase{
-		key:      conf.ApiKey,
-		userRepo: userRepo,
+		key:  conf.ApiKey,
+		repo: repo,
+		log:  log.NewHelper(log.With(logger, "module", "usecase/auth")),
 	}
 }
 
@@ -32,81 +61,88 @@ func (receiver *AuthUseCase) Login(ctx context.Context, req *v1.LoginReq) (*v1.L
 	if len(req.Email) > 0 && len(req.Code) > 0 {
 		return loginByEmailAndCode(ctx, receiver, req.Email, req.Code)
 	}
-	return &v1.LoginReply{}, v1.ErrorLoginFailed("login: params illegal")
+	return &v1.LoginReply{}, v1.ErrorLoginFailed("login failed: params illegal")
 }
 
 func loginByPhoneAndPassword(ctx context.Context, receiver *AuthUseCase, phone, password string) (*v1.LoginReply, error) {
-	user, err := receiver.userRepo.FindByUserPhone(ctx, phone)
+	user, err := receiver.repo.FindByUserPhone(ctx, phone)
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("account not found: %s", err.Error())
+		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
 	}
 
-	err = receiver.userRepo.VerifyPassword(ctx, user, password)
+	err = receiver.repo.VerifyPassword(ctx, user.Id, password)
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("password not match")
+		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
 	}
 
 	return signToken(user.Id, receiver)
 }
 
 func loginByPhoneAndCode(ctx context.Context, receiver *AuthUseCase, phone, code string) (*v1.LoginReply, error) {
-	user, err := receiver.userRepo.FindByUserPhone(ctx, phone)
+	err := receiver.repo.VerifyCode(ctx, phone, code)
 	if err != nil {
 		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
 	}
 
-	err = receiver.userRepo.VerifyCode(ctx, user, code)
+	user, err := receiver.repo.FindByUserPhone(ctx, phone)
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("code not match")
+		user, err = receiver.Register(ctx, phone, "phone")
+		if err != nil {
+			return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
+		}
 	}
 
 	return signToken(user.Id, receiver)
 }
 
 func loginByEmailAndPassword(ctx context.Context, receiver *AuthUseCase, email, password string) (*v1.LoginReply, error) {
-	user, err := receiver.userRepo.FindByUserEmail(ctx, email)
+	user, err := receiver.repo.FindByUserEmail(ctx, email)
 	if err != nil {
 		return nil, v1.ErrorLoginFailed("account not found: %s", err.Error())
 	}
 
-	err = receiver.userRepo.VerifyPassword(ctx, user, password)
+	err = receiver.repo.VerifyPassword(ctx, user.Id, password)
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("code not match")
+		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
 	}
 
 	return signToken(user.Id, receiver)
 }
 
 func loginByEmailAndCode(ctx context.Context, receiver *AuthUseCase, email, code string) (*v1.LoginReply, error) {
-	user, err := receiver.userRepo.FindByUserEmail(ctx, email)
+	err := receiver.repo.VerifyCode(ctx, email, code)
 	if err != nil {
 		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
 	}
 
-	err = receiver.userRepo.VerifyCode(ctx, user, code)
+	user, err := receiver.repo.FindByUserEmail(ctx, email)
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("code not match")
+		user, err = receiver.Register(ctx, email, "email")
+		if err != nil {
+			return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
+		}
 	}
 
 	return signToken(user.Id, receiver)
 }
 
-func (receiver *AuthUseCase) Register(ctx context.Context, account, mode string) error {
+func (receiver *AuthUseCase) Register(ctx context.Context, account, mode string) (*Auth, error) {
+	var user *Auth
 	var err error
 	switch mode {
 	case "phone":
-		err = receiver.userRepo.UserRegister(ctx, &User{Phone: account})
+		user, err = receiver.repo.UserRegister(ctx, &Auth{Phone: account}, "Phone")
 	case "email":
-		err = receiver.userRepo.UserRegister(ctx, &User{Email: account})
+		user, err = receiver.repo.UserRegister(ctx, &Auth{Email: account}, "Email")
 	case "wechat":
-		err = receiver.userRepo.UserRegister(ctx, &User{Wechat: account})
+		user, err = receiver.repo.UserRegister(ctx, &Auth{Wechat: account}, "Wechat")
 	case "github":
-		err = receiver.userRepo.UserRegister(ctx, &User{Github: account})
+		user, err = receiver.repo.UserRegister(ctx, &Auth{Github: account}, "Github")
 	}
 	if err != nil {
-		return v1.ErrorRegisterFailed("account created failed: %s", err.Error())
+		return nil, err
 	}
-	return nil
+	return user, nil
 }
 
 func signToken(id int64, receiver *AuthUseCase) (*v1.LoginReply, error) {
@@ -115,7 +151,7 @@ func signToken(id int64, receiver *AuthUseCase) (*v1.LoginReply, error) {
 	})
 	signedString, err := claims.SignedString([]byte(receiver.key))
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("generate token failed: %s", err.Error())
+		return nil, v1.ErrorLoginFailed("login failed: generate token failed")
 	}
 	return &v1.LoginReply{
 		Token: signedString,
--- app/user/service/internal/biz/user.go
+++ app/user/service/internal/biz/user.go
@@ -1,39 +1,23 @@
 package biz
 
 import (
-	"context"
 	"github.com/go-kratos/kratos/v2/log"
 )
 
 type User struct {
-	Id       int64
-	Username string
-	Password string
-	Phone    string
-	Email    string
-	Wechat   string
-	Github   string
 }
 
 type UserRepo interface {
-	FindByUserPhone(ctx context.Context, phone string) (*User, error)
-	FindByUserEmail(ctx context.Context, email string) (*User, error)
-	VerifyPassword(ctx context.Context, u *User, password string) error
-	VerifyCode(ctx context.Context, u *User, code string) error
-	UserRegister(ctx context.Context, u *User) error
 }
 
 type UserUseCase struct {
-	repo   UserRepo
-	log    *log.Helper
-	authUc *AuthUseCase
+	repo UserRepo
+	log  *log.Helper
 }
 
-func NewUserUseCase(repo UserRepo, logger log.Logger, authUc *AuthUseCase) *UserUseCase {
-	l := log.NewHelper(log.With(logger, "module", "usecase/user"))
+func NewUserUseCase(repo UserRepo, logger log.Logger) *UserUseCase {
 	return &UserUseCase{
-		repo:   repo,
-		log:    l,
-		authUc: authUc,
+		repo: repo,
+		log:  log.NewHelper(log.With(logger, "module", "usecase/user")),
 	}
 }
