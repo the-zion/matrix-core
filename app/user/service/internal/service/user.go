023149b194c4b26483466da14616ffeba5781225
liusiyuan <945212191@qq.com>
2022-04-12 23:54:47 +0800

    refactor(service): 新增api接口实现逻辑

--- app/user/service/internal/service/user.go
+++ app/user/service/internal/service/user.go
@@ -1,1 +1,10 @@
 package service
+
+import (
+	"context"
+	v1 "github.com/Cube-v2/cube-core/api/user/service/v1"
+)
+
+func (s *UserService) SendCode(ctx context.Context, req *v1.SendCodeReq) (*v1.SendCodeReply, error) {
+	return s.uc.SendCode(ctx, req)
+}
