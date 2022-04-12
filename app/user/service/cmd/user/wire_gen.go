f70e69bd624f0383f5501345ecbf9638b791c143
liusiyuan <945212191@qq.com>
2022-04-12 23:52:57 +0800

    refactor(cmd): wire文件重新生成

--- app/user/service/cmd/user/wire_gen.go
+++ app/user/service/cmd/user/wire_gen.go
@@ -21,14 +21,17 @@ import (
 // wireApp init kratos application.
 func wireApp(confServer *conf.Server, confData *conf.Data, auth *conf.Auth, logger log.Logger) (*kratos.App, func(), error) {
 	db := data.NewDB(confData, logger)
-	dataData, cleanup, err := data.NewData(db, logger)
+	cmdable := data.NewRedis(confData, logger)
+	txCode := data.NewPhoneCode(confData)
+	goMail := data.NewGoMail(confData)
+	dataData, cleanup, err := data.NewData(db, cmdable, txCode, goMail, logger)
 	if err != nil {
 		return nil, nil, err
 	}
 	userRepo := data.NewUserRepo(dataData, logger)
 	userUseCase := biz.NewUserUseCase(userRepo, logger)
 	authRepo := data.NewAuthRepo(dataData, logger)
-	authUseCase := biz.NewAuthUseCase(auth, authRepo, logger)
+	authUseCase := biz.NewAuthUseCase(auth, authRepo, userRepo, logger)
 	userService := service.NewUserService(userUseCase, authUseCase, logger)
 	httpServer := server.NewHTTPServer(confServer, userService, logger)
 	grpcServer := server.NewGRPCServer(confServer, userService, logger)
