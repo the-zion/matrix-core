46860b3a16a7e24770f632775e659b9741f4738a
liusiyuan <945212191@qq.com>
2022-04-14 23:59:50 +0800

    refactor(biz): biz业务逻辑调整

--- app/user/service/internal/biz/auth.go
+++ app/user/service/internal/biz/auth.go
@@ -6,97 +6,56 @@ import (
 	v1 "github.com/Cube-v2/cube-core/api/user/service/v1"
 	"github.com/Cube-v2/cube-core/app/user/service/internal/conf"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-playground/validator/v10"
 	"github.com/golang-jwt/jwt/v4"
+	"strings"
 )
 
 var (
 	ErrUserRegisterFailed = errors.New("user register failed")
 )
 
-type Auth struct {
-	Id       int64
-	Username string
-	Password string
-	Phone    string
-	Email    string
-	Wechat   string
-	Github   string
-}
-
 type AuthRepo interface {
-	UserRegister(ctx context.Context, u *Auth, mode string) (*Auth, error)
+	UserRegister(ctx context.Context, account, mode string) (*User, error)
 }
 
 type AuthUseCase struct {
 	key      string
 	repo     AuthRepo
 	userRepo UserRepo
+	validate *validator.Validate
 	log      *log.Helper
 }
 
-func NewAuthUseCase(conf *conf.Auth, repo AuthRepo, userRepo UserRepo, logger log.Logger) *AuthUseCase {
+func NewAuthUseCase(conf *conf.Auth, repo AuthRepo, userRepo UserRepo, validator *validator.Validate, logger log.Logger) *AuthUseCase {
 	return &AuthUseCase{
 		key:      conf.ApiKey,
 		repo:     repo,
 		userRepo: userRepo,
-		log:      log.NewHelper(log.With(logger, "module", "usecase/auth")),
+		validate: validator,
+		log:      log.NewHelper(log.With(logger, "module", "user/biz/authUseCase")),
 	}
 }
 
-func (receiver *AuthUseCase) Login(ctx context.Context, req *v1.LoginReq) (*v1.LoginReply, error) {
-	if len(req.Account) > 0 && len(req.Password) > 0 && len(req.Mode) > 0 {
-		return loginByAccountAndPassword(ctx, receiver, req.Account, req.Password, req.Mode)
+func (r *AuthUseCase) Login(ctx context.Context, account, password, code, mode string) (string, error) {
+	if !loginVerify(r.validate, r.log, account, password, code, mode) {
+		return "", v1.ErrorParamsIllegal("login failed: params illegal")
 	}
-	if len(req.Account) > 0 && len(req.Code) > 0 && len(req.Mode) > 0 {
-		return loginByAccountAndCode(ctx, receiver, req.Account, req.Code, req.Mode)
-	}
-	return &v1.LoginReply{}, v1.ErrorLoginFailed("login failed: params illegal")
+	//if len(account) > 0 && len(password) > 0 && len(mode) > 0 {
+	//	return loginByPassword(ctx, r, req.Account, req.Password, req.Mode)
+	//}
+	//if len(req.Account) > 0 && len(req.Code) > 0 && len(req.Mode) > 0 {
+	//	return loginByCode(ctx, r, req.Account, req.Code, req.Mode)
+	//}
+	return "", nil
 }
 
-func loginByAccountAndPassword(ctx context.Context, receiver *AuthUseCase, account, password, mode string) (*v1.LoginReply, error) {
-	user, err := receiver.userRepo.FindByUserAccount(ctx, account, mode)
-	if err != nil {
-		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
-	}
-
-	err = receiver.userRepo.VerifyPassword(ctx, user.Id, password)
-	if err != nil {
-		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
-	}
-
-	return signToken(user.Id, receiver)
-}
-
-func loginByAccountAndCode(ctx context.Context, receiver *AuthUseCase, account, code, mode string) (*v1.LoginReply, error) {
-	err := receiver.userRepo.VerifyCode(ctx, account, code, mode)
-	if err != nil {
-		return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
-	}
-
-	user, err := receiver.userRepo.FindByUserAccount(ctx, account, mode)
-	if err != nil {
-		user, err = receiver.Register(ctx, account, mode)
-		if err != nil {
-			return nil, v1.ErrorLoginFailed("login failed: %s", err.Error())
-		}
-	}
-
-	return signToken(user.Id, receiver)
-}
-
-func (receiver *AuthUseCase) Register(ctx context.Context, account, mode string) (*Auth, error) {
-	var user *Auth
+func (r *AuthUseCase) Register(ctx context.Context, account, mode string) (*User, error) {
+	var user *User
 	var err error
-	switch mode {
-	case "phone":
-		user, err = receiver.repo.UserRegister(ctx, &Auth{Phone: account}, "Phone")
-	case "email":
-		user, err = receiver.repo.UserRegister(ctx, &Auth{Email: account}, "Email")
-	case "wechat":
-		user, err = receiver.repo.UserRegister(ctx, &Auth{Wechat: account}, "Wechat")
-	case "github":
-		user, err = receiver.repo.UserRegister(ctx, &Auth{Github: account}, "Github")
-	default:
+	if mode == "phone" || mode == "email" || mode == "wechat" || mode == "github" {
+		user, err = r.repo.UserRegister(ctx, account, strings.ToUpper(mode[:1])+mode[1:])
+	} else {
 		err = ErrUserRegisterFailed
 	}
 	if err != nil {
@@ -105,13 +64,90 @@ func (receiver *AuthUseCase) Register(ctx context.Context, account, mode string)
 	return user, nil
 }
 
-func signToken(id int64, receiver *AuthUseCase) (*v1.LoginReply, error) {
+func (r *AuthUseCase) LoginPassWordForget(ctx context.Context, req *v1.LoginPassWordForgetReq) (*v1.LoginReply, error) {
+	if len(req.Account) > 0 && len(req.Code) > 0 && len(req.Password) > 0 && (req.Mode == "phone" || req.Mode == "email") {
+		return passWordForget(ctx, r, req.Account, req.Password, req.Code, req.Mode)
+	}
+	return nil, v1.ErrorParamsIllegal("login failed: params illegal")
+}
+
+func loginByPassword(ctx context.Context, r *AuthUseCase, account, password, mode string) (*v1.LoginReply, error) {
+	user, err := loginFindByAccount(ctx, r, account, mode)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.userRepo.VerifyPassword(ctx, user.Id, password)
+	if errors.Is(err, ErrUnknownError) {
+		return nil, v1.ErrorUnknownError("login failed: %s", err.Error())
+	}
+	if errors.Is(err, ErrPasswordError) {
+		return nil, v1.ErrorVerifyPasswordFailed("login failed: %s", err.Error())
+	}
+
+	return signToken(user.Id, r)
+}
+
+func loginByCode(ctx context.Context, r *AuthUseCase, account, code, mode string) (*v1.LoginReply, error) {
+	err := loginVerifyCode(ctx, r, account, code, mode)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := loginFindByAccount(ctx, r, account, mode)
+	if err != nil {
+		user, err = r.Register(ctx, account, mode)
+		if err != nil {
+			return nil, v1.ErrorUnknownError("login failed: %s", err.Error())
+		}
+	}
+
+	return signToken(user.Id, r)
+}
+
+func loginFindByAccount(ctx context.Context, r *AuthUseCase, account, mode string) (*User, error) {
+	user, err := r.userRepo.FindByAccount(ctx, account, mode)
+	if err != nil {
+		return nil, v1.ErrorGetUserFailed("login failed: %s", err.Error())
+	}
+	return user, nil
+}
+
+func loginVerifyCode(ctx context.Context, r *AuthUseCase, account, code, mode string) error {
+	err := r.userRepo.VerifyCode(ctx, account, code, mode)
+	if errors.Is(err, ErrUnknownError) {
+		return v1.ErrorUnknownError("login failed: %s", err.Error())
+	}
+	if errors.Is(err, ErrCodeError) {
+		return v1.ErrorVerifyCodeFailed("login failed: %s", err.Error())
+	}
+	return nil
+}
+
+func passWordForget(ctx context.Context, r *AuthUseCase, account, password, code, mode string) (*v1.LoginReply, error) {
+	err := loginVerifyCode(ctx, r, account, code, mode)
+	if err != nil {
+		return nil, err
+	}
+	user, err := loginFindByAccount(ctx, r, account, mode)
+	if err != nil {
+		return nil, err
+	}
+	err = r.userRepo.PasswordModify(ctx, user.Id, password)
+	if err != nil {
+		return nil, v1.ErrorUnknownError("login failed: %s", err.Error())
+	}
+	return signToken(user.Id, r)
+}
+
+func signToken(id int64, r *AuthUseCase) (*v1.LoginReply, error) {
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id": id,
 	})
-	signedString, err := claims.SignedString([]byte(receiver.key))
+	signedString, err := claims.SignedString([]byte(r.key))
 	if err != nil {
-		return nil, v1.ErrorLoginFailed("login failed: generate token failed")
+		r.log.Errorf("fail to sign token: id(%v) error(%v)", id, err.Error())
+		return nil, v1.ErrorUnknownError("login failed: generate token failed")
 	}
 	return &v1.LoginReply{
 		Token: signedString,
--- app/user/service/internal/biz/user.go
+++ app/user/service/internal/biz/user.go
@@ -12,13 +12,23 @@ var (
 	ErrPasswordError = errors.New("password error")
 	ErrSendCodeError = errors.New("send code error")
 	ErrCodeError     = errors.New("code error")
+	ErrUnknownError  = errors.New("unknown error")
 )
 
 type User struct {
+	Id       int64
+	Username string
+	Password string
+	Phone    string
+	Email    string
+	Wechat   string
+	Github   string
 }
 
 type UserRepo interface {
-	FindByUserAccount(ctx context.Context, account, mode string) (*Auth, error)
+	FindByAccount(ctx context.Context, account, mode string) (*User, error)
+	GetUser(ctx context.Context, username string) (*User, error)
+	PasswordModify(ctx context.Context, id int64, password string) error
 	VerifyPassword(ctx context.Context, id int64, password string) error
 	VerifyCode(ctx context.Context, account, code, mode string) error
 	SendCode(ctx context.Context, template int64, account, mode string) (string, error)
@@ -32,19 +42,39 @@ type UserUseCase struct {
 func NewUserUseCase(repo UserRepo, logger log.Logger) *UserUseCase {
 	return &UserUseCase{
 		repo: repo,
-		log:  log.NewHelper(log.With(logger, "module", "usecase/user")),
+		log:  log.NewHelper(log.With(logger, "module", "user/biz/userUseCase")),
 	}
 }
 
 func (receiver *UserUseCase) SendCode(ctx context.Context, req *v1.SendCodeReq) (*v1.SendCodeReply, error) {
 	if len(req.Account) > 0 && len(req.Mode) > 0 && (req.Mode == "phone" || req.Mode == "email") && (req.Template > 0 && req.Template < 4) {
 		code, err := receiver.repo.SendCode(ctx, req.Template, req.Account, req.Mode)
-		if err != nil {
+		if errors.Is(err, ErrUnknownError) {
+			return nil, v1.ErrorUnknownError("send code failed: %s", err.Error())
+		}
+		if errors.Is(err, ErrSendCodeError) {
 			return nil, v1.ErrorSendCodeFailed("send code failed: %s", err.Error())
 		}
+
 		return &v1.SendCodeReply{
 			Code: code,
 		}, nil
 	}
-	return nil, v1.ErrorSendCodeFailed("send code failed: params illegal")
+	return nil, v1.ErrorParamsIllegal("send code failed: params illegal")
+}
+
+func (receiver *UserUseCase) GetUser(ctx context.Context, req *v1.GetUserReq) (*v1.GetUserReply, error) {
+	if len(req.Username) > 0 {
+		user, err := receiver.repo.GetUser(ctx, req.Username)
+		if err != nil {
+			return nil, v1.ErrorGetUserFailed("get user failed: %s", err.Error())
+		}
+		return &v1.GetUserReply{
+			Phone:  user.Phone,
+			Email:  user.Email,
+			Wechat: user.Wechat,
+			Github: user.Github,
+		}, nil
+	}
+	return nil, v1.ErrorParamsIllegal("get user failed: params illegal")
 }
