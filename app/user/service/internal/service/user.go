28aefd0163ab424ca9732de35b75eff6d04712d2
liusiyuan <945212191@qq.com>
2022-04-14 23:58:01 +0800

    feat(service): 新增user获取接口实现

--- app/user/service/internal/service/user.go
+++ app/user/service/internal/service/user.go
@@ -8,3 +8,7 @@ import (
 func (s *UserService) SendCode(ctx context.Context, req *v1.SendCodeReq) (*v1.SendCodeReply, error) {
 	return s.uc.SendCode(ctx, req)
 }
+
+func (s *UserService) GetUser(ctx context.Context, req *v1.GetUserReq) (*v1.GetUserReply, error) {
+	return s.uc.GetUser(ctx, req)
+}
