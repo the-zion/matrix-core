d1d15b88fa80d8b34e627be8137d3741fdbb9b70
liusiyuan <945212191@qq.com>
2022-04-17 11:27:40 +0800

    refactor(data): 修改UserRegister data逻辑

--- app/user/service/internal/data/auth.go
+++ app/user/service/internal/data/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Cube-v2/cube-core/app/user/service/internal/biz"
 	"github.com/go-kratos/kratos/v2/log"
+	"gorm.io/gorm"
 )
 
 var _ biz.AuthRepo = (*authRepo)(nil)
@@ -22,19 +23,28 @@ func NewAuthRepo(data *Data, logger log.Logger) biz.AuthRepo {
 
 func (r *authRepo) UserRegister(ctx context.Context, account, mode string) (*biz.User, error) {
 	user := &User{}
-	user.Username = account
 	switch mode {
 	case "Phone":
+		account = account[3:]
 		user.Phone = account
 	case "Email":
 		user.Email = account
 	}
-	result := r.data.db.WithContext(ctx).Select(mode, "Username").Create(user)
-	if result.Error != nil {
-		r.log.Errorf("fail to register a account: account(%v) error(%v)", account, result.Error.Error())
+	if err := r.data.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		if err := tx.Select(mode).Create(user).Error; err != nil {
+			r.log.Errorf("fail to register a account: account(%v) error(%v)", account, err.Error())
+			return err
+		}
+
+		if err := tx.Create(&Profile{UserId: int64(user.ID), Username: account}).Error; err != nil {
+			r.log.Errorf("fail to register a profile: user_id(%v) error(%v)", user.ID, err.Error())
+			return err
+		}
+		return nil
+	}); err != nil {
 		return nil, biz.ErrUserRegisterFailed
 	}
 	return &biz.User{
-		Id: int64(user.Model.ID),
+		Id: int64(user.ID),
 	}, nil
 }
