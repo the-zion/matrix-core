c713fa7891053baee56e5670d5a67c22061bcb93
liusiyuan <945212191@qq.com>
2022-04-18 23:16:24 +0800

    refactor(server): server
    http：添加白名单

--- app/user/service/internal/server/http.go
+++ app/user/service/internal/server/http.go
@@ -1,16 +1,28 @@
 package server
 
 import (
+	"context"
 	v1 "github.com/Cube-v2/cube-core/api/user/service/v1"
 	"github.com/Cube-v2/cube-core/app/user/service/internal/conf"
 	"github.com/Cube-v2/cube-core/app/user/service/internal/service"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	jwt2 "github.com/golang-jwt/jwt/v4"
+	"strings"
 )
+
+func NewWhiteListMatcher() selector.MatchFunc {
+	return func(ctx context.Context, operation string) bool {
+		if strings.Contains(operation, "Set") {
+			return true
+		}
+		return false
+	}
+}
 
 // NewHTTPServer new a HTTP user.
 func NewHTTPServer(c *conf.Server, ac *conf.Auth, userService *service.UserService, logger log.Logger) *http.Server {
@@ -18,11 +30,13 @@ func NewHTTPServer(c *conf.Server, ac *conf.Auth, userService *service.UserServi
 		http.Middleware(
 			recovery.Recovery(),
 			validate.Validator(),
-			jwt.Server(func(token *jwt2.Token) (interface{}, error) {
-				return []byte(ac.ApiKey), nil
-			}, jwt.WithSigningMethod(jwt2.SigningMethodHS256), jwt.WithClaims(func() jwt2.Claims {
-				return &jwt2.MapClaims{}
-			})),
+			selector.Server(
+				jwt.Server(func(token *jwt2.Token) (interface{}, error) {
+					return []byte(ac.ApiKey), nil
+				}, jwt.WithSigningMethod(jwt2.SigningMethodHS256), jwt.WithClaims(func() jwt2.Claims {
+					return &jwt2.MapClaims{}
+				})),
+			).Match(NewWhiteListMatcher()).Build(),
 		),
 	}
 	if c.Http.Network != "" {
