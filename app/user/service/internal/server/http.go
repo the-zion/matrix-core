f6adec266739b85d92f1bed44cd1891b845c2cb3
liusiyuan <945212191@qq.com>
2022-06-06 17:13:17 +0800

    refactor: http不再对接外网，仅用作health check通信

--- app/user/service/internal/server/http.go
+++ app/user/service/internal/server/http.go
@@ -1,44 +1,21 @@
 package server
 
 import (
-	"context"
 	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/middleware/selector"
-	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	jwt2 "github.com/golang-jwt/jwt/v4"
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
 	"github.com/the-zion/matrix-core/app/user/service/internal/conf"
 	"github.com/the-zion/matrix-core/app/user/service/internal/service"
-	"strings"
 )
-
-func NewWhiteListMatcher() selector.MatchFunc {
-	return func(ctx context.Context, operation string) bool {
-		if strings.Contains(operation, "Set") {
-			return true
-		}
-		return false
-	}
-}
 
 // NewHTTPServer new a HTTP user.
 func NewHTTPServer(c *conf.Server, ac *conf.Auth, userService *service.UserService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
-			validate.Validator(),
 			logging.Server(logger),
-			selector.Server(
-				jwt.Server(func(token *jwt2.Token) (interface{}, error) {
-					return []byte(ac.ApiKey), nil
-				}, jwt.WithSigningMethod(jwt2.SigningMethodHS256), jwt.WithClaims(func() jwt2.Claims {
-					return &jwt2.MapClaims{}
-				})),
-			).Match(NewWhiteListMatcher()).Build(),
 		),
 	}
 	if c.Http.Network != "" {
