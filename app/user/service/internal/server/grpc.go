57ef4ec7c60eedaf9b2a3d02a2ad6d6c8673e1bc
liusiyuan <945212191@qq.com>
2022-06-06 17:14:39 +0800

    feat: jwt校验

--- app/user/service/internal/server/grpc.go
+++ app/user/service/internal/server/grpc.go
@@ -1,23 +1,44 @@
 package server
 
 import (
+	"context"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
+	jwt2 "github.com/golang-jwt/jwt/v4"
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
 	"github.com/the-zion/matrix-core/app/user/service/internal/conf"
 	"github.com/the-zion/matrix-core/app/user/service/internal/service"
+	"strings"
 )
 
+func NewWhiteListMatcher() selector.MatchFunc {
+	return func(ctx context.Context, operation string) bool {
+		if strings.Contains(operation, "Set") {
+			return true
+		}
+		return false
+	}
+}
+
 // NewGRPCServer new a gRPC user.
-func NewGRPCServer(c *conf.Server, userService *service.UserService, logger log.Logger) *grpc.Server {
+func NewGRPCServer(c *conf.Server, ac *conf.Auth, userService *service.UserService, logger log.Logger) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
 			validate.Validator(),
 			logging.Server(logger),
+			selector.Server(
+				jwt.Server(func(token *jwt2.Token) (interface{}, error) {
+					return []byte(ac.ApiKey), nil
+				}, jwt.WithSigningMethod(jwt2.SigningMethodHS256), jwt.WithClaims(func() jwt2.Claims {
+					return &jwt2.MapClaims{}
+				})),
+			).Match(NewWhiteListMatcher()).Build(),
 		),
 	}
 	if c.Grpc.Network != "" {
