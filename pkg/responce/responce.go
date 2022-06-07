af086bfc81e74b6906cee720386e90029858f47e
liusiyuan <945212191@qq.com>
2022-06-07 16:06:38 +0800

    feat: add response 敏感信息过滤

---
+++ pkg/responce/responce.go
@@ -0,0 +1,21 @@
+package responce
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/middleware"
+)
+
+func Server() middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			reply, err = handler(ctx, req)
+			if err != nil {
+				e := err.(*errors.Error)
+				e.Message = e.Reason
+				return reply, e
+			}
+			return
+		}
+	}
+}
