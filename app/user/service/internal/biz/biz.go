d9354d465886d3c2942fc47e194ddb2fb1d83d54
liusiyuan <945212191@qq.com>
2022-06-08 23:15:51 +0800

    feat: add Transaction

--- app/user/service/internal/biz/biz.go
+++ app/user/service/internal/biz/biz.go
@@ -1,6 +1,13 @@
 package biz
 
-import "github.com/google/wire"
+import (
+	"context"
+	"github.com/google/wire"
+)
 
 // ProviderSet is biz providers.
 var ProviderSet = wire.NewSet(NewUserUseCase, NewAuthUseCase, NewProfileUseCase)
+
+type Transaction interface {
+	ExecTx(context.Context, func(ctx context.Context) error) error
+}
