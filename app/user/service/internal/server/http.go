44ca88c69bfecd4b7d4234686896070cf451cbf2
liusiyuan <945212191@qq.com>
2022-06-12 23:54:57 +0800

    refactor: add middleware responce & validate

--- app/user/service/internal/server/http.go
+++ app/user/service/internal/server/http.go
@@ -4,10 +4,12 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
 	"github.com/the-zion/matrix-core/app/user/service/internal/conf"
 	"github.com/the-zion/matrix-core/app/user/service/internal/service"
+	"github.com/the-zion/matrix-core/pkg/responce"
 )
 
 // NewHTTPServer new a HTTP user.
@@ -15,7 +17,9 @@ func NewHTTPServer(c *conf.Server, ac *conf.Auth, userService *service.UserServi
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			responce.Server(),
 			logging.Server(logger),
+			validate.Validator(),
 		),
 	}
 	if c.Http.Network != "" {
