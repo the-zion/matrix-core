af7dc6a3f852a76466eaa88ad7f00b4534165569
liusiyuan <945212191@qq.com>
2022-06-12 23:57:42 +0800

    refactor: add server mqConsumer

--- app/user/service/cmd/user/main.go
+++ app/user/service/cmd/user/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/the-zion/matrix-core/app/user/service/internal/conf"
+	"github.com/the-zion/matrix-core/app/user/service/internal/server"
 	"gopkg.in/yaml.v3"
 	"os"
 )
@@ -33,7 +34,7 @@ func init() {
 	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
 }
 
-func newApp(logger log.Logger, r *nacos.Registry, hs *http.Server, gs *grpc.Server) *kratos.App {
+func newApp(logger log.Logger, r *nacos.Registry, hs *http.Server, gs *grpc.Server, mc *server.MqConsumerServer) *kratos.App {
 	return kratos.New(
 		kratos.ID(id),
 		kratos.Name(Name),
@@ -44,6 +45,7 @@ func newApp(logger log.Logger, r *nacos.Registry, hs *http.Server, gs *grpc.Serv
 		kratos.Server(
 			hs,
 			gs,
+			mc,
 		),
 	)
 }
