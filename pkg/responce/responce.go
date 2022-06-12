6cb7c66ea4e0fb012a6223d255134910d28cf01b
liusiyuan <945212191@qq.com>
2022-06-12 23:51:35 +0800

    refactor: update responce

--- pkg/responce/responce.go
+++ pkg/responce/responce.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
 )
 
 func Server() middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			if header, ok := transport.FromServerContext(ctx); ok {
+				ctx = context.WithValue(ctx, "uuid", header.RequestHeader().Get("uuid"))
+			}
 			reply, err = handler(ctx, req)
 			if err != nil {
 				e := err.(*errors.Error)
-				e.Message = e.Reason
+				e.Message = ""
 				return reply, e
 			}
 			return
