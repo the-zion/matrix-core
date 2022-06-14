f712b6102e0688c22c42137ae2454bb681a84dd7
liusiyuan <945212191@qq.com>
2022-06-14 23:58:20 +0800

    feat: add cmd

---
+++ app/info/interface/cmd/info/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/go-kratos/kratos/contrib/registry/nacos/v2"
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"github.com/the-zion/matrix-core/app/info/interface/internal/biz"
+	"github.com/the-zion/matrix-core/app/info/interface/internal/conf"
+	"github.com/the-zion/matrix-core/app/info/interface/internal/data"
+	"github.com/the-zion/matrix-core/app/info/interface/internal/server"
+	"github.com/the-zion/matrix-core/app/info/interface/internal/service"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger, *nacos.Registry) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
