7216b5761d40a63c6faea732a98a12f6fdccc2ca
liusiyuan <945212191@qq.com>
2022-06-15 23:51:30 +0800

    feat: add model

---
+++ app/user/service/internal/data/model.go
@@ -0,0 +1,24 @@
+package data
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Uuid     string `gorm:"uniqueIndex;size:200"`
+	Email    string `gorm:"uniqueIndex;size:200"`
+	Phone    string `gorm:"uniqueIndex;size:200"`
+	Wechat   string `gorm:"uniqueIndex;size:500"`
+	Github   string `gorm:"uniqueIndex;size:500"`
+	Password string `gorm:"size:500"`
+}
+
+type Profile struct {
+	gorm.Model
+	Uuid      string `gorm:"uniqueIndex;size:200"`
+	Username  string `gorm:"uniqueIndex;size:100"`
+	Avatar    string `gorm:"size:200"`
+	School    string `gorm:"size:100"`
+	Company   string `gorm:"size:100"`
+	Homepage  string `gorm:"size:200"`
+	Introduce string `gorm:"size:500"`
+}
