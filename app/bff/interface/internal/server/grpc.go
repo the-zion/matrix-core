aed3da2355dea5dd689de8d2fd3dc6895328bca9
liusiyuan <945212191@qq.com>
2022-06-15 23:52:28 +0800

    refactor: update

--- app/bff/interface/internal/server/grpc.go
+++ app/bff/interface/internal/server/grpc.go
@@ -1,30 +1,18 @@
 package server
 
 import (
-	"context"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
-	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
-	"github.com/the-zion/matrix-core/app/info/interface/internal/conf"
-	"github.com/the-zion/matrix-core/app/info/interface/internal/service"
+	v1 "github.com/the-zion/matrix-core/api/bff/interface/v1"
+	"github.com/the-zion/matrix-core/app/bff/interface/internal/conf"
+	"github.com/the-zion/matrix-core/app/bff/interface/internal/service"
 	"github.com/the-zion/matrix-core/pkg/responce"
-	"strings"
 )
 
-func NewWhiteListMatcher() selector.MatchFunc {
-	return func(ctx context.Context, operation string) bool {
-		if strings.Contains(operation, "Set") {
-			return true
-		}
-		return false
-	}
-}
-
-func NewGRPCServer(c *conf.Server, infoService *service.InfoService, logger log.Logger) *grpc.Server {
+func NewGRPCServer(c *conf.Server, bffService *service.BffService, logger log.Logger) *grpc.Server {
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
@@ -43,6 +31,6 @@ func NewGRPCServer(c *conf.Server, infoService *service.InfoService, logger log.
 		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
 	}
 	srv := grpc.NewServer(opts...)
-	v1.RegisterUserServer(srv, infoService)
+	v1.RegisterBffServer(srv, bffService)
 	return srv
 }
--- app/bff/interface/internal/server/http.go
+++ app/bff/interface/internal/server/http.go
@@ -6,18 +6,18 @@ import (
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
-	"github.com/the-zion/matrix-core/app/info/interface/internal/conf"
-	"github.com/the-zion/matrix-core/app/info/interface/internal/service"
-	"github.com/the-zion/matrix-core/pkg/responce"
+	"github.com/the-zion/matrix-core/api/bff/interface/v1"
+	"github.com/the-zion/matrix-core/app/bff/interface/internal/conf"
+	"github.com/the-zion/matrix-core/app/bff/interface/internal/service"
+	"github.com/the-zion/matrix-core/pkg/request"
 )
 
 // NewHTTPServer new a HTTP user.
-func NewHTTPServer(c *conf.Server, infoService *service.InfoService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, bffService *service.BffService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
-			responce.Server(),
+			request.Server(),
 			logging.Server(logger),
 			validate.Validator(),
 		),
@@ -32,6 +32,6 @@ func NewHTTPServer(c *conf.Server, infoService *service.InfoService, logger log.
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
-	v1.RegisterUserHTTPServer(srv, infoService)
+	v1.RegisterBffHTTPServer(srv, bffService)
 	return srv
 }
