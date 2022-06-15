7c9e60857c996246ea97a0cc5c8d2f2130fa88a8
liusiyuan <945212191@qq.com>
2022-06-15 23:44:19 +0800

    refactor: update
    delete GetUserProfile

--- app/user/service/internal/service/profile.go
+++ app/user/service/internal/service/profile.go
@@ -5,24 +5,6 @@ import (
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
 )
 
-func (s *UserService) GetUserProfile(ctx context.Context, req *v1.GetUserProfileReq) (*v1.GetUserProfileReply, error) {
-	profile, err := s.pc.GetUserProfile(ctx, req.Id)
-	if err != nil {
-		return nil, err
-	}
-	return &v1.GetUserProfileReply{
-		Username:   profile.Username,
-		Sex:        profile.Sex,
-		Introduce:  profile.Introduce,
-		Address:    profile.Address,
-		Industry:   profile.Industry,
-		Profile:    profile.PersonalProfile,
-		Tag:        profile.Tag,
-		Background: profile.Background,
-		Image:      profile.Image,
-	}, nil
-}
-
 func (s *UserService) SetUserProfile(ctx context.Context, req *v1.SetUserProfileReq) (*v1.SetUserProfileReply, error) {
 	id := GetUserIdFromContext(ctx)
 	err := s.pc.SetUserProfile(ctx, id, req.Sex, req.Introduce, req.Industry, req.Address, req.Profile, req.Tag)
