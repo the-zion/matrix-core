ae157bbf9dbb04c1e86eb9df416fc0dc0d94277d
liusiyuan <945212191@qq.com>
2022-06-15 23:51:52 +0800

    feat: add request

---
+++ pkg/request/request.go
@@ -0,0 +1,19 @@
+package request
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+)
+
+func Server() middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			if header, ok := transport.FromServerContext(ctx); ok {
+				ctx = context.WithValue(ctx, "uuid", header.RequestHeader().Get("uuid"))
+			}
+			reply, err = handler(ctx, req)
+			return
+		}
+	}
+}
