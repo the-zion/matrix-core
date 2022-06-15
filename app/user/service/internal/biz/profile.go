ff06ad239a2f0944d025dbe69cfaa12d0237959a
liusiyuan <945212191@qq.com>
2022-06-15 23:47:52 +0800

    refactor: update
    delete Profile struct
    delete GetUserProfile

--- app/user/service/internal/biz/profile.go
+++ app/user/service/internal/biz/profile.go
@@ -6,20 +6,7 @@ import (
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
 )
 
-type Profile struct {
-	Username        string
-	Sex             string
-	Introduce       string
-	Industry        string
-	Address         string
-	PersonalProfile string
-	Tag             string
-	Background      string
-	Image           string
-}
-
 type ProfileRepo interface {
-	GetProfile(ctx context.Context, id int64) (*Profile, error)
 	SetProfile(ctx context.Context, id int64, sex, introduce, industry, address, profile, tag string) error
 	SetName(ctx context.Context, id int64, name string) error
 }
@@ -34,24 +21,6 @@ func NewProfileUseCase(repo ProfileRepo, logger log.Logger) *ProfileUseCase {
 		repo: repo,
 		log:  log.NewHelper(log.With(logger, "module", "user/biz/profileUseCase")),
 	}
-}
-
-func (r *ProfileUseCase) GetUserProfile(ctx context.Context, id int64) (*Profile, error) {
-	profile, err := r.repo.GetProfile(ctx, id)
-	if err != nil {
-		return nil, v1.ErrorGetProfileFailed("get user profile failed: %s", err.Error())
-	}
-	return &Profile{
-		Username:        profile.Username,
-		Sex:             profile.Sex,
-		Introduce:       profile.Introduce,
-		Industry:        profile.Industry,
-		Address:         profile.Address,
-		PersonalProfile: profile.PersonalProfile,
-		Tag:             profile.Tag,
-		Background:      profile.Background,
-		Image:           profile.Image,
-	}, nil
 }
 
 func (r *ProfileUseCase) SetUserProfile(ctx context.Context, id int64, sex, introduce, industry, address, profile, tag string) error {
