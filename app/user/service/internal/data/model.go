59948d988f3f9d8d18a65246acccca9fe018a569
liusiyuan <945212191@qq.com>
2022-06-17 21:43:26 +0800

    feat: update
    add struct ProfileUpdate
    add Job to struct Profile

--- app/user/service/internal/data/model.go
+++ app/user/service/internal/data/model.go
@@ -19,6 +19,12 @@ type Profile struct {
 	Avatar    string `gorm:"size:200"`
 	School    string `gorm:"size:100"`
 	Company   string `gorm:"size:100"`
+	Job       string `gorm:"size:100"`
 	Homepage  string `gorm:"size:200"`
 	Introduce string `gorm:"size:500"`
+}
+
+type ProfileUpdate struct {
+	Profile
+	Status int64 `gorm:"default:1"`
 }
