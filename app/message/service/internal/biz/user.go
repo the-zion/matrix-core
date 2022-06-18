8671d3a75b985ec014b9e1c42bb982c5344c6674
liusiyuan <945212191@qq.com>
2022-06-18 18:11:07 +0800

    feat: add biz

---
+++ app/message/service/internal/biz/user.go
@@ -0,0 +1,20 @@
+package biz
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type UserRepo interface {
+}
+
+type UserUseCase struct {
+	repo UserRepo
+	log  *log.Helper
+}
+
+func NewUserUseCase(repo UserRepo, logger log.Logger) *UserUseCase {
+	return &UserUseCase{
+		repo: repo,
+		log:  log.NewHelper(log.With(logger, "module", "message/biz/userUseCase")),
+	}
+}
