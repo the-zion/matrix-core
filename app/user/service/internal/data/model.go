2dd0ccb8955baa610ae663a534423197c5f4920f
liusiyuan <945212191@qq.com>
2022-06-18 18:13:58 +0800

    refactor: update
    update struct size

--- app/user/service/internal/data/model.go
+++ app/user/service/internal/data/model.go
@@ -1,30 +1,34 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
 	gorm.Model
-	Uuid     string `gorm:"uniqueIndex;size:200"`
-	Email    string `gorm:"uniqueIndex;size:200"`
-	Phone    string `gorm:"uniqueIndex;size:200"`
-	Wechat   string `gorm:"uniqueIndex;size:500"`
-	Github   string `gorm:"uniqueIndex;size:500"`
+	Uuid     string `gorm:"uniqueIndex;size:36"`
+	Email    string `gorm:"uniqueIndex;size:50"`
+	Phone    string `gorm:"uniqueIndex;size:20"`
+	Wechat   string `gorm:"uniqueIndex;size:100"`
+	Github   string `gorm:"uniqueIndex;size:100"`
 	Password string `gorm:"size:500"`
 }
 
 type Profile struct {
-	gorm.Model
-	Uuid      string `gorm:"uniqueIndex;size:200"`
-	Username  string `gorm:"uniqueIndex;size:100"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Uuid      string `gorm:"primaryKey;size:36"`
+	Username  string `gorm:"uniqueIndex;not null;size:20"`
 	Avatar    string `gorm:"size:200"`
-	School    string `gorm:"size:100"`
-	Company   string `gorm:"size:100"`
-	Job       string `gorm:"size:100"`
-	Homepage  string `gorm:"size:200"`
-	Introduce string `gorm:"size:500"`
+	School    string `gorm:"size:50"`
+	Company   string `gorm:"size:50"`
+	Job       string `gorm:"size:50"`
+	Homepage  string `gorm:"size:100"`
+	Introduce string `gorm:"size:100"`
 }
 
 type ProfileUpdate struct {
 	Profile
-	Status int64 `gorm:"default:1"`
+	Status int32 `gorm:"default:1"`
 }
