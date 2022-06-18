50a1cfedfe9599a560f2f292ee4455b207294452
liusiyuan <945212191@qq.com>
2022-06-18 18:11:42 +0800

    feat: add server

---
+++ app/message/service/internal/server/grpc.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	v1 "github.com/the-zion/matrix-core/api/message/service/v1"
+	"github.com/the-zion/matrix-core/app/message/service/internal/conf"
+	"github.com/the-zion/matrix-core/app/message/service/internal/service"
+	"github.com/the-zion/matrix-core/pkg/responce"
+)
+
+func NewGRPCServer(c *conf.Server, messageService *service.MessageService, logger log.Logger) *grpc.Server {
+	var opts = []grpc.ServerOption{
+		grpc.Middleware(
+			recovery.Recovery(),
+			responce.Server(),
+			logging.Server(logger),
+			validate.Validator(),
+		),
+	}
+	if c.Grpc.Network != "" {
+		opts = append(opts, grpc.Network(c.Grpc.Network))
+	}
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
+	}
+	if c.Grpc.Timeout != nil {
+		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
+	}
+	srv := grpc.NewServer(opts...)
+	v1.RegisterMessageServer(srv, messageService)
+	return srv
+}
