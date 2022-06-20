c9e9df8150ce6dc8f50be027d7b2bfc66e2be009
liusiyuan <945212191@qq.com>
2022-06-20 17:45:39 +0800

    refactor: update
    add: usecase MessageUseCase
    add: func NewMessageServiceClient

--- app/bff/interface/internal/data/data.go
+++ app/bff/interface/internal/data/data.go
@@ -7,21 +7,24 @@ import (
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/google/wire"
+	messagev1 "github.com/the-zion/matrix-core/api/message/service/v1"
 	userv1 "github.com/the-zion/matrix-core/api/user/service/v1"
 )
 
-var ProviderSet = wire.NewSet(NewData, NewUserRepo, NewUserServiceClient)
+var ProviderSet = wire.NewSet(NewData, NewUserRepo, NewMessageRepo, NewUserServiceClient, NewMessageServiceClient)
 
 type Data struct {
 	log *log.Helper
 	uc  userv1.UserClient
+	mc  messagev1.MessageClient
 }
 
-func NewData(logger log.Logger, uc userv1.UserClient) (*Data, error) {
+func NewData(logger log.Logger, uc userv1.UserClient, mc messagev1.MessageClient) (*Data, error) {
 	l := log.NewHelper(log.With(logger, "module", "bff/data"))
 	d := &Data{
 		log: l,
 		uc:  uc,
+		mc:  mc,
 	}
 	return d, nil
 }
@@ -41,5 +44,23 @@ func NewUserServiceClient(r *nacos.Registry, logger log.Logger) userv1.UserClien
 		l.Fatalf(err.Error())
 	}
 	c := userv1.NewUserClient(conn)
+	return c
+}
+
+func NewMessageServiceClient(r *nacos.Registry, logger log.Logger) messagev1.MessageClient {
+	l := log.NewHelper(log.With(logger, "module", "bff/data/new-message-client"))
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("discovery:///matrix.message.service.grpc"),
+		grpc.WithDiscovery(r),
+		grpc.WithMiddleware(
+			//tracing.Client(tracing.WithTracerProvider(tp)),
+			recovery.Recovery(),
+		),
+	)
+	if err != nil {
+		l.Fatalf(err.Error())
+	}
+	c := messagev1.NewMessageClient(conn)
 	return c
 }
