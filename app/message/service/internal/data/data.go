0afbd298ffc04b78c5e9a35c28808b41baba8cfe
liusiyuan <945212191@qq.com>
2022-06-20 17:42:19 +0800

    refactor: update
    add: struct TxCode GoMail
    add: func NewPhoneCode NewGoMail NewData

--- app/message/service/internal/data/data.go
+++ app/message/service/internal/data/data.go
@@ -7,23 +7,57 @@ import (
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/google/wire"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
 	userv1 "github.com/the-zion/matrix-core/api/user/service/v1"
+	"github.com/the-zion/matrix-core/app/message/service/internal/conf"
+	"gopkg.in/gomail.v2"
 )
 
-var ProviderSet = wire.NewSet(NewData, NewUserRepo, NewUserServiceClient)
+var ProviderSet = wire.NewSet(NewData, NewUserRepo, NewPhoneCode, NewGoMail, NewUserServiceClient)
 
-type Data struct {
-	log *log.Helper
-	uc  userv1.UserClient
+type TxCode struct {
+	client  *sms.Client
+	request *sms.SendSmsRequest
 }
 
-func NewData(logger log.Logger, uc userv1.UserClient) (*Data, error) {
-	l := log.NewHelper(log.With(logger, "module", "message/data"))
-	d := &Data{
-		log: l,
-		uc:  uc,
+type GoMail struct {
+	message *gomail.Message
+	dialer  *gomail.Dialer
+}
+
+type Data struct {
+	log          *log.Helper
+	uc           userv1.UserClient
+	phoneCodeCli *TxCode
+	goMailCli    *GoMail
+}
+
+func NewPhoneCode(conf *conf.Data) *TxCode {
+	credential := common.NewCredential(
+		conf.Code.SecretId,
+		conf.Code.SecretKey,
+	)
+	cpf := profile.NewClientProfile()
+	client, _ := sms.NewClient(credential, "ap-guangzhou", cpf)
+	request := sms.NewSendSmsRequest()
+	request.SmsSdkAppId = common.StringPtr("1400590793")
+	request.SignName = common.StringPtr("魔方技术")
+	return &TxCode{
+		client:  client,
+		request: request,
 	}
-	return d, nil
+}
+
+func NewGoMail(conf *conf.Data) *GoMail {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "matrixtechnology@163.com")
+	d := gomail.NewDialer("smtp.163.com", 465, "matrixtechnology@163.com", conf.Mail.Code)
+	return &GoMail{
+		message: m,
+		dialer:  d,
+	}
 }
 
 func NewUserServiceClient(r *nacos.Registry, logger log.Logger) userv1.UserClient {
@@ -42,4 +76,15 @@ func NewUserServiceClient(r *nacos.Registry, logger log.Logger) userv1.UserClien
 	}
 	c := userv1.NewUserClient(conn)
 	return c
+}
+
+func NewData(logger log.Logger, uc userv1.UserClient, phoneCodeCli *TxCode, goMailCli *GoMail) (*Data, error) {
+	l := log.NewHelper(log.With(logger, "module", "message/data"))
+	d := &Data{
+		log:          l,
+		uc:           uc,
+		phoneCodeCli: phoneCodeCli,
+		goMailCli:    goMailCli,
+	}
+	return d, nil
 }
