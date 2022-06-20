9eff5852309b4cf5722d2119eefd6492ac1ff4fc
liusiyuan <945212191@qq.com>
2022-06-20 17:44:08 +0800

    refactor: update
    add: repo SendCode
    add: func SendCode

--- app/message/service/internal/biz/user.go
+++ app/message/service/internal/biz/user.go
@@ -1,10 +1,14 @@
 package biz
 
 import (
+	"context"
+	"fmt"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"github.com/go-kratos/kratos/v2/log"
 )
 
 type UserRepo interface {
+	SendCode(msgs ...*primitive.MessageExt)
 }
 
 type UserUseCase struct {
@@ -17,4 +21,13 @@ func NewUserUseCase(repo UserRepo, logger log.Logger) *UserUseCase {
 		repo: repo,
 		log:  log.NewHelper(log.With(logger, "module", "message/biz/userUseCase")),
 	}
+}
+
+func (r *UserUseCase) SendCode(msgs ...*primitive.MessageExt) {
+	r.repo.SendCode(msgs...)
+}
+
+func (r *UserUseCase) AvatarReview(ctx context.Context, ar *AvatarReview) error {
+	fmt.Println(ar)
+	return nil
 }
