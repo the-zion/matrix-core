11439c985549a671a08ed104d75a9c7b472a6f4e
liusiyuan <945212191@qq.com>
2022-06-20 17:33:57 +0800

    refactor: update
    delete: func GetUserIdFromContext

--- app/user/service/internal/service/service.go
+++ app/user/service/internal/service/service.go
@@ -1,10 +1,7 @@
 package service
 
 import (
-	"context"
 	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
-	v4 "github.com/golang-jwt/jwt/v4"
 	"github.com/google/wire"
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
 	"github.com/the-zion/matrix-core/app/user/service/internal/biz"
@@ -19,12 +16,6 @@ type UserService struct {
 	pc   *biz.ProfileUseCase
 	achc *biz.AchievementUseCase
 	log  *log.Helper
-}
-
-func GetUserIdFromContext(ctx context.Context) int64 {
-	claims, _ := jwt.FromContext(ctx)
-	mapClaims := *(claims.(*v4.MapClaims))
-	return int64(mapClaims["user_id"].(float64))
 }
 
 func NewUserService(uc *biz.UserUseCase, ac *biz.AuthUseCase, pc *biz.ProfileUseCase, logger log.Logger) *UserService {
