27c4754e94a0e1de69e8d0b5b471dcc5ea6cfd43
liusiyuan <945212191@qq.com>
2022-06-20 17:43:32 +0800

    feat: add AvatarReview

--- app/message/service/internal/biz/do.go
+++ app/message/service/internal/biz/do.go
@@ -1,1 +1,16 @@
 package biz
+
+type AvatarReview struct {
+	Code       string
+	Message    string
+	JobId      string
+	State      string
+	Object     string
+	Label      string
+	Result     int32
+	Category   string
+	BucketId   string
+	Region     string
+	CosHeaders map[string]string
+	EventName  string
+}
