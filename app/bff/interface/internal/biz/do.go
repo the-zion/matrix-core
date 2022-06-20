ecfda0b93bb298bb824023711d89a44860b38fb0
liusiyuan <945212191@qq.com>
2022-06-20 17:47:34 +0800

    feat: add struct AvatarReview

--- app/bff/interface/internal/biz/do.go
+++ app/bff/interface/internal/biz/do.go
@@ -23,3 +23,18 @@ type Credentials struct {
 	StartTime    int64
 	ExpiredTime  int64
 }
+
+type AvatarReview struct {
+	Code       string
+	Message    string
+	JobId      string
+	State      string
+	Object     string
+	Label      string
+	Result     int32
+	Category   string
+	BucketId   string
+	Region     string
+	CosHeaders map[string]string
+	EventName  string
+}
