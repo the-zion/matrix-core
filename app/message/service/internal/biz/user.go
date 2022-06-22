55eb056d468f681efe0d83192b1a3c4152f869c8
liusiyuan <945212191@qq.com>
2022-06-22 23:34:10 +0800

    refactor: update

--- app/message/service/internal/biz/user.go
+++ app/message/service/internal/biz/user.go
@@ -9,6 +9,7 @@ import (
 
 type UserRepo interface {
 	SendCode(msgs ...*primitive.MessageExt)
+	UploadProfileToCos(msgs ...*primitive.MessageExt)
 }
 
 type UserUseCase struct {
@@ -27,7 +28,18 @@ func (r *UserUseCase) SendCode(msgs ...*primitive.MessageExt) {
 	r.repo.SendCode(msgs...)
 }
 
+func (r *UserUseCase) UploadProfileToCos(msgs ...*primitive.MessageExt) {
+	r.repo.UploadProfileToCos(msgs...)
+}
+
 func (r *UserUseCase) AvatarReview(ctx context.Context, ar *AvatarReview) error {
 	fmt.Println(ar)
+	return nil
+}
+
+func (r *UserUseCase) ProfileReview(ctx context.Context, tr *TextReview) error {
+	if tr.State != "Success" {
+		r.log.Info("profile Review failed，%v", tr)
+	}
 	return nil
 }
