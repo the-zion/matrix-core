acdabb1bac022c2555d5a10150435ce9d057a761
liusiyuan <945212191@qq.com>
2022-06-28 18:40:23 +0800

    feat: add
    add: server

---
+++ app/creation/service/internal/server/grpc.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	v1 "github.com/the-zion/matrix-core/api/creation/service/v1"
+	"github.com/the-zion/matrix-core/app/creation/service/internal/conf"
+	"github.com/the-zion/matrix-core/app/creation/service/internal/service"
+)
+
+func NewGRPCServer(c *conf.Server, creationService *service.CreationService, logger log.Logger) *grpc.Server {
+	var opts = []grpc.ServerOption{
+		grpc.Middleware(
+			recovery.Recovery(),
+			logging.Server(logger),
+			validate.Validator(),
+		),
+	}
+	if c.Grpc.Network != "" {
+		opts = append(opts, grpc.Network(c.Grpc.Network))
+	}
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
+	}
+	if c.Grpc.Timeout != nil {
+		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
+	}
+	srv := grpc.NewServer(opts...)
+	v1.RegisterCreationServer(srv, creationService)
+	return srv
+}
---
+++ app/creation/service/internal/server/http.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/the-zion/matrix-core/api/creation/service/v1"
+	"github.com/the-zion/matrix-core/app/creation/service/internal/conf"
+	"github.com/the-zion/matrix-core/app/creation/service/internal/service"
+)
+
+// NewHTTPServer new a HTTP user.
+func NewHTTPServer(c *conf.Server, creationService *service.CreationService, logger log.Logger) *http.Server {
+	var opts = []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			logging.Server(logger),
+		),
+	}
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+	srv := http.NewServer(opts...)
+	v1.RegisterCreationHTTPServer(srv, creationService)
+	return srv
+}
