073004f5d7d40e41c0256ab6dd50885bf1172a40
liusiyuan <945212191@qq.com>
2022-06-28 18:46:36 +0800

    refactor: update

--- app/message/service/cmd/message/wire_gen.go
+++ app/message/service/cmd/message/wire_gen.go
@@ -20,7 +20,7 @@ import (
 // Injectors from wire.go:
 
 // wireApp init kratos application.
-func wireApp(confServer *conf.Server, confData *conf.Data, auth *conf.Auth, logger log.Logger, registry *nacos.Registry) (*kratos.App, func(), error) {
+func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger, registry *nacos.Registry) (*kratos.App, func(), error) {
 	userClient := data.NewUserServiceClient(registry, logger)
 	client := data.NewCosServiceClient(confData, logger)
 	txCode := data.NewPhoneCode(confData)
@@ -29,7 +29,7 @@ func wireApp(confServer *conf.Server, confData *conf.Data, auth *conf.Auth, logg
 	if err != nil {
 		return nil, nil, err
 	}
-	userRepo := data.NewUserRepo(auth, dataData, logger)
+	userRepo := data.NewUserRepo(dataData, logger)
 	userUseCase := biz.NewUserUseCase(userRepo, logger)
 	messageService := service.NewMessageService(userUseCase, logger)
 	httpServer := server.NewHTTPServer(confServer, messageService, logger)
