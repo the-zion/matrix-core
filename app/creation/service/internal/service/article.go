25322362d4ecb71efc72584847118e722f2c4bda
liusiyuan <945212191@qq.com>
2022-06-29 23:31:26 +0800

    refactor: update
    add: func GetLastArticleDraft ArticleDraftMark GetArticleDraftList

--- app/creation/service/internal/service/article.go
+++ app/creation/service/internal/service/article.go
@@ -3,7 +3,19 @@ package service
 import (
 	"context"
 	v1 "github.com/the-zion/matrix-core/api/creation/service/v1"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
+
+func (s *CreationService) GetLastArticleDraft(ctx context.Context, req *v1.GetLastArticleDraftReq) (*v1.GetLastArticleDraftReply, error) {
+	draft, err := s.ac.GetLastArticleDraft(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetLastArticleDraftReply{
+		Id:     draft.Id,
+		Status: draft.Status,
+	}, nil
+}
 
 func (s *CreationService) CreateArticleDraft(ctx context.Context, req *v1.CreateArticleDraftReq) (*v1.CreateArticleDraftReply, error) {
 	id, err := s.ac.CreateArticleDraft(ctx, req.Uuid)
@@ -13,4 +25,26 @@ func (s *CreationService) CreateArticleDraft(ctx context.Context, req *v1.Create
 	return &v1.CreateArticleDraftReply{
 		Id: id,
 	}, nil
+}
+
+func (s *CreationService) ArticleDraftMark(ctx context.Context, req *v1.ArticleDraftMarkReq) (*emptypb.Empty, error) {
+	err := s.ac.ArticleDraftMark(ctx, req.Uuid, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *CreationService) GetArticleDraftList(ctx context.Context, req *v1.GetArticleDraftListReq) (*v1.GetArticleDraftListReply, error) {
+	reply := &v1.GetArticleDraftListReply{Draft: make([]*v1.GetArticleDraftListReply_Draft, 0)}
+	draftList, err := s.ac.GetArticleDraftList(ctx, req.Uuid)
+	if err != nil {
+		return reply, err
+	}
+	for _, item := range draftList {
+		reply.Draft = append(reply.Draft, &v1.GetArticleDraftListReply_Draft{
+			Id: item.Id,
+		})
+	}
+	return reply, nil
 }
