48b6d052af5211cfc119fcb9a163b38b0b26b32e
liusiyuan <945212191@qq.com>
2022-06-29 23:31:54 +0800

    refactor: update
    add: func GetLastArticleDraft ArticleDraftMark GetArticleDraftList

--- app/creation/service/internal/data/article.go
+++ app/creation/service/internal/data/article.go
@@ -2,8 +2,14 @@ package data
 
 import (
 	"context"
+	"fmt"
+	kerrors "github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/pkg/errors"
 	"github.com/the-zion/matrix-core/app/creation/service/internal/biz"
+	"gorm.io/gorm"
+	"strconv"
+	"strings"
 )
 
 var _ biz.ArticleRepo = (*articleRepo)(nil)
@@ -20,6 +26,60 @@ func NewArticleRepo(data *Data, logger log.Logger) biz.ArticleRepo {
 	}
 }
 
+func (r *articleRepo) GetLastArticleDraft(ctx context.Context, uuid string) (*biz.ArticleDraft, error) {
+	draft := &ArticleDraft{}
+	err := r.data.db.Where("uuid = ?", uuid).Last(draft).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, kerrors.NotFound("draft not found from db", fmt.Sprintf("uuid(%s)", uuid))
+	}
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("db query system error: uuid(%s)", uuid))
+	}
+	return &biz.ArticleDraft{
+		Id:     int32(draft.ID),
+		Status: draft.Status,
+	}, nil
+}
+
 func (r *articleRepo) CreateArticleDraft(ctx context.Context, uuid string) (int32, error) {
-	return 0, nil
+	draft := &ArticleDraft{
+		Uuid: uuid,
+	}
+	err := r.data.DB(ctx).Select("Uuid").Create(draft).Error
+	if err != nil {
+		return 0, errors.Wrapf(err, fmt.Sprintf("fail to create an article draft: uuid(%s)", uuid))
+	}
+	return int32(draft.ID), nil
+}
+
+func (r *articleRepo) CreateArticleFolder(ctx context.Context, id int32) error {
+	name := "article/" + strconv.Itoa(int(id)) + "/"
+	_, err := r.data.cosCli.Object.Put(ctx, name, strings.NewReader(""), nil)
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to create an article folder: id(%v)", id))
+	}
+	return nil
+}
+
+func (r *articleRepo) ArticleDraftMark(ctx context.Context, uuid string, id int32) error {
+	err := r.data.db.WithContext(ctx).Model(&ArticleDraft{}).Where("uuid = ? and id = ?", uuid, id).Update("status", 3).Error
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to mark draft: uuid(%s), id(%v)", uuid, id))
+	}
+	return nil
+}
+
+func (r *articleRepo) GetArticleDraftList(ctx context.Context, uuid string) ([]*biz.ArticleDraft, error) {
+	reply := make([]*biz.ArticleDraft, 0)
+	draftList := make([]*ArticleDraft, 0)
+	err := r.data.db.WithContext(ctx).Where("uuid = ?", uuid).Order("id desc").Find(&draftList).Error
+	if err != nil {
+		return reply, errors.Wrapf(err, fmt.Sprintf("fail to get draft list : uuid(%s)", uuid))
+	}
+	for _, item := range draftList {
+		reply = append(reply, &biz.ArticleDraft{
+			Id: int32(item.ID),
+		})
+	}
+	return reply, nil
 }
