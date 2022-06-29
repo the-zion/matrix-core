7b93be95b9f59cf638d26a62c3d71065b993c5c7
liusiyuan <945212191@qq.com>
2022-06-29 23:30:50 +0800

    refactor: update
    add: func GetLastArticleDraft ArticleDraftMark GetArticleDraftList

--- app/bff/interface/internal/biz/creation.go
+++ app/bff/interface/internal/biz/creation.go
@@ -7,6 +7,9 @@ import (
 
 type ArticleRepo interface {
 	CreateArticleDraft(ctx context.Context, uuid string) (int32, error)
+	GetLastArticleDraft(ctx context.Context, uuid string) (*ArticleDraft, error)
+	ArticleDraftMark(ctx context.Context, uuid string, id int32) error
+	GetArticleDraftList(ctx context.Context, uuid string) ([]*ArticleDraft, error)
 }
 
 type ArticleUseCase struct {
@@ -21,7 +24,22 @@ func NewArticleUseCase(repo ArticleRepo, logger log.Logger) *ArticleUseCase {
 	}
 }
 
+func (r *ArticleUseCase) GetLastArticleDraft(ctx context.Context) (*ArticleDraft, error) {
+	uuid := ctx.Value("uuid").(string)
+	return r.repo.GetLastArticleDraft(ctx, uuid)
+}
+
 func (r *ArticleUseCase) CreateArticleDraft(ctx context.Context) (int32, error) {
 	uuid := ctx.Value("uuid").(string)
 	return r.repo.CreateArticleDraft(ctx, uuid)
+}
+
+func (r *ArticleUseCase) ArticleDraftMark(ctx context.Context, id int32) error {
+	uuid := ctx.Value("uuid").(string)
+	return r.repo.ArticleDraftMark(ctx, uuid, id)
+}
+
+func (r *ArticleUseCase) GetArticleDraftList(ctx context.Context) ([]*ArticleDraft, error) {
+	uuid := ctx.Value("uuid").(string)
+	return r.repo.GetArticleDraftList(ctx, uuid)
 }
