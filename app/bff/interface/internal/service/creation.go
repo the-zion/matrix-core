cfb5760ab176d462f24fca6bf6bc0c86439dd3b9
liusiyuan <945212191@qq.com>
2022-06-29 23:30:34 +0800

    refactor: update
    add: func GetLastArticleDraft ArticleDraftMark GetArticleDraftList

--- app/bff/interface/internal/service/creation.go
+++ app/bff/interface/internal/service/creation.go
@@ -6,6 +6,17 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+func (s *BffService) GetLastArticleDraft(ctx context.Context, _ *emptypb.Empty) (*v1.GetLastArticleDraftReply, error) {
+	draft, err := s.ac.GetLastArticleDraft(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetLastArticleDraftReply{
+		Id:     draft.Id,
+		Status: draft.Status,
+	}, nil
+}
+
 func (s *BffService) CreateArticleDraft(ctx context.Context, _ *emptypb.Empty) (*v1.CreateArticleDraftReply, error) {
 	id, err := s.ac.CreateArticleDraft(ctx)
 	if err != nil {
@@ -14,4 +25,26 @@ func (s *BffService) CreateArticleDraft(ctx context.Context, _ *emptypb.Empty) (
 	return &v1.CreateArticleDraftReply{
 		Id: id,
 	}, nil
+}
+
+func (s *BffService) ArticleDraftMark(ctx context.Context, req *v1.ArticleDraftMarkReq) (*emptypb.Empty, error) {
+	err := s.ac.ArticleDraftMark(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *BffService) GetArticleDraftList(ctx context.Context, _ *emptypb.Empty) (*v1.GetArticleDraftListReply, error) {
+	reply := &v1.GetArticleDraftListReply{Draft: make([]*v1.GetArticleDraftListReply_Draft, 0)}
+	draftList, err := s.ac.GetArticleDraftList(ctx)
+	if err != nil {
+		return reply, err
+	}
+	for _, item := range draftList {
+		reply.Draft = append(reply.Draft, &v1.GetArticleDraftListReply_Draft{
+			Id: item.Id,
+		})
+	}
+	return reply, nil
 }
