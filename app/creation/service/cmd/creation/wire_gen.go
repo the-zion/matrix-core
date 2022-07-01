8b7c4bebec230adcc0d37d74e8d04f772af8d707
liusiyuan <945212191@qq.com>
2022-07-01 23:49:36 +0800

    refactor: update

--- app/creation/service/cmd/creation/wire_gen.go
+++ app/creation/service/cmd/creation/wire_gen.go
@@ -23,8 +23,8 @@ import (
 func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger, registry *nacos.Registry) (*kratos.App, func(), error) {
 	db := data.NewDB(confData, logger)
 	client := data.NewCosServiceClient(confData, logger)
-	articleMqPro := data.NewRocketmqArticleProducer(confData, logger)
-	dataData, cleanup, err := data.NewData(db, client, articleMqPro, logger)
+	articleDraftMqPro := data.NewRocketmqArticleDraftProducer(confData, logger)
+	dataData, cleanup, err := data.NewData(db, client, articleDraftMqPro, logger)
 	if err != nil {
 		return nil, nil, err
 	}
