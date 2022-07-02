6171e986b31fdbaac3d504fe8a087f7c1287a171
liusiyuan <945212191@qq.com>
2022-07-02 23:09:51 +0800

    feat: add
    add: model Article
    add: model ArticleStatistic

--- app/creation/service/internal/data/model.go
+++ app/creation/service/internal/data/model.go
@@ -2,11 +2,29 @@ package data
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
+
+type Article struct {
+	ArticleId int32 `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Uuid      string `gorm:"index;size:36"`
+	Status    int32  `gorm:"default:1"`
+}
+
+type ArticleStatistic struct {
+	ArticleId int32 `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Agree     int32 `gorm:"default:0"`
+	View      int32 `gorm:"default:0"`
+	Collect   int32 `gorm:"default:0"`
+	Comment   int32 `gorm:"default:0"`
+}
 
 type ArticleDraft struct {
 	gorm.Model
-	Updated int64
-	Uuid    string `gorm:"index;size:36"`
-	Status  int32  `gorm:"default:1"`
+	Uuid   string `gorm:"index;size:36"`
+	Status int32  `gorm:"default:1"`
 }
