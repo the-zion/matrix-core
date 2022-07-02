7983fe75d68bf1e8cf6cbd2795a9badf6702c87e
liusiyuan <945212191@qq.com>
2022-07-02 23:13:08 +0800

    refactor: update

--- app/message/service/internal/service/user.go
+++ app/message/service/internal/service/user.go
@@ -16,59 +16,12 @@ func (s *MessageService) UploadProfileToCos(msgs ...*primitive.MessageExt) {
 	s.uc.UploadProfileToCos(msgs...)
 }
 
-func (s *MessageService) ProfileReview(ctx context.Context, req *v1.ProfileReviewReq) (*emptypb.Empty, error) {
-	tr := &biz.TextReview{
-		Code:         req.JobsDetail.Code,
-		Message:      req.JobsDetail.Message,
-		JobId:        req.JobsDetail.JobId,
-		DataId:       req.JobsDetail.DataId,
-		State:        req.JobsDetail.State,
-		CreationTime: req.JobsDetail.CreationTime,
-		Object:       req.JobsDetail.Object,
-		Label:        req.JobsDetail.Label,
-		Result:       req.JobsDetail.Result,
-		BucketId:     req.JobsDetail.BucketId,
-		Region:       req.JobsDetail.Region,
-		CosHeaders:   req.JobsDetail.CosHeaders,
-	}
-
-	var section []*biz.Section
-
-	for _, item := range req.JobsDetail.Section {
-		se := &biz.Section{
-			Label:  item.Label,
-			Result: item.Result,
-			PornInfo: &biz.SectionPornInfo{
-				HitFlag:  item.PornInfo.HitFlag,
-				Score:    item.PornInfo.Score,
-				Keywords: item.PornInfo.Keywords,
-			},
-			AdsInfo: &biz.SectionAdsInfo{
-				HitFlag:  item.AdsInfo.HitFlag,
-				Score:    item.AdsInfo.Score,
-				Keywords: item.AdsInfo.Keywords,
-			},
-			IllegalInfo: &biz.SectionIllegalInfo{
-				HitFlag:  item.IllegalInfo.HitFlag,
-				Score:    item.IllegalInfo.Score,
-				Keywords: item.IllegalInfo.Keywords,
-			},
-			AbuseInfo: &biz.SectionAbuseInfo{
-				HitFlag:  item.AbuseInfo.HitFlag,
-				Score:    item.AbuseInfo.Score,
-				Keywords: item.AbuseInfo.Keywords,
-			},
-		}
-		section = append(section, se)
-	}
-
-	tr.Section = section
-
+func (s *MessageService) ProfileReview(ctx context.Context, req *v1.TextReviewReq) (*emptypb.Empty, error) {
+	tr := s.TextReview(req)
 	err := s.uc.ProfileReview(ctx, tr)
 	if err != nil {
 		return nil, err
 	}
-
 	return &emptypb.Empty{}, nil
 }
 
