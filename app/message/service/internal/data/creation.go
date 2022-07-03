67d15d93561066f546ef72c1ed3dbafdc5f25eb3
liusiyuan <945212191@qq.com>
2022-07-03 23:51:33 +0800

    feat: add
    add: method CreateArticleCacheAndSearch

--- app/message/service/internal/data/creation.go
+++ app/message/service/internal/data/creation.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/pkg/errors"
 	"github.com/tencentyun/cos-go-sdk-v5"
 	creationV1 "github.com/the-zion/matrix-core/api/creation/service/v1"
 	"github.com/the-zion/matrix-core/app/message/service/internal/biz"
@@ -24,32 +25,42 @@ func NewCreationRepo(data *Data, logger log.Logger) biz.CreationRepo {
 	}
 }
 
-func (r *creationRepo) ToReviewArticle(mode string, msgs ...*primitive.MessageExt) {
-	for _, i := range msgs {
-		m := map[string]interface{}{"Uuid": "", "Id": ""}
-		err := json.Unmarshal(i.Body, &m)
-		if err != nil {
-			log.Errorf("fail to unmarshal profile: err(%v)", err)
-		}
-
-		opt := &cos.PutTextAuditingJobOptions{
-			InputObject: "article/" + strconv.Itoa(int(m["Id"].(float64))) + "/" + m["Uuid"].(string),
-			Conf: &cos.TextAuditingJobConf{
-				CallbackVersion: "Detail",
-				Callback:        r.data.cosCreationCli.callback[mode],
-			},
-		}
-
-		res, _, err := r.data.cosCreationCli.cos.CI.PutTextAuditingJob(context.Background(), opt)
-		fmt.Println(res)
-		if err != nil {
-			log.Errorf("fail to send article review request to cos: err(%v)", err)
-		}
+func (r *creationRepo) ToReviewArticle(mode string, msg *primitive.MessageExt) error {
+	m := map[string]interface{}{"Uuid": "", "Id": ""}
+	err := json.Unmarshal(msg.Body, &m)
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to unmarshal article: profile(%v)", msg.Body))
 	}
+
+	opt := &cos.PutTextAuditingJobOptions{
+		InputObject: "article/" + strconv.Itoa(int(m["Id"].(float64))) + "/" + m["Uuid"].(string),
+		Conf: &cos.TextAuditingJobConf{
+			CallbackVersion: "Detail",
+			Callback:        r.data.cosCreationCli.callback[mode],
+		},
+	}
+
+	res, _, err := r.data.cosCreationCli.cos.CI.PutTextAuditingJob(context.Background(), opt)
+	fmt.Println(res)
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to send article review request to cos: article(%v)", msg.Body))
+	}
+	return nil
 }
 
 func (r *creationRepo) ArticleDraftReviewPass(ctx context.Context, uuid string, id int32) error {
 	_, err := r.data.cc.CreateArticle(ctx, &creationV1.CreateArticleReq{
+		Id:   id,
+		Uuid: uuid,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *creationRepo) CreateArticleCacheAndSearch(ctx context.Context, uuid string, id int32) error {
+	_, err := r.data.cc.CreateArticleCacheAndSearch(ctx, &creationV1.CreateArticleCacheAndSearchReq{
 		Id:   id,
 		Uuid: uuid,
 	})
