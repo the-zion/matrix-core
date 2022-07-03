4e1bd7fe0e4a73b43b9d573d1c7e62c542e890fe
liusiyuan <945212191@qq.com>
2022-07-03 23:54:05 +0800

    refactor: update

--- app/message/service/internal/server/creationMq.go
+++ app/message/service/internal/server/creationMq.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/apache/rocketmq-client-go/v2"
 	"github.com/apache/rocketmq-client-go/v2/consumer"
@@ -28,7 +29,7 @@ func NewArticleDraftMqConsumerServer(conf *conf.Server, messageService *service.
 			AccessKey: conf.CreationMq.AccessKey,
 		}),
 		consumer.WithInstance("creation"),
-		consumer.WithMaxReconsumeTimes(5),
+		consumer.WithConsumeMessageBatchMaxSize(1),
 		consumer.WithNamespace(conf.CreationMq.NameSpace),
 		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset),
 		consumer.WithConsumerModel(consumer.Clustering),
@@ -37,13 +38,18 @@ func NewArticleDraftMqConsumerServer(conf *conf.Server, messageService *service.
 		l.Fatalf("init consumer error: %v", err)
 	}
 
-	//delayLevel := 2
+	delayLevel := 3
 	err = c.Subscribe("article_draft", consumer.MessageSelector{}, func(ctx context.Context,
 		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
-		messageService.ToReviewArticleDraft(msgs...)
-		fmt.Println(msgs)
-		//concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
-		//concurrentCtx.DelayLevelWhenNextConsume = delayLevel
+
+		concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
+		concurrentCtx.DelayLevelWhenNextConsume = delayLevel
+
+		err := messageService.ToReviewArticleDraft(msgs[0])
+		if err != nil {
+			l.Error(err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
 		return consumer.ConsumeSuccess, nil
 	})
 	if err != nil {
@@ -55,12 +61,84 @@ func NewArticleDraftMqConsumerServer(conf *conf.Server, messageService *service.
 	}
 }
 
-func (s *ArticleDraftMqConsumerServer) Start(ctx context.Context) error {
+func (s *ArticleDraftMqConsumerServer) Start(_ context.Context) error {
 	log.Info("mq article draft consumer starting")
 	return s.c.Start()
 }
 
-func (s *ArticleDraftMqConsumerServer) Stop(ctx context.Context) error {
+func (s *ArticleDraftMqConsumerServer) Stop(_ context.Context) error {
+	log.Info("mq article draft consumer closing")
+	return s.c.Shutdown()
+}
+
+type ArticleMqConsumerServer struct {
+	c  rocketmq.PushConsumer
+	ms *service.MessageService
+}
+
+func NewArticleMqConsumerServer(conf *conf.Server, messageService *service.MessageService, logger log.Logger) *ArticleMqConsumerServer {
+	l := log.NewHelper(log.With(logger, "server", "message/server/rocketmq-article-consumer"))
+	rlog.SetLogLevel("warn")
+	c, err := rocketmq.NewPushConsumer(
+		consumer.WithGroupName(conf.CreationMq.Article.GroupName),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{conf.CreationMq.ServerAddress})),
+		consumer.WithCredentials(primitive.Credentials{
+			SecretKey: conf.CreationMq.SecretKey,
+			AccessKey: conf.CreationMq.AccessKey,
+		}),
+		consumer.WithInstance("creation"),
+		consumer.WithConsumeMessageBatchMaxSize(1),
+		consumer.WithNamespace(conf.CreationMq.NameSpace),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset),
+		consumer.WithConsumerModel(consumer.Clustering),
+	)
+	if err != nil {
+		l.Fatalf("init consumer error: %v", err)
+	}
+
+	delayLevel := 3
+	err = c.Subscribe("article", consumer.MessageSelector{}, func(ctx context.Context,
+		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+
+		concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
+		concurrentCtx.DelayLevelWhenNextConsume = delayLevel
+
+		var err error
+		fmt.Println(msgs[0])
+		m := map[string]interface{}{}
+		err = json.Unmarshal(msgs[0].Body, &m)
+		if err != nil {
+			l.Errorf("fail to unmarshal msg: %s", err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+
+		switch m["mode"].(string) {
+		case "create_article_cache_and_search":
+			err = messageService.CreateArticleCacheAndSearch(ctx, m["Uuid"].(string), int32(m["Id"].(float64)))
+		}
+
+		//err = messageService.ToReviewArticleDraft(msgs[0])
+		if err != nil {
+			l.Error(err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+		return consumer.ConsumeSuccess, nil
+	})
+	if err != nil {
+		l.Fatalf("consumer subscribe error: %v", err)
+	}
+
+	return &ArticleMqConsumerServer{
+		c: c,
+	}
+}
+
+func (s *ArticleMqConsumerServer) Start(_ context.Context) error {
+	log.Info("mq article draft consumer starting")
+	return s.c.Start()
+}
+
+func (s *ArticleMqConsumerServer) Stop(_ context.Context) error {
 	log.Info("mq article draft consumer closing")
 	return s.c.Shutdown()
 }
