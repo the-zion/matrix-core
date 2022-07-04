2ef35478768345d49f6fc4e156cfc41b3d441009
liusiyuan <945212191@qq.com>
2022-07-04 23:55:54 +0800

    feat: add
    add: method GetArticleList

--- app/bff/interface/internal/data/creation.go
+++ app/bff/interface/internal/data/creation.go
@@ -36,6 +36,38 @@ func (r *articleRepo) GetLastArticleDraft(ctx context.Context, uuid string) (*bi
 		Status: reply.Status,
 	}, nil
 }
+func (r *articleRepo) GetArticleList(ctx context.Context, page int32) ([]*biz.Article, error) {
+	reply := make([]*biz.Article, 0)
+	articleList, err := r.data.ac.GetArticleList(ctx, &creationV1.GetArticleListReq{
+		Page: page,
+	})
+	if err != nil {
+		return reply, err
+	}
+	for _, item := range articleList.Article {
+		reply = append(reply, &biz.Article{
+			Id:   item.Id,
+			Uuid: item.Uuid,
+		})
+	}
+	return reply, nil
+}
+
+func (r *articleRepo) GetArticleDraftList(ctx context.Context, uuid string) ([]*biz.ArticleDraft, error) {
+	reply := make([]*biz.ArticleDraft, 0)
+	draftList, err := r.data.ac.GetArticleDraftList(ctx, &creationV1.GetArticleDraftListReq{
+		Uuid: uuid,
+	})
+	if err != nil {
+		return reply, err
+	}
+	for _, item := range draftList.Draft {
+		reply = append(reply, &biz.ArticleDraft{
+			Id: item.Id,
+		})
+	}
+	return reply, nil
+}
 
 func (r *articleRepo) CreateArticleDraft(ctx context.Context, uuid string) (int32, error) {
 	reply, err := r.data.ac.CreateArticleDraft(ctx, &creationV1.CreateArticleDraftReq{
@@ -56,22 +88,6 @@ func (r *articleRepo) ArticleDraftMark(ctx context.Context, uuid string, id int3
 		return err
 	}
 	return nil
-}
-
-func (r *articleRepo) GetArticleDraftList(ctx context.Context, uuid string) ([]*biz.ArticleDraft, error) {
-	reply := make([]*biz.ArticleDraft, 0)
-	draftList, err := r.data.ac.GetArticleDraftList(ctx, &creationV1.GetArticleDraftListReq{
-		Uuid: uuid,
-	})
-	if err != nil {
-		return reply, err
-	}
-	for _, item := range draftList.Draft {
-		reply = append(reply, &biz.ArticleDraft{
-			Id: item.Id,
-		})
-	}
-	return reply, nil
 }
 
 func (r *articleRepo) SendArticle(ctx context.Context, uuid string, id int32) error {
