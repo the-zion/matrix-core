fb0c2d267b7425fcc17db9b7eacca3fef7c3813f
liusiyuan <945212191@qq.com>
2022-07-04 23:56:47 +0800

    feat: add
    add: method GetArticleList

--- app/creation/service/internal/data/article.go
+++ app/creation/service/internal/data/article.go
@@ -7,6 +7,7 @@ import (
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	kerrors "github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
 	"github.com/the-zion/matrix-core/app/creation/service/internal/biz"
 	"gorm.io/gorm"
@@ -43,11 +44,24 @@ func (r *articleRepo) GetLastArticleDraft(ctx context.Context, uuid string) (*bi
 	}, nil
 }
 
+func (r *articleRepo) GetArticleList(ctx context.Context, page int32) ([]*biz.Article, error) {
+	article, err := r.getArticleFromCache(ctx, "article", page)
+	if err != nil {
+		return nil, err
+	}
+
+	size := len(article)
+	if size != 0 {
+		return article, nil
+	}
+	return nil, nil
+}
+
 func (r *articleRepo) GetArticleDraftList(ctx context.Context, uuid string) ([]*biz.ArticleDraft, error) {
 	reply := make([]*biz.ArticleDraft, 0)
 	draftList := make([]*ArticleDraft, 0)
-	//err := r.data.db.WithContext(ctx).Where("uuid = ? and status = ?", uuid, 3).Order("id desc").Find(&draftList).Error
-	err := r.data.db.WithContext(ctx).Where("uuid = ?", uuid).Order("id desc").Find(&draftList).Error
+	err := r.data.db.WithContext(ctx).Where("uuid = ? and status = ?", uuid, 3).Order("id desc").Find(&draftList).Error
+	//err := r.data.db.WithContext(ctx).Where("uuid = ?", uuid).Order("id desc").Find(&draftList).Error
 	if err != nil {
 		return reply, errors.Wrapf(err, fmt.Sprintf("fail to get draft list : uuid(%s)", uuid))
 	}
@@ -104,6 +118,30 @@ func (r *articleRepo) CreateArticleStatistic(ctx context.Context, uuid string, i
 }
 
 func (r *articleRepo) CreateArticleCache(ctx context.Context, uuid string, id int32) error {
+	ids := strconv.Itoa(int(id))
+	_, err := r.data.redisCli.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+		pipe.ZAddNX(ctx, "article", &redis.Z{
+			Score:  float64(id),
+			Member: ids + "%" + uuid,
+		})
+		pipe.ZAddNX(ctx, "article_hot", &redis.Z{
+			Score:  0,
+			Member: ids + "%" + uuid,
+		})
+		pipe.ZAddNX(ctx, "leaderboard", &redis.Z{
+			Score:  0,
+			Member: ids + "%" + uuid + "%article",
+		})
+		pipe.HSetNX(ctx, "article_"+ids, "uuid", uuid)
+		pipe.HSetNX(ctx, "article_"+ids, "agree", 0)
+		pipe.HSetNX(ctx, "article_"+ids, "collect", 0)
+		pipe.HSetNX(ctx, "article_"+ids, "view", 0)
+		pipe.HSetNX(ctx, "article_"+ids, "comment", 0)
+		return nil
+	})
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to create article cache: uuid(%s), id(%v)", uuid, id))
+	}
 	return nil
 }
 
@@ -116,7 +154,7 @@ func (r *articleRepo) DeleteArticleDraft(ctx context.Context, uuid string, id in
 	ad.ID = uint(id)
 	err := r.data.DB(ctx).Where("uuid = ?", uuid).Delete(ad).Error
 	if err != nil {
-		errors.Wrapf(err, fmt.Sprintf("fail to delete article draft: uuid(%s), id(%v)", uuid, id))
+		return errors.Wrapf(err, fmt.Sprintf("fail to delete article draft: uuid(%s), id(%v)", uuid, id))
 	}
 	return nil
 }
@@ -133,7 +171,7 @@ func (r *articleRepo) SendArticle(ctx context.Context, uuid string, id int32) (*
 	ad := &ArticleDraft{
 		Status: 2,
 	}
-	err := r.data.DB(ctx).Model(&ArticleDraft{}).Where("id = ? and uuid = ?", id, uuid).Updates(ad).Error
+	err := r.data.DB(ctx).Model(&ArticleDraft{}).Where("id = ? and uuid = ? and status = ?", id, uuid, 3).Updates(ad).Error
 	if err != nil {
 		return nil, errors.Wrapf(err, fmt.Sprintf("fail to mark draft to 2: uuid(%s), id(%v)", uuid, id))
 	}
@@ -177,7 +215,32 @@ func (r *articleRepo) SendArticleToMq(ctx context.Context, article *biz.Article,
 	msg.WithKeys([]string{article.Uuid})
 	_, err = r.data.articleDraftMqPro.producer.SendSync(ctx, msg)
 	if err != nil {
-		return errors.Wrapf(err, fmt.Sprintf("fail to send article to mq: %v", err))
+		return errors.Wrapf(err, fmt.Sprintf("fail to send article to mq: %v", article))
 	}
 	return nil
+}
+
+func (r *articleRepo) getArticleFromCache(ctx context.Context, key string, page int32) ([]*biz.Article, error) {
+	if page < 1 {
+		page = 1
+	}
+	index := int64(page - 1)
+	list, err := r.data.redisCli.ZRevRange(ctx, key, index, index+9).Result()
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("fail to get article from cache: key(%s), page(%v)", key, page))
+	}
+
+	article := make([]*biz.Article, 0)
+	for _, item := range list {
+		member := strings.Split(item, "%")
+		id, err := strconv.ParseInt(member[0], 10, 32)
+		if err != nil {
+			return nil, errors.Wrapf(err, fmt.Sprintf("fail to covert string to int64: id(%s)", member[0]))
+		}
+		article = append(article, &biz.Article{
+			ArticleId: int32(id),
+			Uuid:      member[1],
+		})
+	}
+	return article, nil
 }
