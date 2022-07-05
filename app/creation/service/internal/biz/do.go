ce0a58dcce8542c30ea27db9187672a679985f2c
liusiyuan <945212191@qq.com>
2022-07-05 23:56:49 +0800

    feat: add
    add: do ArticleStatistic

--- app/creation/service/internal/biz/do.go
+++ app/creation/service/internal/biz/do.go
@@ -6,6 +6,15 @@ type Article struct {
 	Status    int32
 }
 
+type ArticleStatistic struct {
+	ArticleId int32
+	Uuid      string
+	Agree     int32
+	View      int32
+	Collect   int32
+	Comment   int32
+}
+
 type ArticleDraft struct {
 	Id     int32
 	Status int32
