cd53c83f528f35d6e41c28aecf4a057d13af1d13
liusiyuan <945212191@qq.com>
2022-07-06 23:53:04 +0800

    feat: add
    add: CreationRepo CreationUseCase
    add: repo GetArticleStatistic
    add: method GetLeaderBoard GetArticleStatistic

--- app/bff/interface/internal/biz/creation.go
+++ app/bff/interface/internal/biz/creation.go
@@ -5,11 +5,16 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 )
 
+type CreationRepo interface {
+	GetLeaderBoard(ctx context.Context) ([]*LeaderBoard, error)
+}
+
 type ArticleRepo interface {
 	CreateArticleDraft(ctx context.Context, uuid string) (int32, error)
 	GetLastArticleDraft(ctx context.Context, uuid string) (*ArticleDraft, error)
 	GetArticleList(ctx context.Context, page int32) ([]*Article, error)
 	GetArticleListHot(ctx context.Context, page int32) ([]*Article, error)
+	GetArticleStatistic(ctx context.Context, id int32) (*ArticleStatistic, error)
 	GetArticleListStatistic(ctx context.Context, ids []int32) ([]*ArticleStatistic, error)
 	GetArticleDraftList(ctx context.Context, uuid string) ([]*ArticleDraft, error)
 	ArticleDraftMark(ctx context.Context, uuid string, id int32) error
@@ -21,11 +26,27 @@ type ArticleUseCase struct {
 	log  *log.Helper
 }
 
+type CreationUseCase struct {
+	repo CreationRepo
+	log  *log.Helper
+}
+
 func NewArticleUseCase(repo ArticleRepo, logger log.Logger) *ArticleUseCase {
 	return &ArticleUseCase{
 		repo: repo,
 		log:  log.NewHelper(log.With(logger, "module", "bff/biz/ArticleUseCase")),
 	}
+}
+
+func NewCreationUseCase(repo CreationRepo, logger log.Logger) *CreationUseCase {
+	return &CreationUseCase{
+		repo: repo,
+		log:  log.NewHelper(log.With(logger, "module", "bff/biz/CreationUseCase")),
+	}
+}
+
+func (r *CreationUseCase) GetLeaderBoard(ctx context.Context) ([]*LeaderBoard, error) {
+	return r.repo.GetLeaderBoard(ctx)
 }
 
 func (r *ArticleUseCase) GetArticleList(ctx context.Context, page int32) ([]*Article, error) {
@@ -34,6 +55,10 @@ func (r *ArticleUseCase) GetArticleList(ctx context.Context, page int32) ([]*Art
 
 func (r *ArticleUseCase) GetArticleListHot(ctx context.Context, page int32) ([]*Article, error) {
 	return r.repo.GetArticleListHot(ctx, page)
+}
+
+func (r *ArticleUseCase) GetArticleStatistic(ctx context.Context, id int32) (*ArticleStatistic, error) {
+	return r.repo.GetArticleStatistic(ctx, id)
 }
 
 func (r *ArticleUseCase) GetArticleListStatistic(ctx context.Context, ids []int32) ([]*ArticleStatistic, error) {
