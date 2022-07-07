2ffed46d675ac39d140e324af87d1b69e6486156
liusiyuan <945212191@qq.com>
2022-07-07 23:11:55 +0800

    feat: add

---
+++ app/achievement/service/internal/tool/update/db.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/the-zion/matrix-core/app/achievement/service/internal/data"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+var (
+	source string
+)
+
+func NewDB(logger log.Logger) *gorm.DB {
+	l := log.NewHelper(log.With(logger, "update", "db"))
+
+	db, err := gorm.Open(mysql.Open(source), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		l.Fatalf("failed opening connection to db: %v", err)
+	}
+	if err := db.AutoMigrate(
+		&data.Achievement{},
+	); err != nil {
+		l.Fatalf("failed creat or update table resources: %v", err)
+	}
+	return db
+}
+
+func main() {
+	flag.Parse()
+	logger := log.NewStdLogger(os.Stdout)
+	NewDB(logger)
+}
+
+func init() {
+	flag.StringVar(&source, "source",
+		"root:123456@tcp(127.0.0.1:3306)/core?charset=utf8mb4&parseTime=True&loc=Local",
+		"database source, eg: -source source path")
+}
