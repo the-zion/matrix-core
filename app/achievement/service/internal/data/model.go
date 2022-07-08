4d002cc9438da8a9ad4c4598d0b8d158bd5c2fcc
liusiyuan <945212191@qq.com>
2022-07-08 23:55:11 +0800

    refactor: update

--- app/achievement/service/internal/data/model.go
+++ app/achievement/service/internal/data/model.go
@@ -1,11 +1,6 @@
 package data
 
-import (
-	"gorm.io/gorm"
-)
-
 type Achievement struct {
-	gorm.Model
 	Uuid     string `gorm:"primaryKey;size:36"`
 	Agree    int32  `gorm:"default:0"`
 	Collect  int32  `gorm:"default:0"`
