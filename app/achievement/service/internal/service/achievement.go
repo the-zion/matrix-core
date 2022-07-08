89fe1ddf032c09fac388c32f4aa2aed2f9d0509a
liusiyuan <945212191@qq.com>
2022-07-08 23:54:39 +0800

    feat: add
    add: method SetAchievementAgree SetAchievementView SetAchievementCollect

--- app/achievement/service/internal/service/achievement.go
+++ app/achievement/service/internal/service/achievement.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"context"
+	v1 "github.com/the-zion/matrix-core/api/achievement/service/v1"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *AchievementService) SetAchievementAgree(ctx context.Context, req *v1.SetAchievementAgreeReq) (*emptypb.Empty, error) {
+	err := s.ac.SetAchievementAgree(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *AchievementService) SetAchievementView(ctx context.Context, req *v1.SetAchievementViewReq) (*emptypb.Empty, error) {
+	err := s.ac.SetAchievementView(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *AchievementService) SetAchievementCollect(ctx context.Context, req *v1.SetAchievementCollectReq) (*emptypb.Empty, error) {
+	err := s.ac.SetAchievementCollect(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
