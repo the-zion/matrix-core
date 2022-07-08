294250137e9b4d011bcf8d37a7a029c64afedc36
liusiyuan <945212191@qq.com>
2022-07-08 23:58:57 +0800

    feat: add

---
+++ app/message/service/internal/biz/achievement.go
@@ -0,0 +1,36 @@
+package biz
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type AchievementRepo interface {
+	SetAchievementAgree(ctx context.Context, uuid string) error
+	SetAchievementView(ctx context.Context, uuid string) error
+	SetAchievementCollect(ctx context.Context, uuid string) error
+}
+
+type AchievementCase struct {
+	repo AchievementRepo
+	log  *log.Helper
+}
+
+func NewAchievementUseCase(repo AchievementRepo, logger log.Logger) *AchievementCase {
+	return &AchievementCase{
+		repo: repo,
+		log:  log.NewHelper(log.With(logger, "module", "message/biz/achievementUseCase")),
+	}
+}
+
+func (r *AchievementCase) SetAchievementAgree(ctx context.Context, uuid string) error {
+	return r.repo.SetAchievementAgree(ctx, uuid)
+}
+
+func (r *AchievementCase) SetAchievementView(ctx context.Context, uuid string) error {
+	return r.repo.SetAchievementView(ctx, uuid)
+}
+
+func (r *AchievementCase) SetAchievementCollect(ctx context.Context, uuid string) error {
+	return r.repo.SetAchievementCollect(ctx, uuid)
+}
