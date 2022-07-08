1b2f96bded4b73932a0bac077117b2c89296c16f
liusiyuan <945212191@qq.com>
2022-07-08 23:55:21 +0800

    refactor: update

--- app/bff/interface/internal/biz/creation.go
+++ app/bff/interface/internal/biz/creation.go
@@ -7,6 +7,7 @@ import (
 
 type CreationRepo interface {
 	GetLeaderBoard(ctx context.Context) ([]*LeaderBoard, error)
+	CreateCollections(ctx context.Context, uuid, name, introduce string, auth int32) error
 }
 
 type ArticleRepo interface {
@@ -17,9 +18,11 @@ type ArticleRepo interface {
 	GetArticleStatistic(ctx context.Context, id int32) (*ArticleStatistic, error)
 	GetArticleListStatistic(ctx context.Context, ids []int32) ([]*ArticleStatistic, error)
 	GetArticleDraftList(ctx context.Context, uuid string) ([]*ArticleDraft, error)
-	ArticleDraftMark(ctx context.Context, uuid string, id int32) error
-	SendArticle(ctx context.Context, uuid string, id int32) error
-	SetArticleAgree(ctx context.Context, uuid string, id int32) error
+	ArticleDraftMark(ctx context.Context, id int32, uuid string) error
+	SendArticle(ctx context.Context, id int32, uuid string) error
+	SetArticleAgree(ctx context.Context, id int32, uuid string) error
+	SetArticleView(ctx context.Context, id int32, uuid string) error
+	SetArticleCollect(ctx context.Context, id, collectionsId int32, uuid, userUuid string) error
 }
 
 type ArticleUseCase struct {
@@ -78,7 +81,7 @@ func (r *ArticleUseCase) CreateArticleDraft(ctx context.Context) (int32, error) 
 
 func (r *ArticleUseCase) ArticleDraftMark(ctx context.Context, id int32) error {
 	uuid := ctx.Value("uuid").(string)
-	return r.repo.ArticleDraftMark(ctx, uuid, id)
+	return r.repo.ArticleDraftMark(ctx, id, uuid)
 }
 
 func (r *ArticleUseCase) GetArticleDraftList(ctx context.Context) ([]*ArticleDraft, error) {
@@ -88,9 +91,23 @@ func (r *ArticleUseCase) GetArticleDraftList(ctx context.Context) ([]*ArticleDra
 
 func (r *ArticleUseCase) SendArticle(ctx context.Context, id int32) error {
 	uuid := ctx.Value("uuid").(string)
-	return r.repo.SendArticle(ctx, uuid, id)
+	return r.repo.SendArticle(ctx, id, uuid)
 }
 
 func (r *ArticleUseCase) SetArticleAgree(ctx context.Context, id int32, uuid string) error {
-	return r.repo.SetArticleAgree(ctx, uuid, id)
+	return r.repo.SetArticleAgree(ctx, id, uuid)
+}
+
+func (r *ArticleUseCase) SetArticleView(ctx context.Context, id int32, uuid string) error {
+	return r.repo.SetArticleView(ctx, id, uuid)
+}
+
+func (r *ArticleUseCase) SetArticleCollect(ctx context.Context, id, collectionsId int32, uuid string) error {
+	userUuid := ctx.Value("uuid").(string)
+	return r.repo.SetArticleCollect(ctx, id, collectionsId, uuid, userUuid)
+}
+
+func (r *CreationUseCase) CreateCollections(ctx context.Context, name, introduce string, auth int32) error {
+	uuid := ctx.Value("uuid").(string)
+	return r.repo.CreateCollections(ctx, uuid, name, introduce, auth)
 }
