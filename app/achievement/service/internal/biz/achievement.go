11bd0062c54d239425e106f8e14ec6f4d6c92232
liusiyuan <945212191@qq.com>
2022-07-09 23:28:29 +0800

    feat: add
    add: repo SetAchievementCollectToCache
    add: repo CancelAchievementAgree
    add: repo CancelAchievementAgreeFromCache
    add: repo CancelAchievementCollect
    add: repo CancelAchievementCollectFromCache

--- app/achievement/service/internal/biz/achievement.go
+++ app/achievement/service/internal/biz/achievement.go
@@ -12,7 +12,11 @@ type AchievementRepo interface {
 	SetAchievementCollect(ctx context.Context, uuid string) error
 	SetAchievementAgreeToCache(ctx context.Context, uuid string) error
 	SetAchievementViewToCache(ctx context.Context, uuid string) error
-	SetAchievementViewToCache(ctx context.Context, uuid string) error
+	SetAchievementCollectToCache(ctx context.Context, uuid string) error
+	CancelAchievementAgree(ctx context.Context, uuid string) error
+	CancelAchievementAgreeFromCache(ctx context.Context, uuid string) error
+	CancelAchievementCollect(ctx context.Context, uuid string) error
+	CancelAchievementCollectFromCache(ctx context.Context, uuid string) error
 }
 
 type AchievementUseCase struct {
@@ -37,7 +41,21 @@ func (r *AchievementUseCase) SetAchievementAgree(ctx context.Context, uuid strin
 		}
 		err = r.repo.SetAchievementAgreeToCache(ctx, uuid)
 		if err != nil {
-			return v1.ErrorSendAchievementAgreeFailed("set achievement agree failed", err.Error())
+			return v1.ErrorSendAchievementAgreeFailed("set achievement agree to cache failed", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *AchievementUseCase) CancelAchievementAgree(ctx context.Context, uuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.CancelAchievementAgree(ctx, uuid)
+		if err != nil {
+			return v1.ErrorCancelAchievementAgreeFailed("cancel achievement agree failed", err.Error())
+		}
+		err = r.repo.CancelAchievementAgreeFromCache(ctx, uuid)
+		if err != nil {
+			return v1.ErrorCancelAchievementAgreeFailed("cancel achievement agree from cache failed", err.Error())
 		}
 		return nil
 	})
@@ -51,7 +69,7 @@ func (r *AchievementUseCase) SetAchievementView(ctx context.Context, uuid string
 		}
 		err = r.repo.SetAchievementViewToCache(ctx, uuid)
 		if err != nil {
-			return v1.ErrorSendAchievementViewFailed("set achievement view failed", err.Error())
+			return v1.ErrorSendAchievementViewFailed("set achievement view to cache failed", err.Error())
 		}
 		return nil
 	})
@@ -65,7 +83,20 @@ func (r *AchievementUseCase) SetAchievementCollect(ctx context.Context, uuid str
 		}
 		err = r.repo.SetAchievementCollectToCache(ctx, uuid)
 		if err != nil {
-			return v1.ErrorSendAchievementCollectFailed("set achievement collect failed", err.Error())
+			return v1.ErrorSendAchievementCollectFailed("set achievement collect to cache failed", err.Error())
+		}
+		return nil
+	})
+}
+func (r *AchievementUseCase) CancelAchievementCollect(ctx context.Context, uuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.CancelAchievementCollect(ctx, uuid)
+		if err != nil {
+			return v1.ErrorSendAchievementCollectFailed("cancel achievement collect failed", err.Error())
+		}
+		err = r.repo.CancelAchievementCollectFromCache(ctx, uuid)
+		if err != nil {
+			return v1.ErrorSendAchievementCollectFailed("cancel achievement collect from cache failed", err.Error())
 		}
 		return nil
 	})
