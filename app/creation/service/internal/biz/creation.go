c9b1952816dc32c8f2ec0df2141b9f4f55d9d42e
liusiyuan <945212191@qq.com>
2022-07-12 23:25:47 +0800

    feat: add
    add: repo GetCollectCount GetCollection EditCollections DeleteCollections

--- app/creation/service/internal/biz/creation.go
+++ app/creation/service/internal/biz/creation.go
@@ -10,11 +10,15 @@ type CreationRepo interface {
 	GetLeaderBoard(ctx context.Context) ([]*LeaderBoard, error)
 	GetCollectArticle(ctx context.Context, id, page int32) ([]*Article, error)
 	GetCollectArticleCount(ctx context.Context, id int32) (int32, error)
+	GetCollectCount(ctx context.Context, id int32) (int64, error)
+	GetCollection(ctx context.Context, id int32, uuid string) (*Collections, error)
 	GetCollections(ctx context.Context, uuid string, page int32) ([]*Collections, error)
 	GetCollectionsByVisitor(ctx context.Context, uuid string, page int32) ([]*Collections, error)
 	GetCollectionsCount(ctx context.Context, uuid string) (int32, error)
 	GetCollectionsVisitorCount(ctx context.Context, uuid string) (int32, error)
 	CreateCollections(ctx context.Context, uuid, name, introduce string, auth int32) error
+	EditCollections(ctx context.Context, id int32, uuid, name, introduce string, auth int32) error
+	DeleteCollections(ctx context.Context, id int32, uuid string) error
 }
 
 type CreationUseCase struct {
@@ -40,7 +44,7 @@ func (r *CreationUseCase) GetLeaderBoard(ctx context.Context) ([]*LeaderBoard, e
 func (r *CreationUseCase) GetCollectArticle(ctx context.Context, id, page int32) ([]*Article, error) {
 	articleList, err := r.repo.GetCollectArticle(ctx, id, page)
 	if err != nil {
-		return nil, v1.ErrorGetCollectionsArticleFailed("get collect article list failed: %s", err.Error())
+		return nil, v1.ErrorGetCollectArticleFailed("get collect article list failed: %s", err.Error())
 	}
 	return articleList, nil
 }
@@ -48,9 +52,17 @@ func (r *CreationUseCase) GetCollectArticle(ctx context.Context, id, page int32)
 func (r *CreationUseCase) GetCollectArticleCount(ctx context.Context, id int32) (int32, error) {
 	count, err := r.repo.GetCollectArticleCount(ctx, id)
 	if err != nil {
-		return 0, v1.ErrorGetCollectionsArticleCountFailed("get collect article count failed: %s", err.Error())
+		return 0, v1.ErrorGetCollectArticleCountFailed("get collect article count failed: %s", err.Error())
 	}
 	return count, nil
+}
+
+func (r *CreationUseCase) GetCollection(ctx context.Context, id int32, uuid string) (*Collections, error) {
+	collection, err := r.repo.GetCollection(ctx, id, uuid)
+	if err != nil {
+		return nil, v1.ErrorGetCollectionFailed("get collection failed: %s", err.Error())
+	}
+	return collection, nil
 }
 
 func (r *CreationUseCase) GetCollections(ctx context.Context, uuid string, page int32) ([]*Collections, error) {
@@ -88,6 +100,31 @@ func (r *CreationUseCase) CreateCollections(ctx context.Context, uuid, name, int
 	err := r.repo.CreateCollections(ctx, uuid, name, introduce, auth)
 	if err != nil {
 		return v1.ErrorCreateCollectionsFailed("create collections failed: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *CreationUseCase) EditCollections(ctx context.Context, id int32, uuid, name, introduce string, auth int32) error {
+	err := r.repo.EditCollections(ctx, id, uuid, name, introduce, auth)
+	if err != nil {
+		return v1.ErrorEditCollectionsFailed("edit collections failed: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *CreationUseCase) DeleteCollections(ctx context.Context, id int32, uuid string) error {
+	count, err := r.repo.GetCollectCount(ctx, id)
+	if err != nil {
+		return v1.ErrorGetCollectCountFailed("get collection count failed: %s", err.Error())
+	}
+
+	if count == 1 {
+		return v1.ErrorCollectionsIsNotEmpty("collections is not empty")
+	}
+
+	err = r.repo.DeleteCollections(ctx, id, uuid)
+	if err != nil {
+		return v1.ErrorDeleteCollectionsFailed("delete collections failed: %s", err.Error())
 	}
 	return nil
 }
