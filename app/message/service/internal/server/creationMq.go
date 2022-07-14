05488f0f268db84aef109c76463f1a289d992bbe
liusiyuan <945212191@qq.com>
2022-07-14 23:54:23 +0800

    refactor: update
    change draft to review

--- app/message/service/internal/server/creationMq.go
+++ app/message/service/internal/server/creationMq.go
@@ -12,16 +12,16 @@ import (
 	"github.com/the-zion/matrix-core/app/message/service/internal/service"
 )
 
-type ArticleDraftMqConsumerServer struct {
+type ArticleReviewMqConsumerServer struct {
 	c  rocketmq.PushConsumer
 	ms *service.MessageService
 }
 
-func NewArticleDraftMqConsumerServer(conf *conf.Server, messageService *service.MessageService, logger log.Logger) *ArticleDraftMqConsumerServer {
+func NewArticleReviewMqConsumerServer(conf *conf.Server, messageService *service.MessageService, logger log.Logger) *ArticleReviewMqConsumerServer {
 	l := log.NewHelper(log.With(logger, "server", "message/server/rocketmq-article-draft-consumer"))
 	rlog.SetLogLevel("warn")
 	c, err := rocketmq.NewPushConsumer(
-		consumer.WithGroupName(conf.CreationMq.ArticleDraft.GroupName),
+		consumer.WithGroupName(conf.CreationMq.ArticleReview.GroupName),
 		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{conf.CreationMq.ServerAddress})),
 		consumer.WithCredentials(primitive.Credentials{
 			SecretKey: conf.CreationMq.SecretKey,
@@ -38,13 +38,28 @@ func NewArticleDraftMqConsumerServer(conf *conf.Server, messageService *service.
 	}
 
 	delayLevel := 3
-	err = c.Subscribe("article_draft", consumer.MessageSelector{}, func(ctx context.Context,
+	err = c.Subscribe("article_review", consumer.MessageSelector{}, func(ctx context.Context,
 		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
 
 		concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
 		concurrentCtx.DelayLevelWhenNextConsume = delayLevel
 
-		err := messageService.ToReviewArticleDraft(msgs[0])
+		var err error
+		m := map[string]interface{}{}
+		err = json.Unmarshal(msgs[0].Body, &m)
+		if err != nil {
+			l.Errorf("fail to unmarshal msg: %s", err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+
+		mode := m["Mode"].(string)
+		switch mode {
+		case "create":
+			err = messageService.ToReviewCreateArticle(int32(m["Id"].(float64)), m["Uuid"].(string))
+		case "edit":
+			err = messageService.ToReviewEditArticle(int32(m["Id"].(float64)), m["Uuid"].(string))
+		}
+
 		if err != nil {
 			l.Error(err.Error())
 			return consumer.ConsumeRetryLater, nil
@@ -55,18 +70,18 @@ func NewArticleDraftMqConsumerServer(conf *conf.Server, messageService *service.
 		l.Fatalf("consumer subscribe error: %v", err)
 	}
 
-	return &ArticleDraftMqConsumerServer{
+	return &ArticleReviewMqConsumerServer{
 		c: c,
 	}
 }
 
-func (s *ArticleDraftMqConsumerServer) Start(_ context.Context) error {
-	log.Info("mq article draft consumer starting")
+func (s *ArticleReviewMqConsumerServer) Start(_ context.Context) error {
+	log.Info("mq article review consumer starting")
 	return s.c.Start()
 }
 
-func (s *ArticleDraftMqConsumerServer) Stop(_ context.Context) error {
-	log.Info("mq article draft consumer closing")
+func (s *ArticleReviewMqConsumerServer) Stop(_ context.Context) error {
+	log.Info("mq article review consumer closing")
 	return s.c.Shutdown()
 }
 
@@ -113,7 +128,9 @@ func NewArticleMqConsumerServer(conf *conf.Server, messageService *service.Messa
 		mode := m["mode"].(string)
 		switch mode {
 		case "create_article_cache_and_search":
-			err = messageService.CreateArticleCacheAndSearch(ctx, m["uuid"].(string), int32(m["id"].(float64)))
+			err = messageService.CreateArticleCacheAndSearch(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "edit_article_cos_and_search":
+			err = messageService.EditArticleCosAndSearch(ctx, int32(m["id"].(float64)), m["uuid"].(string))
 		}
 
 		if err != nil {
@@ -132,11 +149,11 @@ func NewArticleMqConsumerServer(conf *conf.Server, messageService *service.Messa
 }
 
 func (s *ArticleMqConsumerServer) Start(_ context.Context) error {
-	log.Info("mq article draft consumer starting")
+	log.Info("mq article consumer starting")
 	return s.c.Start()
 }
 
 func (s *ArticleMqConsumerServer) Stop(_ context.Context) error {
-	log.Info("mq article draft consumer closing")
+	log.Info("mq article consumer closing")
 	return s.c.Shutdown()
 }
