bf04402828dba7e52a5436beaa415231623e64d2
liusiyuan <945212191@qq.com>
2022-07-15 23:47:58 +0800

    feat: add
    add: provider TalkUseCase

--- app/bff/interface/internal/biz/biz.go
+++ app/bff/interface/internal/biz/biz.go
@@ -4,4 +4,4 @@ import (
 	"github.com/google/wire"
 )
 
-var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase)
+var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase)
