840256f51f91693c2b2b3dd4178bbf3b1a6f8104
liusiyuan <945212191@qq.com>
2022-07-15 23:54:31 +0800

    feat: add
    add: model Talk TalkStatistic TalkDraft

--- app/creation/service/internal/data/model.go
+++ app/creation/service/internal/data/model.go
@@ -12,6 +12,17 @@ type Article struct {
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 	Uuid      string         `gorm:"index;size:36"`
 	Status    int32          `gorm:"default:1"`
+	Auth      int32          `gorm:"default:1"`
+}
+
+type Talk struct {
+	TalkId    int32 `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Uuid      string         `gorm:"index;size:36"`
+	Status    int32          `gorm:"default:1"`
+	Auth      int32          `gorm:"default:1"`
 }
 
 type ArticleStatistic struct {
@@ -24,9 +35,29 @@ type ArticleStatistic struct {
 	View      int32          `gorm:"default:0"`
 	Collect   int32          `gorm:"default:0"`
 	Comment   int32          `gorm:"default:0"`
+	Auth      int32          `gorm:"default:1"`
+}
+
+type TalkStatistic struct {
+	TalkId    int32 `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Uuid      string         `gorm:"index;size:36"`
+	Agree     int32          `gorm:"index;default:0"`
+	View      int32          `gorm:"default:0"`
+	Collect   int32          `gorm:"default:0"`
+	Comment   int32          `gorm:"default:0"`
+	Auth      int32          `gorm:"default:1"`
 }
 
 type ArticleDraft struct {
+	gorm.Model
+	Uuid   string `gorm:"index;size:36"`
+	Status int32  `gorm:"default:1"`
+}
+
+type TalkDraft struct {
 	gorm.Model
 	Uuid   string `gorm:"index;size:36"`
 	Status int32  `gorm:"default:1"`
