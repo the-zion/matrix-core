b570d5c719306dc1f50da80f7c1dee0729534823
liusiyuan <945212191@qq.com>
2022-07-17 23:42:50 +0800

    feat: add
    add: repo GetTalkListHot
    add: repo GetTalkListStatistic
    add: repo GetTalkStatistic
    add: repo GetTalkAgreeJudge
    add: repo GetTalkCollectJudge
    add: repo SetTalkAgree
    add: repo SetTalkView
    add: repo SetTalkViewToCache
    add: repo SetTalkAgreeToCache
    add: repo SetTalkCollect
    add: repo SetTalkUserCollect
    add: repo SetTalkCollectToCache
    add: repo CancelTalkAgree
    add: repo CancelTalkAgreeFromCache
    add: repo CancelTalkUserCollect
    add: repo CancelTalkCollect
    add: repo CancelTalkCollectFromCache
    add: repo SendTalkStatisticToMq

--- app/creation/service/internal/biz/talk.go
+++ app/creation/service/internal/biz/talk.go
@@ -10,7 +10,12 @@ import (
 type TalkRepo interface {
 	GetTalk(ctx context.Context, id int32) (*Talk, error)
 	GetTalkList(ctx context.Context, page int32) ([]*Talk, error)
+	GetTalkListHot(ctx context.Context, page int32) ([]*TalkStatistic, error)
+	GetTalkListStatistic(ctx context.Context, ids []int32) ([]*TalkStatistic, error)
+	GetTalkStatistic(ctx context.Context, id int32) (*TalkStatistic, error)
 	GetLastTalkDraft(ctx context.Context, uuid string) (*TalkDraft, error)
+	GetTalkAgreeJudge(ctx context.Context, id int32, uuid string) (bool, error)
+	GetTalkCollectJudge(ctx context.Context, id int32, uuid string) (bool, error)
 
 	CreateTalkDraft(ctx context.Context, uuid string) (int32, error)
 	CreateTalkFolder(ctx context.Context, id int32, uuid string) error
@@ -19,9 +24,23 @@ type TalkRepo interface {
 	CreateTalkCache(ctx context.Context, id, auth int32, uuid string) error
 	CreateTalkSearch(ctx context.Context, id int32, uuid string) error
 
+	SetTalkAgree(ctx context.Context, id int32, uuid string) error
+	SetTalkView(ctx context.Context, id int32, uuid string) error
+	SetTalkViewToCache(ctx context.Context, id int32, uuid string) error
+	SetTalkAgreeToCache(ctx context.Context, id int32, uuid, userUuid string) error
+	SetTalkCollect(ctx context.Context, id int32, uuid string) error
+	SetTalkUserCollect(ctx context.Context, id, collectionsId int32, userUuid string) error
+	SetTalkCollectToCache(ctx context.Context, id int32, uuid, userUuid string) error
+	CancelTalkAgree(ctx context.Context, id int32, uuid string) error
+	CancelTalkAgreeFromCache(ctx context.Context, id int32, uuid, userUuid string) error
+	CancelTalkUserCollect(ctx context.Context, id int32, userUuid string) error
+	CancelTalkCollect(ctx context.Context, id int32, uuid string) error
+	CancelTalkCollectFromCache(ctx context.Context, id int32, uuid, userUuid string) error
+
 	SendTalk(ctx context.Context, id int32, uuid string) (*TalkDraft, error)
 	SendReviewToMq(ctx context.Context, review *TalkReview) error
-	SendTalkToMq(ctx context.Context, article *Talk, mode string) error
+	SendTalkToMq(ctx context.Context, talk *Talk, mode string) error
+	SendTalkStatisticToMq(ctx context.Context, uuid, mode string) error
 
 	DeleteTalkDraft(ctx context.Context, id int32, uuid string) error
 	EditTalkCos(ctx context.Context, id int32, uuid string) error
@@ -48,7 +67,7 @@ func (r *TalkUseCase) GetLastTalkDraft(ctx context.Context, uuid string) (*TalkD
 		return nil, v1.ErrorRecordNotFound("last draft not found: %s", err.Error())
 	}
 	if err != nil {
-		return nil, v1.ErrorGetArticleDraftFailed("get last draft failed: %s", err.Error())
+		return nil, v1.ErrorGetTalkDraftFailed("get last draft failed: %s", err.Error())
 	}
 	return draft, nil
 }
@@ -59,6 +78,30 @@ func (r *TalkUseCase) GetTalkList(ctx context.Context, page int32) ([]*Talk, err
 		return nil, v1.ErrorGetTalkListFailed("get talk list failed: %s", err.Error())
 	}
 	return talkList, nil
+}
+
+func (r *TalkUseCase) GetTalkListHot(ctx context.Context, page int32) ([]*TalkStatistic, error) {
+	talkList, err := r.repo.GetTalkListHot(ctx, page)
+	if err != nil {
+		return nil, v1.ErrorGetTalkListFailed("get talk hot list failed: %s", err.Error())
+	}
+	return talkList, nil
+}
+
+func (r *TalkUseCase) GetTalkListStatistic(ctx context.Context, ids []int32) ([]*TalkStatistic, error) {
+	statisticList, err := r.repo.GetTalkListStatistic(ctx, ids)
+	if err != nil {
+		return nil, v1.ErrorGetStatisticFailed("get talk list statistic failed: %s", err.Error())
+	}
+	return statisticList, nil
+}
+
+func (r *TalkUseCase) GetTalkStatistic(ctx context.Context, id int32) (*TalkStatistic, error) {
+	statistic, err := r.repo.GetTalkStatistic(ctx, id)
+	if err != nil {
+		return nil, v1.ErrorGetStatisticFailed("get talk statistic failed: %s", err.Error())
+	}
+	return statistic, nil
 }
 
 func (r *TalkUseCase) CreateTalkDraft(ctx context.Context, uuid string) (int32, error) {
@@ -191,4 +234,116 @@ func (r *TalkUseCase) EditTalkCosAndSearch(ctx context.Context, id int32, uuid s
 		return v1.ErrorEditTalkFailed("edit talk search failed: %s", err.Error())
 	}
 	return nil
+}
+
+func (r *TalkUseCase) SetTalkAgree(ctx context.Context, id int32, uuid, userUuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.SetTalkAgree(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorSetAgreeFailed("set talk agree failed: %s", err.Error())
+		}
+		err = r.repo.SetTalkAgreeToCache(ctx, id, uuid, userUuid)
+		if err != nil {
+			return v1.ErrorSetAgreeFailed("set talk agree to cache failed: %s", err.Error())
+		}
+		err = r.repo.SendTalkStatisticToMq(ctx, uuid, "agree")
+		if err != nil {
+			return v1.ErrorSetAgreeFailed("set talk agree to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *TalkUseCase) CancelTalkAgree(ctx context.Context, id int32, uuid, userUuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.CancelTalkAgree(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorCancelAgreeFailed("cancel talk agree failed: %s", err.Error())
+		}
+		err = r.repo.CancelTalkAgreeFromCache(ctx, id, uuid, userUuid)
+		if err != nil {
+			return v1.ErrorCancelAgreeFailed("cancel talk agree from cache failed: %s", err.Error())
+		}
+		err = r.repo.SendTalkStatisticToMq(ctx, uuid, "agree_cancel")
+		if err != nil {
+			return v1.ErrorCancelAgreeFailed("set talk agree to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *TalkUseCase) SetTalkView(ctx context.Context, id int32, uuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.SetTalkView(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorSetViewFailed("set talk view failed: %s", err.Error())
+		}
+		err = r.repo.SetTalkViewToCache(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorSetViewFailed("set talk view to cache failed: %s", err.Error())
+		}
+		err = r.repo.SendTalkStatisticToMq(ctx, uuid, "view")
+		if err != nil {
+			return v1.ErrorSetViewFailed("set talk view to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+func (r *TalkUseCase) SetTalkCollect(ctx context.Context, id, collectionsId int32, uuid, userUuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.SetTalkUserCollect(ctx, id, collectionsId, userUuid)
+		if err != nil {
+			return v1.ErrorSetCollectFailed("set talk user collect failed: %s", err.Error())
+		}
+		err = r.repo.SetTalkCollect(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorSetCollectFailed("set talk collect failed: %s", err.Error())
+		}
+		err = r.repo.SetTalkCollectToCache(ctx, id, uuid, userUuid)
+		if err != nil {
+			return v1.ErrorSetCollectFailed("set talk collect to cache failed: %s", err.Error())
+		}
+		err = r.repo.SendTalkStatisticToMq(ctx, uuid, "collect")
+		if err != nil {
+			return v1.ErrorSetCollectFailed("set talk collect to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *TalkUseCase) CancelTalkCollect(ctx context.Context, id int32, uuid, userUuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		err := r.repo.CancelTalkUserCollect(ctx, id, userUuid)
+		if err != nil {
+			return v1.ErrorCancelCollectFailed("cancel talk user collect failed: %s", err.Error())
+		}
+		err = r.repo.CancelTalkCollect(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorCancelCollectFailed("cancel talk collect failed: %s", err.Error())
+		}
+		err = r.repo.CancelTalkCollectFromCache(ctx, id, uuid, userUuid)
+		if err != nil {
+			return v1.ErrorCancelCollectFailed("cancel talk collect to cache failed: %s", err.Error())
+		}
+		err = r.repo.SendTalkStatisticToMq(ctx, uuid, "collect_cancel")
+		if err != nil {
+			return v1.ErrorCancelCollectFailed("set talk collect to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *TalkUseCase) TalkStatisticJudge(ctx context.Context, id int32, uuid string) (*TalkStatisticJudge, error) {
+	agree, err := r.repo.GetTalkAgreeJudge(ctx, id, uuid)
+	if err != nil {
+		return nil, v1.ErrorGetStatisticJudgeFailed("get talk statistic judge failed: %s", err.Error())
+	}
+	collect, err := r.repo.GetTalkCollectJudge(ctx, id, uuid)
+	if err != nil {
+		return nil, v1.ErrorGetStatisticJudgeFailed("get talk statistic judge failed: %s", err.Error())
+	}
+	return &TalkStatisticJudge{
+		Agree:   agree,
+		Collect: collect,
+	}, nil
 }
