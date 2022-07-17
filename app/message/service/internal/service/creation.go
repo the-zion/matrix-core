8b0d60ca6eb537f773adf202bdcc76a4ea3efb2e
liusiyuan <945212191@qq.com>
2022-07-17 23:53:09 +0800

    refactor: update

--- app/message/service/internal/service/creation.go
+++ app/message/service/internal/service/creation.go
@@ -2,24 +2,78 @@ package service
 
 import (
 	"context"
-	"github.com/apache/rocketmq-client-go/v2/primitive"
 	v1 "github.com/the-zion/matrix-core/api/message/service/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func (s *MessageService) ToReviewArticleDraft(msg *primitive.MessageExt) error {
-	return s.cc.ToReviewArticleDraft(msg)
+func (s *MessageService) ToReviewCreateArticle(id int32, uuid string) error {
+	return s.cc.ToReviewCreateArticle(id, uuid)
 }
 
-func (s *MessageService) ArticleDraftReview(ctx context.Context, req *v1.TextReviewReq) (*emptypb.Empty, error) {
+func (s *MessageService) ToReviewEditArticle(id int32, uuid string) error {
+	return s.cc.ToReviewEditArticle(id, uuid)
+}
+
+func (s *MessageService) ArticleCreateReview(ctx context.Context, req *v1.TextReviewReq) (*emptypb.Empty, error) {
 	tr := s.TextReview(req)
-	err := s.cc.ArticleDraftReview(ctx, tr)
+	err := s.cc.ArticleCreateReview(ctx, tr)
 	if err != nil {
 		return nil, err
 	}
 	return &emptypb.Empty{}, nil
 }
 
-func (s *MessageService) CreateArticleCacheAndSearch(ctx context.Context, uuid string, id int32) error {
-	return s.cc.CreateArticleCacheAndSearch(ctx, uuid, id)
+func (s *MessageService) ArticleEditReview(ctx context.Context, req *v1.TextReviewReq) (*emptypb.Empty, error) {
+	tr := s.TextReview(req)
+	err := s.cc.ArticleEditReview(ctx, tr)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *MessageService) CreateArticleCacheAndSearch(ctx context.Context, id, auth int32, uuid string) error {
+	return s.cc.CreateArticleCacheAndSearch(ctx, id, auth, uuid)
+}
+
+func (s *MessageService) EditArticleCosAndSearch(ctx context.Context, id int32, uuid string) error {
+	return s.cc.EditArticleCosAndSearch(ctx, id, uuid)
+}
+
+func (s *MessageService) DeleteArticleCacheAndSearch(ctx context.Context, id int32, uuid string) error {
+	return s.cc.DeleteArticleCacheAndSearch(ctx, id, uuid)
+}
+
+func (s *MessageService) ToReviewCreateTalk(id int32, uuid string) error {
+	return s.cc.ToReviewCreateTalk(id, uuid)
+}
+
+func (s *MessageService) ToReviewEditTalk(id int32, uuid string) error {
+	return s.cc.ToReviewEditTalk(id, uuid)
+}
+
+func (s *MessageService) TalkCreateReview(ctx context.Context, req *v1.TextReviewReq) (*emptypb.Empty, error) {
+	tr := s.TextReview(req)
+	err := s.cc.TalkCreateReview(ctx, tr)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *MessageService) TalkEditReview(ctx context.Context, req *v1.TextReviewReq) (*emptypb.Empty, error) {
+	tr := s.TextReview(req)
+	err := s.cc.TalkEditReview(ctx, tr)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *MessageService) CreateTalkCacheAndSearch(ctx context.Context, id, auth int32, uuid string) error {
+	return s.cc.CreateTalkCacheAndSearch(ctx, id, auth, uuid)
+}
+
+func (s *MessageService) EditTalkCosAndSearch(ctx context.Context, id int32, uuid string) error {
+	return s.cc.EditTalkCosAndSearch(ctx, id, uuid)
 }
