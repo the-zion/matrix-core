06f7523dd4958fa59bbddb726737b551b34b7d4f
liusiyuan <945212191@qq.com>
2022-07-19 23:40:38 +0800

    feat: add
    add: provider NewColumnUseCase

--- app/bff/interface/internal/biz/biz.go
+++ app/bff/interface/internal/biz/biz.go
@@ -4,4 +4,4 @@ import (
 	"github.com/google/wire"
 )
 
-var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase)
+var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase, NewColumnUseCase)
