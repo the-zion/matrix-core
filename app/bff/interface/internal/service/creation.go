5eea221948c92b16886984d47b009bbe6f0ab9bf
liusiyuan <945212191@qq.com>
2022-07-19 23:37:53 +0800

    feat: add
    add: method GetLastColumnDraft
    add: method CreateColumnDraft
    add: method CreateColumn
    add: method GetColumnList
    add: method GetColumnListHot
    add: method GetColumnListStatistic

--- app/bff/interface/internal/service/creation.go
+++ app/bff/interface/internal/service/creation.go
@@ -604,3 +604,79 @@ func (s *BffService) TalkStatisticJudge(ctx context.Context, req *v1.TalkStatist
 		Collect: judge.Collect,
 	}, nil
 }
+
+func (s *BffService) GetLastColumnDraft(ctx context.Context, _ *emptypb.Empty) (*v1.GetLastColumnDraftReply, error) {
+	draft, err := s.coc.GetLastColumnDraft(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetLastColumnDraftReply{
+		Id:     draft.Id,
+		Status: draft.Status,
+	}, nil
+}
+
+func (s *BffService) CreateColumnDraft(ctx context.Context, _ *emptypb.Empty) (*v1.CreateColumnDraftReply, error) {
+	id, err := s.coc.CreateColumnDraft(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.CreateColumnDraftReply{
+		Id: id,
+	}, nil
+}
+
+func (s *BffService) CreateColumn(ctx context.Context, req *v1.CreateColumnReq) (*emptypb.Empty, error) {
+	err := s.coc.CreateColumn(ctx, req.Name, req.Introduce, req.Cover, req.Auth)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *BffService) GetColumnList(ctx context.Context, req *v1.GetColumnListReq) (*v1.GetColumnListReply, error) {
+	reply := &v1.GetColumnListReply{Column: make([]*v1.GetColumnListReply_Column, 0)}
+	columnList, err := s.coc.GetColumnList(ctx, req.Page)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range columnList {
+		reply.Column = append(reply.Column, &v1.GetColumnListReply_Column{
+			Id:   item.Id,
+			Uuid: item.Uuid,
+		})
+	}
+	return reply, nil
+}
+
+func (s *BffService) GetColumnListHot(ctx context.Context, req *v1.GetColumnListHotReq) (*v1.GetColumnListHotReply, error) {
+	reply := &v1.GetColumnListHotReply{Column: make([]*v1.GetColumnListHotReply_Column, 0)}
+	columnList, err := s.coc.GetColumnListHot(ctx, req.Page)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range columnList {
+		reply.Column = append(reply.Column, &v1.GetColumnListHotReply_Column{
+			Id:   item.Id,
+			Uuid: item.Uuid,
+		})
+	}
+	return reply, nil
+}
+
+func (s *BffService) GetColumnListStatistic(ctx context.Context, req *v1.GetColumnListStatisticReq) (*v1.GetColumnListStatisticReply, error) {
+	reply := &v1.GetColumnListStatisticReply{Count: make([]*v1.GetColumnListStatisticReply_Count, 0)}
+	statisticList, err := s.coc.GetColumnListStatistic(ctx, req.Ids)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range statisticList {
+		reply.Count = append(reply.Count, &v1.GetColumnListStatisticReply_Count{
+			Id:      item.Id,
+			Agree:   item.Agree,
+			Collect: item.Collect,
+			View:    item.View,
+		})
+	}
+	return reply, nil
+}
