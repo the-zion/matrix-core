815105a5a94760a56c2db089ba27db40efb06f49
liusiyuan <945212191@qq.com>
2022-07-20 23:50:08 +0800

    feat: add
    add: repo SendColumn
    add: repo EditColumnCosAndSearch
    add: repo GetUserColumnList
    add: repo GetUserColumnListVisitor
    add: repo GetColumnCount
    add: repo GetColumnCountVisitor
    add: repo SendColumnEdit
    add: repo EditColumn
    add: repo DeleteColumn
    add: repo DeleteColumnCacheAndSearch

--- app/creation/service/internal/biz/column.go
+++ app/creation/service/internal/biz/column.go
@@ -1,5 +1,6 @@
 package biz
 
+import "C"
 import (
 	"context"
 	kerrors "github.com/go-kratos/kratos/v2/errors"
@@ -8,17 +9,38 @@ import (
 )
 
 type ColumnRepo interface {
-	GetLastColumnDraft(ctx context.Context, uuid string) (*ColumnDraft, error)
 	CreateColumnDraft(ctx context.Context, uuid string) (int32, error)
 	CreateColumnFolder(ctx context.Context, id int32, uuid string) error
-	CreateColumn(ctx context.Context, uuid, name, introduce, cover string, auth int32) (int32, error)
+	CreateColumn(ctx context.Context, id, auth int32, uuid string) error
 	CreateColumnStatistic(ctx context.Context, id, auth int32, uuid string) error
-	SendColumnToMq(ctx context.Context, column *Column, mode string) error
 	CreateColumnCache(ctx context.Context, id, auth int32, uuid string) error
 	CreateColumnSearch(ctx context.Context, id int32, uuid string) error
+
+	UpdateColumnCache(ctx context.Context, id, auth int32, uuid string) error
+	EditColumnCos(ctx context.Context, id int32, uuid string) error
+	EditColumnSearch(ctx context.Context, id int32, uuid string) error
+
+	DeleteColumnDraft(ctx context.Context, id int32, uuid string) error
+	DeleteColumnStatistic(ctx context.Context, id int32, uuid string) error
+	DeleteColumn(ctx context.Context, id int32, uuid string) error
+	DeleteColumnCache(ctx context.Context, id int32, uuid string) error
+	DeleteColumnSearch(ctx context.Context, id int32, uuid string) error
+
+	GetColumn(ctx context.Context, id int32) (*Column, error)
+	GetLastColumnDraft(ctx context.Context, uuid string) (*ColumnDraft, error)
 	GetColumnList(ctx context.Context, page int32) ([]*Column, error)
 	GetColumnListHot(ctx context.Context, page int32) ([]*ColumnStatistic, error)
+	GetUserColumnList(ctx context.Context, page int32, uuid string) ([]*Column, error)
+	GetUserColumnListVisitor(ctx context.Context, page int32, uuid string) ([]*Column, error)
+	GetColumnCount(ctx context.Context, uuid string) (int32, error)
+	GetColumnCountVisitor(ctx context.Context, uuid string) (int32, error)
 	GetColumnListStatistic(ctx context.Context, ids []int32) ([]*ColumnStatistic, error)
+
+	SendColumn(ctx context.Context, id int32, uuid string) (*ColumnDraft, error)
+	SendColumnToMq(ctx context.Context, column *Column, mode string) error
+	SendReviewToMq(ctx context.Context, review *ColumnReview) error
+
+	FreezeColumnCos(ctx context.Context, id int32, uuid string) error
 }
 
 type ColumnUseCase struct {
@@ -67,9 +89,54 @@ func (r *ColumnUseCase) CreateColumnDraft(ctx context.Context, uuid string) (int
 	return id, nil
 }
 
-func (r *ColumnUseCase) CreateColumn(ctx context.Context, uuid, name, introduce, cover string, auth int32) error {
+func (r *ColumnUseCase) SendColumn(ctx context.Context, id int32, uuid string) error {
 	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
-		id, err := r.repo.CreateColumn(ctx, uuid, name, introduce, cover, auth)
+		draft, err := r.repo.SendColumn(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorCreateColumnFailed("send column failed: %s", err.Error())
+		}
+		err = r.repo.SendReviewToMq(ctx, &ColumnReview{
+			Uuid: draft.Uuid,
+			Id:   draft.Id,
+			Mode: "create",
+		})
+		if err != nil {
+			return v1.ErrorCreateColumnFailed("send create review to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *ColumnUseCase) SendColumnEdit(ctx context.Context, id int32, uuid string) error {
+	column, err := r.repo.GetColumn(ctx, id)
+	if err != nil {
+		return v1.ErrorGetColumnFailed("get column failed: %s", err.Error())
+	}
+
+	if column.Uuid != uuid {
+		return v1.ErrorGetColumnFailed("send column edit failed: no auth")
+	}
+
+	err = r.repo.SendReviewToMq(ctx, &ColumnReview{
+		Uuid: column.Uuid,
+		Id:   column.ColumnId,
+		Mode: "edit",
+	})
+	if err != nil {
+		return v1.ErrorGetColumnFailed("send edit review to mq failed: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *ColumnUseCase) CreateColumn(ctx context.Context, id, auth int32, uuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		var err error
+		err = r.repo.DeleteColumnDraft(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorCreateColumnFailed("delete column draft failed: %s", err.Error())
+		}
+
+		err = r.repo.CreateColumn(ctx, id, auth, uuid)
 		if err != nil {
 			return v1.ErrorCreateColumnFailed("create column failed: %s", err.Error())
 		}
@@ -80,12 +147,48 @@ func (r *ColumnUseCase) CreateColumn(ctx context.Context, uuid, name, introduce,
 		}
 
 		err = r.repo.SendColumnToMq(ctx, &Column{
-			Id:   id,
-			Uuid: uuid,
-			Auth: auth,
+			ColumnId: id,
+			Uuid:     uuid,
+			Auth:     auth,
 		}, "create_column_cache_and_search")
 		if err != nil {
 			return v1.ErrorCreateColumnFailed("create column to mq failed: %s", err.Error())
+		}
+		return nil
+	})
+}
+
+func (r *ColumnUseCase) EditColumn(ctx context.Context, id, auth int32, uuid string) error {
+	err := r.repo.SendColumnToMq(ctx, &Column{
+		ColumnId: id,
+		Auth:     auth,
+		Uuid:     uuid,
+	}, "edit_column_cos_and_search")
+	if err != nil {
+		return v1.ErrorEditColumnFailed("edit column to mq failed: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *ColumnUseCase) DeleteColumn(ctx context.Context, id int32, uuid string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
+		var err error
+		err = r.repo.DeleteColumn(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorDeleteColumnFailed("delete column failed: %s", err.Error())
+		}
+
+		err = r.repo.DeleteColumnStatistic(ctx, id, uuid)
+		if err != nil {
+			return v1.ErrorDeleteColumnFailed("delete column statistic failed: %s", err.Error())
+		}
+
+		err = r.repo.SendColumnToMq(ctx, &Column{
+			ColumnId: id,
+			Uuid:     uuid,
+		}, "delete_column_cache_and_search")
+		if err != nil {
+			return v1.ErrorDeleteColumnFailed("delete column to mq failed: %s", err.Error())
 		}
 		return nil
 	})
@@ -108,6 +211,42 @@ func (r *ColumnUseCase) CreateColumnCacheAndSearch(ctx context.Context, id, auth
 	return nil
 }
 
+func (r *ColumnUseCase) EditColumnCosAndSearch(ctx context.Context, id, auth int32, uuid string) error {
+	err := r.repo.UpdateColumnCache(ctx, id, auth, uuid)
+	if err != nil {
+		return v1.ErrorEditColumnFailed("edit column cache failed: %s", err.Error())
+	}
+
+	err = r.repo.EditColumnCos(ctx, id, uuid)
+	if err != nil {
+		return v1.ErrorEditColumnFailed("edit column cache failed: %s", err.Error())
+	}
+
+	err = r.repo.EditColumnSearch(ctx, id, uuid)
+	if err != nil {
+		return v1.ErrorEditColumnFailed("edit column search failed: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *ColumnUseCase) DeleteColumnCacheAndSearch(ctx context.Context, id int32, uuid string) error {
+	err := r.repo.DeleteColumnCache(ctx, id, uuid)
+	if err != nil {
+		return v1.ErrorDeleteColumnFailed("delete column cache failed: %s", err.Error())
+	}
+
+	err = r.repo.FreezeColumnCos(ctx, id, uuid)
+	if err != nil {
+		return v1.ErrorDeleteColumnFailed("freeze column cos failed: %s", err.Error())
+	}
+
+	err = r.repo.DeleteColumnSearch(ctx, id, uuid)
+	if err != nil {
+		return v1.ErrorDeleteColumnFailed("delete column search failed: %s", err.Error())
+	}
+	return nil
+}
+
 func (r *ColumnUseCase) GetColumnList(ctx context.Context, page int32) ([]*Column, error) {
 	columnList, err := r.repo.GetColumnList(ctx, page)
 	if err != nil {
@@ -122,6 +261,38 @@ func (r *ColumnUseCase) GetColumnListHot(ctx context.Context, page int32) ([]*Co
 		return nil, v1.ErrorGetColumnListFailed("get column hot list failed: %s", err.Error())
 	}
 	return columnList, nil
+}
+
+func (r *ColumnUseCase) GetUserColumnList(ctx context.Context, page int32, uuid string) ([]*Column, error) {
+	columnList, err := r.repo.GetUserColumnList(ctx, page, uuid)
+	if err != nil {
+		return nil, v1.ErrorGetColumnListFailed("get user column list failed: %s", err.Error())
+	}
+	return columnList, nil
+}
+
+func (r *ColumnUseCase) GetUserColumnListVisitor(ctx context.Context, page int32, uuid string) ([]*Column, error) {
+	columnList, err := r.repo.GetUserColumnListVisitor(ctx, page, uuid)
+	if err != nil {
+		return nil, v1.ErrorGetColumnListFailed("get user column list visitor failed: %s", err.Error())
+	}
+	return columnList, nil
+}
+
+func (r *ColumnUseCase) GetColumnCount(ctx context.Context, uuid string) (int32, error) {
+	count, err := r.repo.GetColumnCount(ctx, uuid)
+	if err != nil {
+		return 0, v1.ErrorGetCountFailed("get column count failed: %s", err.Error())
+	}
+	return count, nil
+}
+
+func (r *ColumnUseCase) GetColumnCountVisitor(ctx context.Context, uuid string) (int32, error) {
+	count, err := r.repo.GetColumnCountVisitor(ctx, uuid)
+	if err != nil {
+		return 0, v1.ErrorGetCountFailed("get column count visitor failed: %s", err.Error())
+	}
+	return count, nil
 }
 
 func (r *ColumnUseCase) GetColumnListStatistic(ctx context.Context, ids []int32) ([]*ColumnStatistic, error) {
