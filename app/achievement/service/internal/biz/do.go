27b7ab6cf55b39c209ebf7f73f629b5c228e855e
liusiyuan <945212191@qq.com>
2022-07-24 23:36:32 +0800

    refactor: update

--- app/achievement/service/internal/biz/do.go
+++ app/achievement/service/internal/biz/do.go
@@ -1,28 +1,10 @@
 package biz
 
-type Article struct {
-	ArticleId int32
-	Uuid      string
-	Status    int32
-}
-
-type ArticleStatistic struct {
-	ArticleId int32
-	Uuid      string
-	Agree     int32
-	View      int32
-	Collect   int32
-	Comment   int32
-}
-
-type ArticleDraft struct {
-	Id     int32
-	Status int32
-	Uuid   string
-}
-
-type LeaderBoard struct {
-	Id   int32
-	Uuid string
-	Mode string
+type Achievement struct {
+	Uuid     string
+	Agree    int32
+	Collect  int32
+	View     int32
+	Follow   int32
+	Followed int32
 }
