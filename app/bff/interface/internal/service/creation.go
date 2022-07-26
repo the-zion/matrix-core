fad0f488a9a33e88dc1c8676da59abbc62ae21fd
liusiyuan <945212191@qq.com>
2022-07-26 23:52:27 +0800

    feat: add
    add: method GetNewsFromTianXing

--- app/bff/interface/internal/service/creation.go
+++ app/bff/interface/internal/service/creation.go
@@ -934,3 +934,23 @@ func (s *BffService) DeleteColumnIncludes(ctx context.Context, req *v1.DeleteCol
 	}
 	return &emptypb.Empty{}, nil
 }
+
+func (s *BffService) GetNewsFromTianXing(ctx context.Context, req *v1.GetNewsReq) (*v1.GetNewsReply, error) {
+	reply := &v1.GetNewsReply{News: make([]*v1.GetNewsReply_News, 0)}
+	newsList, err := s.nc.GetNewsFromTianXing(ctx, req.Page, req.Kind)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range newsList {
+		reply.News = append(reply.News, &v1.GetNewsReply_News{
+			Id:     item.Id,
+			Update: item.Update,
+			Title:  item.Title,
+			Text:   item.Text,
+			Tags:   item.Tags,
+			Cover:  item.Cover,
+			Url:    item.Url,
+		})
+	}
+	return reply, nil
+}
