5007dcb07e1c068b94a0d76627db9a7c14ded1ab
liusiyuan <945212191@qq.com>
2022-07-26 23:54:45 +0800

    refactor: update

--- app/bff/interface/internal/biz/biz.go
+++ app/bff/interface/internal/biz/biz.go
@@ -4,4 +4,4 @@ import (
 	"github.com/google/wire"
 )
 
-var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase, NewColumnUseCase, NewAchievementUseCase)
+var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase, NewColumnUseCase, NewAchievementUseCase, NewNewsUseCase)
