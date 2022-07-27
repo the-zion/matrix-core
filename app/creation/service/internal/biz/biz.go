8210aac2e3c6ffb1a1d2d27820b93ba7ae40ade1
liusiyuan <945212191@qq.com>
2022-07-27 23:57:54 +0800

    feat: add
    add: provider NewNewsUseCase

--- app/creation/service/internal/biz/biz.go
+++ app/creation/service/internal/biz/biz.go
@@ -5,7 +5,7 @@ import (
 	"github.com/google/wire"
 )
 
-var ProviderSet = wire.NewSet(NewArticleUseCase, NewTalkUseCase, NewCreationUseCase, NewColumnUseCase)
+var ProviderSet = wire.NewSet(NewArticleUseCase, NewTalkUseCase, NewCreationUseCase, NewColumnUseCase, NewNewsUseCase)
 
 type Transaction interface {
 	ExecTx(context.Context, func(ctx context.Context) error) error
