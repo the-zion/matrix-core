99da2430f52a9237a68ec6419c559b41b72cbb79
liusiyuan <945212191@qq.com>
2022-07-29 23:49:14 +0800

    feat: add method about follow
    add: method GetProfileList
    add: method GetUserFollow
    add: method GetUserFollows
    add: method GetFollowList
    add: method GetFollowListCount
    add: method GetFollowedList
    add: method GetFollowedListCount
    add: method SetUserFollow
    add: method CancelUserFollow
    add: method GetFollowedListCount

--- app/user/service/internal/service/user.go
+++ app/user/service/internal/service/user.go
@@ -41,6 +41,22 @@ func (s *UserService) GetProfile(ctx context.Context, req *v1.GetProfileReq) (*v
 	}, nil
 }
 
+func (s *UserService) GetProfileList(ctx context.Context, req *v1.GetProfileListReq) (*v1.GetProfileListReply, error) {
+	reply := &v1.GetProfileListReply{Profile: make([]*v1.GetProfileListReply_Profile, 0)}
+	profileList, err := s.uc.GetProfileList(ctx, req.Uuids)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range profileList {
+		reply.Profile = append(reply.Profile, &v1.GetProfileListReply_Profile{
+			Uuid:      item.Uuid,
+			Username:  item.Username,
+			Introduce: item.Introduce,
+		})
+	}
+	return reply, nil
+}
+
 func (s *UserService) GetProfileUpdate(ctx context.Context, req *v1.GetProfileUpdateReq) (*v1.GetProfileUpdateReply, error) {
 	profile, err := s.uc.GetProfileUpdate(ctx, req.Uuid)
 	if err != nil {
@@ -58,6 +74,79 @@ func (s *UserService) GetProfileUpdate(ctx context.Context, req *v1.GetProfileUp
 	}, nil
 }
 
+func (s *UserService) GetUserFollow(ctx context.Context, req *v1.GetUserFollowReq) (*v1.GetUserFollowReply, error) {
+	follow, err := s.uc.GetUserFollow(ctx, req.Uuid, req.UserUuid)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetUserFollowReply{
+		Follow: follow,
+	}, nil
+}
+
+func (s *UserService) GetFollowList(ctx context.Context, req *v1.GetFollowListReq) (*v1.GetFollowListReply, error) {
+	reply := &v1.GetFollowListReply{Follow: make([]*v1.GetFollowListReply_Follow, 0)}
+	followList, err := s.uc.GetFollowList(ctx, req.Page, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range followList {
+		reply.Follow = append(reply.Follow, &v1.GetFollowListReply_Follow{
+			Uuid: item.Follow,
+		})
+	}
+	return reply, nil
+}
+
+func (s *UserService) GetFollowListCount(ctx context.Context, req *v1.GetFollowListCountReq) (*v1.GetFollowListCountReply, error) {
+	count, err := s.uc.GetFollowListCount(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetFollowListCountReply{
+		Count: count,
+	}, nil
+}
+
+func (s *UserService) GetFollowedList(ctx context.Context, req *v1.GetFollowedListReq) (*v1.GetFollowedListReply, error) {
+	reply := &v1.GetFollowedListReply{Follow: make([]*v1.GetFollowedListReply_Follow, 0)}
+	followedList, err := s.uc.GetFollowedList(ctx, req.Page, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range followedList {
+		reply.Follow = append(reply.Follow, &v1.GetFollowedListReply_Follow{
+			Uuid: item.Followed,
+		})
+	}
+	return reply, nil
+}
+
+func (s *UserService) GetFollowedListCount(ctx context.Context, req *v1.GetFollowedListCountReq) (*v1.GetFollowedListCountReply, error) {
+	count, err := s.uc.GetFollowedListCount(ctx, req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetFollowedListCountReply{
+		Count: count,
+	}, nil
+}
+
+func (s *UserService) GetUserFollows(ctx context.Context, req *v1.GetUserFollowsReq) (*v1.GetUserFollowsReply, error) {
+	reply := &v1.GetUserFollowsReply{Follows: make([]*v1.GetUserFollowsReply_Follows, 0)}
+	followsList, err := s.uc.GetUserFollows(ctx, req.UserId, req.Uuids)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range followsList {
+		reply.Follows = append(reply.Follows, &v1.GetUserFollowsReply_Follows{
+			Uuid:        item.Uuid,
+			FollowJudge: item.Follow,
+		})
+	}
+	return reply, nil
+}
+
 func (s *UserService) SetProfileUpdate(ctx context.Context, req *v1.SetProfileUpdateReq) (*emptypb.Empty, error) {
 	profile := &biz.ProfileUpdate{}
 	profile.Uuid = req.Uuid
@@ -68,6 +157,22 @@ func (s *UserService) SetProfileUpdate(ctx context.Context, req *v1.SetProfileUp
 	profile.Homepage = req.Homepage
 	profile.Introduce = req.Introduce
 	err := s.uc.SetProfileUpdate(ctx, profile)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *UserService) SetUserFollow(ctx context.Context, req *v1.SetUserFollowReq) (*emptypb.Empty, error) {
+	err := s.uc.SetUserFollow(ctx, req.Uuid, req.UserUuid)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *UserService) CancelUserFollow(ctx context.Context, req *v1.CancelUserFollowReq) (*emptypb.Empty, error) {
+	err := s.uc.CancelUserFollow(ctx, req.Uuid, req.UserUuid)
 	if err != nil {
 		return nil, err
 	}
