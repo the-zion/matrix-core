f2c74a0bc09ae1ac74d28e338ef7cdecfdefbddb
liusiyuan <945212191@qq.com>
2022-07-29 23:50:32 +0800

    feat: add method to get real request ip

--- pkg/request/request.go
+++ pkg/request/request.go
@@ -11,6 +11,7 @@ func Server() middleware.Middleware {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			if header, ok := transport.FromServerContext(ctx); ok {
 				ctx = context.WithValue(ctx, "uuid", header.RequestHeader().Get("uuid"))
+				ctx = context.WithValue(ctx, "realIp", header.RequestHeader().Get("realIp"))
 			}
 			reply, err = handler(ctx, req)
 			return
