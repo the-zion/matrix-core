b15c427875fda064f990f380079b309366e850fe
liusiyuan <945212191@qq.com>
2022-08-04 23:29:06 +0800

    feat: add usercase about comment

---
+++ app/bff/interface/internal/biz/comment.go
@@ -0,0 +1,40 @@
+package biz
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type CommentRepo interface {
+	GetLastCommentDraft(ctx context.Context, uuid string) (*CommentDraft, error)
+	CreateCommentDraft(ctx context.Context, uuid string) (int32, error)
+	SendComment(ctx context.Context, id int32, uuid, ip string) error
+}
+
+type CommentUseCase struct {
+	repo CommentRepo
+	log  *log.Helper
+}
+
+func NewCommentUseCase(repo CommentRepo, logger log.Logger) *CommentUseCase {
+	return &CommentUseCase{
+		repo: repo,
+		log:  log.NewHelper(log.With(logger, "module", "bff/biz/CommentUseCase")),
+	}
+}
+
+func (r *CommentUseCase) CreateCommentDraft(ctx context.Context) (int32, error) {
+	uuid := ctx.Value("uuid").(string)
+	return r.repo.CreateCommentDraft(ctx, uuid)
+}
+
+func (r *CommentUseCase) GetLastCommentDraft(ctx context.Context) (*CommentDraft, error) {
+	uuid := ctx.Value("uuid").(string)
+	return r.repo.GetLastCommentDraft(ctx, uuid)
+}
+
+func (r *CommentUseCase) SendComment(ctx context.Context, id int32) error {
+	uuid := ctx.Value("uuid").(string)
+	ip := ctx.Value("realIp").(string)
+	return r.repo.SendComment(ctx, id, uuid, ip)
+}
