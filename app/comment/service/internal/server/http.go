0564ffc4bf574945cc2e264f4dabaa155a94395a
liusiyuan <945212191@qq.com>
2022-08-04 23:39:11 +0800

    feat: add server about comment

---
+++ app/comment/service/internal/server/http.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/the-zion/matrix-core/api/comment/service/v1"
+	"github.com/the-zion/matrix-core/app/comment/service/internal/conf"
+	"github.com/the-zion/matrix-core/app/comment/service/internal/service"
+)
+
+// NewHTTPServer new a HTTP user.
+func NewHTTPServer(c *conf.Server, commentService *service.CommentService, logger log.Logger) *http.Server {
+	var opts = []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			logging.Server(logger),
+		),
+	}
+	if c.Http.Network != "" {
+		opts = append(opts, http.Network(c.Http.Network))
+	}
+	if c.Http.Addr != "" {
+		opts = append(opts, http.Address(c.Http.Addr))
+	}
+	if c.Http.Timeout != nil {
+		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+	}
+	srv := http.NewServer(opts...)
+	v1.RegisterCommentHTTPServer(srv, commentService)
+	return srv
+}
