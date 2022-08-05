bba2f7ce1cefbac5d31786188e6ff0fb99af8e2f
liusiyuan <945212191@qq.com>
2022-08-05 23:58:05 +0800

    feat: add repo about comment list get

--- app/bff/interface/internal/biz/comment.go
+++ app/bff/interface/internal/biz/comment.go
@@ -7,6 +7,7 @@ import (
 
 type CommentRepo interface {
 	GetLastCommentDraft(ctx context.Context, uuid string) (*CommentDraft, error)
+	GetCommentList(ctx context.Context, page, creationId, creationType int32) ([]*Comment, error)
 	CreateCommentDraft(ctx context.Context, uuid string) (int32, error)
 	SendComment(ctx context.Context, id int32, uuid, ip string) error
 }
@@ -31,6 +32,10 @@ func (r *CommentUseCase) CreateCommentDraft(ctx context.Context) (int32, error) 
 func (r *CommentUseCase) GetLastCommentDraft(ctx context.Context) (*CommentDraft, error) {
 	uuid := ctx.Value("uuid").(string)
 	return r.repo.GetLastCommentDraft(ctx, uuid)
+}
+
+func (r *CommentUseCase) GetCommentList(ctx context.Context, page, creationId, creationType int32) ([]*Comment, error) {
+	return r.repo.GetCommentList(ctx, page, creationId, creationType)
 }
 
 func (r *CommentUseCase) SendComment(ctx context.Context, id int32) error {
