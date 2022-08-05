914cd27ebfb209e4a5135591a385302ee8fc0495
liusiyuan <945212191@qq.com>
2022-08-06 00:00:52 +0800

    feat: add method about comment get and create
    add: GetCommentList
    add: CreateComment
    add: CreateCommentCache
    add: SendCommentToMq
    add: DeleteCommentDraft

--- app/comment/service/internal/data/comment.go
+++ app/comment/service/internal/data/comment.go
@@ -7,6 +7,7 @@ import (
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	kerrors "github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
 	"github.com/the-zion/matrix-core/app/comment/service/internal/biz"
 	"gorm.io/gorm"
@@ -40,6 +41,76 @@ func (r *commentRepo) GetLastCommentDraft(ctx context.Context, uuid string) (*bi
 	return &biz.CommentDraft{
 		Id: int32(draft.ID),
 	}, nil
+}
+
+func (r *commentRepo) GetCommentList(ctx context.Context, page, creationId, creationType int32) ([]*biz.Comment, error) {
+	comment, err := r.getCommentFromCache(ctx, page, creationId, creationType)
+	if err != nil {
+		return nil, err
+	}
+
+	size := len(comment)
+	if size != 0 {
+		return comment, nil
+	}
+
+	comment, err = r.getCommentFromDB(ctx, page, creationId, creationType)
+	if err != nil {
+		return nil, err
+	}
+
+	size = len(comment)
+	if size != 0 {
+		go r.setTalkToCache("talk", talk)
+	}
+	return talk, nil
+}
+
+func (r *commentRepo) getCommentFromCache(ctx context.Context, page, creationId, creationType int32) ([]*biz.Comment, error) {
+	if page < 1 {
+		page = 1
+	}
+	index := int64(page - 1)
+	key := fmt.Sprintf("comment_%v_%v", creationId, creationType)
+	list, err := r.data.redisCli.ZRevRange(ctx, key, index*10, index+9).Result()
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("fail to get comment from cache: key(%s), page(%v)", key, page))
+	}
+
+	comment := make([]*biz.Comment, 0)
+	for _, item := range list {
+		member := strings.Split(item, "%")
+		id, err := strconv.ParseInt(member[0], 10, 32)
+		if err != nil {
+			return nil, errors.Wrapf(err, fmt.Sprintf("fail to covert string to int64: id(%s)", member[0]))
+		}
+		comment = append(comment, &biz.Comment{
+			CommentId: int32(id),
+			Uuid:      member[1],
+		})
+	}
+	return comment, nil
+}
+
+func (r *commentRepo) getCommentFromDB(ctx context.Context, page, creationId, creationType int32) ([]*biz.Comment, error) {
+	if page < 1 {
+		page = 1
+	}
+	index := int(page - 1)
+	list := make([]*Comment, 0)
+	err := r.data.db.WithContext(ctx).Where("creation_id = ? and creation_type = ?", creationId, creationType).Order("comment_id desc").Offset(index * 10).Limit(10).Find(&list).Error
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("fail to get comment from db: page(%v)", page))
+	}
+
+	comment := make([]*biz.Comment, 0)
+	for _, item := range list {
+		comment = append(comment, &biz.Comment{
+			CommentId: item.CommentId,
+			Uuid:      item.Uuid,
+		})
+	}
+	return comment, nil
 }
 
 func (r *commentRepo) CreateCommentDraft(ctx context.Context, uuid string) (int32, error) {
@@ -102,6 +173,76 @@ func (r *commentRepo) SendReviewToMq(ctx context.Context, review *biz.CommentRev
 	_, err = r.data.reviewMqPro.producer.SendSync(ctx, msg)
 	if err != nil {
 		return errors.Wrapf(err, fmt.Sprintf("fail to send review to mq: %v", err))
+	}
+	return nil
+}
+
+func (r *commentRepo) SendCommentToMq(ctx context.Context, comment *biz.Comment, mode string) error {
+	commentMap := map[string]interface{}{}
+	commentMap["uuid"] = comment.Uuid
+	commentMap["id"] = comment.CommentId
+	commentMap["creationId"] = comment.CreationId
+	commentMap["creationType"] = comment.CreationType
+	commentMap["mode"] = mode
+
+	data, err := json.Marshal(commentMap)
+	if err != nil {
+		return err
+	}
+	msg := &primitive.Message{
+		Topic: "comment",
+		Body:  data,
+	}
+	msg.WithKeys([]string{comment.Uuid})
+	_, err = r.data.commonMqPro.producer.SendSync(ctx, msg)
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to send comment to mq: %v", comment))
+	}
+	return nil
+}
+
+func (r *commentRepo) CreateComment(ctx context.Context, id, createId, createType int32, uuid string) error {
+	comment := &Comment{
+		CommentId:    id,
+		Uuid:         uuid,
+		CreationId:   createId,
+		CreationType: createType,
+	}
+	err := r.data.DB(ctx).Select("CommentId", "Uuid", "CreationId", "CreationType").Create(comment).Error
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to create a comment: id(%v), uuid(%s), creationId(%v), creationType(%v)", id, uuid, createId, createType))
+	}
+	return nil
+}
+
+func (r *commentRepo) CreateCommentCache(ctx context.Context, id, createId, createType int32, uuid string) error {
+	ids := strconv.Itoa(int(id))
+	createIds := strconv.Itoa(int(createId))
+	createTypes := strconv.Itoa(int(createType))
+	_, err := r.data.redisCli.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+		pipe.HSetNX(ctx, "comment_statistic", ids, 0)
+		pipe.ZAddNX(ctx, "comment_"+createIds+"_"+createTypes, &redis.Z{
+			Score:  float64(id),
+			Member: ids + "%" + uuid,
+		})
+		pipe.ZAddNX(ctx, "comment_"+createIds+"_"+createTypes+"_hot", &redis.Z{
+			Score:  0,
+			Member: ids + "%" + uuid,
+		})
+		return nil
+	})
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to create comment cache: id(%v), uuid(%s), creationId(%v), creationType(%v)", id, uuid, createId, createType))
+	}
+	return nil
+}
+
+func (r *commentRepo) DeleteCommentDraft(ctx context.Context, id int32, uuid string) error {
+	cd := &CommentDraft{}
+	cd.ID = uint(id)
+	err := r.data.DB(ctx).Where("uuid = ?", uuid).Delete(cd).Error
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to delete comment draft: uuid(%s), id(%v)", uuid, id))
 	}
 	return nil
 }
