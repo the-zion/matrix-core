11ce012f507c52c710cdb1ae030c97a244b75eeb
liusiyuan <945212191@qq.com>
2022-08-06 23:40:59 +0800

    feat: add repo about comment list get
    add: GetCommentListHot
    add: GetCommentListStatistic
    add: GetUserProfileList

--- app/bff/interface/internal/biz/comment.go
+++ app/bff/interface/internal/biz/comment.go
@@ -3,11 +3,15 @@ package biz
 import (
 	"context"
 	"github.com/go-kratos/kratos/v2/log"
+	"golang.org/x/sync/errgroup"
 )
 
 type CommentRepo interface {
 	GetLastCommentDraft(ctx context.Context, uuid string) (*CommentDraft, error)
 	GetCommentList(ctx context.Context, page, creationId, creationType int32) ([]*Comment, error)
+	GetCommentListHot(ctx context.Context, page, creationId, creationType int32) ([]*Comment, error)
+	GetCommentListStatistic(ctx context.Context, page, creationId, creationType int32, key string, commentList []*Comment) ([]*CommentStatistic, error)
+	GetUserProfileList(ctx context.Context, page, creationId, creationType int32, key string, commentList []*Comment) ([]*UserProfile, error)
 	CreateCommentDraft(ctx context.Context, uuid string) (int32, error)
 	SendComment(ctx context.Context, id int32, uuid, ip string) error
 }
@@ -35,7 +39,87 @@ func (r *CommentUseCase) GetLastCommentDraft(ctx context.Context) (*CommentDraft
 }
 
 func (r *CommentUseCase) GetCommentList(ctx context.Context, page, creationId, creationType int32) ([]*Comment, error) {
-	return r.repo.GetCommentList(ctx, page, creationId, creationType)
+	commentList, err := r.repo.GetCommentList(ctx, page, creationId, creationType)
+	if err != nil {
+		return nil, err
+	}
+	g, _ := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		commentListStatistic, err := r.repo.GetCommentListStatistic(ctx, page, creationId, creationType, "comment_statistic", commentList)
+		if err != nil {
+			return err
+		}
+		for _, item := range commentListStatistic {
+			for index, listItem := range commentList {
+				if listItem.Id == item.Id {
+					commentList[index].Agree = item.Agree
+					commentList[index].Comment = item.Comment
+				}
+			}
+		}
+		return nil
+	})
+	g.Go(func() error {
+		userProfileList, err := r.repo.GetUserProfileList(ctx, page, creationId, creationType, "comment_user_profile_list", commentList)
+		if err != nil {
+			return err
+		}
+		for _, item := range userProfileList {
+			for index, listItem := range commentList {
+				if listItem.Uuid == item.Uuid {
+					commentList[index].UserName = item.Username
+				}
+			}
+		}
+		return nil
+	})
+	err = g.Wait()
+	if err != nil {
+		return nil, err
+	}
+	return commentList, nil
+}
+
+func (r *CommentUseCase) GetCommentListHot(ctx context.Context, page, creationId, creationType int32) ([]*Comment, error) {
+	commentList, err := r.repo.GetCommentListHot(ctx, page, creationId, creationType)
+	if err != nil {
+		return nil, err
+	}
+	g, _ := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		commentListStatistic, err := r.repo.GetCommentListStatistic(ctx, page, creationId, creationType, "comment_statistic_hot", commentList)
+		if err != nil {
+			return err
+		}
+		for _, item := range commentListStatistic {
+			for index, listItem := range commentList {
+				if listItem.Id == item.Id {
+					commentList[index].Agree = item.Agree
+					commentList[index].Comment = item.Comment
+				}
+			}
+		}
+		return nil
+	})
+	g.Go(func() error {
+		userProfileList, err := r.repo.GetUserProfileList(ctx, page, creationId, creationType, "comment_user_profile_list_hot", commentList)
+		if err != nil {
+			return err
+		}
+		for _, item := range userProfileList {
+			for index, listItem := range commentList {
+				if listItem.Uuid == item.Uuid {
+					commentList[index].UserName = item.Username
+				}
+			}
+		}
+		return nil
+	})
+	err = g.Wait()
+	if err != nil {
+		return nil, err
+	}
+	return commentList, nil
 }
 
 func (r *CommentUseCase) SendComment(ctx context.Context, id int32) error {
