7a5caca8073000aa8cda192169983a868f93a2df
liusiyuan <945212191@qq.com>
2022-08-06 23:44:15 +0800

    feat: add and update DO object about comment
    add: CommentStatistic
    add: Comment
    update: CommentDraft

--- app/comment/service/internal/biz/do.go
+++ app/comment/service/internal/biz/do.go
@@ -1,11 +1,27 @@
 package biz
 
 type CommentDraft struct {
-	Id   int32
-	Uuid string
+	Id     int32
+	Uuid   string
+	Status int32
 }
 
 type CommentReview struct {
 	Uuid string
 	Id   int32
+}
+
+type Comment struct {
+	CommentId    int32
+	CreationId   int32
+	CreationType int32
+	Uuid         string
+	Agree        int32
+	Comment      int32
+}
+
+type CommentStatistic struct {
+	CommentId int32
+	Agree     int32
+	Comment   int32
 }
