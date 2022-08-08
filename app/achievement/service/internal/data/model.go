91ff19ca887db28e6c2349e316e46082904706fc
liusiyuan <945212191@qq.com>
2022-08-08 23:37:45 +0800

    refactor: update column type to unsigned

--- app/achievement/service/internal/data/model.go
+++ app/achievement/service/internal/data/model.go
@@ -2,9 +2,9 @@ package data
 
 type Achievement struct {
 	Uuid     string `gorm:"primaryKey;size:36"`
-	Agree    int32  `gorm:"default:0"`
-	Collect  int32  `gorm:"default:0"`
-	View     int32  `gorm:"default:0"`
-	Follow   int32  `gorm:"default:0"`
-	Followed int32  `gorm:"default:0"`
+	Agree    int32  `gorm:"type:int unsigned;default:0"`
+	Collect  int32  `gorm:"type:int unsigned;default:0"`
+	View     int32  `gorm:"type:int unsigned;default:0"`
+	Follow   int32  `gorm:"type:int unsigned;default:0"`
+	Followed int32  `gorm:"type:int unsigned;default:0"`
 }
