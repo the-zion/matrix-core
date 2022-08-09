d5dd180660aa00a13fdb21c318c8a44afcc6adc9
liusiyuan <945212191@qq.com>
2022-08-09 23:55:16 +0800

    feat: add new biz provider about comment

--- app/message/service/internal/biz/biz.go
+++ app/message/service/internal/biz/biz.go
@@ -4,4 +4,4 @@ import (
 	"github.com/google/wire"
 )
 
-var ProviderSet = wire.NewSet(NewUserUseCase, NewCreationUseCase, NewAchievementUseCase)
+var ProviderSet = wire.NewSet(NewUserUseCase, NewCreationUseCase, NewAchievementUseCase, NewCommentUseCase)
