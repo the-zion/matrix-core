192d583a4f27d9437caca8c0581d0b3727fc030b
liusiyuan <945212191@qq.com>
2022-08-11 23:17:30 +0800

    feat: add interface of recovery

--- app/bff/interface/internal/biz/biz.go
+++ app/bff/interface/internal/biz/biz.go
@@ -1,7 +1,12 @@
 package biz
 
 import (
+	"context"
 	"github.com/google/wire"
 )
 
 var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase, NewColumnUseCase, NewAchievementUseCase, NewNewsUseCase, NewCommentUseCase)
+
+type Recovery interface {
+	GroupRecover(context.Context, func(ctx context.Context) error) func() error
+}
