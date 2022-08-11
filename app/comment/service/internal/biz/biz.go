54f42139145e2661455f63f536b81a6c3fe62aa0
liusiyuan <945212191@qq.com>
2022-08-11 23:19:11 +0800

    feat: add interface of recovery

--- app/comment/service/internal/biz/biz.go
+++ app/comment/service/internal/biz/biz.go
@@ -10,3 +10,7 @@ var ProviderSet = wire.NewSet(NewCommentUseCase)
 type Transaction interface {
 	ExecTx(context.Context, func(ctx context.Context) error) error
 }
+
+type Recovery interface {
+	GroupRecover(context.Context, func(ctx context.Context) error) func() error
+}
