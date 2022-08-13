67b87e66ae4dcbe066098302994461f3fe95f230
liusiyuan <945212191@qq.com>
2022-08-13 23:13:07 +0800

    feat: add error about achievement score add and reduce

--- api/achievement/service/v1/achievement_error_errors.pb.go
+++ api/achievement/service/v1/achievement_error_errors.pb.go
@@ -130,3 +130,27 @@ func IsGetAchievementFailed(err error) bool {
 func ErrorGetAchievementFailed(format string, args ...interface{}) *errors.Error {
 	return errors.New(500, AchievementErrorReason_GET_ACHIEVEMENT_FAILED.String(), fmt.Sprintf(format, args...))
 }
+
+func IsAddAchievementScoreFailed(err error) bool {
+	if err == nil {
+		return false
+	}
+	e := errors.FromError(err)
+	return e.Reason == AchievementErrorReason_ADD_ACHIEVEMENT_SCORE_FAILED.String() && e.Code == 500
+}
+
+func ErrorAddAchievementScoreFailed(format string, args ...interface{}) *errors.Error {
+	return errors.New(500, AchievementErrorReason_ADD_ACHIEVEMENT_SCORE_FAILED.String(), fmt.Sprintf(format, args...))
+}
+
+func IsReduceAchievementScoreFailed(err error) bool {
+	if err == nil {
+		return false
+	}
+	e := errors.FromError(err)
+	return e.Reason == AchievementErrorReason_REDUCE_ACHIEVEMENT_SCORE_FAILED.String() && e.Code == 500
+}
+
+func ErrorReduceAchievementScoreFailed(format string, args ...interface{}) *errors.Error {
+	return errors.New(500, AchievementErrorReason_REDUCE_ACHIEVEMENT_SCORE_FAILED.String(), fmt.Sprintf(format, args...))
+}
