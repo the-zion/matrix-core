fc676204271d7535b9824d079c7829996d04b677
liusiyuan <945212191@qq.com>
2022-08-13 23:17:33 +0800

    refactor: update Achievement DO object

--- app/achievement/service/internal/data/model.go
+++ app/achievement/service/internal/data/model.go
@@ -2,6 +2,7 @@ package data
 
 type Achievement struct {
 	Uuid     string `gorm:"primaryKey;size:36"`
+	Score    int32  `gorm:"type:int unsigned;default:0"`
 	Agree    int32  `gorm:"type:int unsigned;default:0"`
 	Collect  int32  `gorm:"type:int unsigned;default:0"`
 	View     int32  `gorm:"type:int unsigned;default:0"`
