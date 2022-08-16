8c33e8d70d68989d8a74f256dcb813a90159bcd5
liusiyuan <945212191@qq.com>
2022-08-16 23:46:57 +0800

    feat: add recovery

--- app/message/service/internal/server/userMq.go
+++ app/message/service/internal/server/userMq.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/apache/rocketmq-client-go/v2"
 	"github.com/apache/rocketmq-client-go/v2/consumer"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
@@ -36,11 +37,12 @@ func NewCodeMqConsumerServer(conf *conf.Server, messageService *service.MessageS
 	err = c.Subscribe("code", consumer.MessageSelector{
 		Type:       consumer.TAG,
 		Expression: "phone || email",
-	}, func(ctx context.Context,
+	}, MqRecovery(func(ctx context.Context,
 		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+
 		messageService.SendCode(msgs...)
 		return consumer.ConsumeSuccess, nil
-	})
+	}))
 	if err != nil {
 		l.Fatalf("consumer subscribe error: %v", err)
 	}
@@ -86,7 +88,7 @@ func NewProfileMqConsumerServer(conf *conf.Server, messageService *service.Messa
 
 	delayLevel := 3
 	err = c.Subscribe("profile", consumer.MessageSelector{},
-		func(ctx context.Context,
+		MqRecovery(func(ctx context.Context,
 			msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
 
 			concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
@@ -98,7 +100,7 @@ func NewProfileMqConsumerServer(conf *conf.Server, messageService *service.Messa
 				return consumer.ConsumeRetryLater, nil
 			}
 			return consumer.ConsumeSuccess, nil
-		})
+		}))
 	if err != nil {
 		l.Fatalf("consumer subscribe error: %v", err)
 	}
@@ -115,5 +117,78 @@ func (s *ProfileMqConsumerServer) Start(ctx context.Context) error {
 
 func (s *ProfileMqConsumerServer) Stop(ctx context.Context) error {
 	log.Info("mq profile consumer closing")
+	return s.c.Shutdown()
+}
+
+type FollowMqConsumerServer struct {
+	c  rocketmq.PushConsumer
+	ms *service.MessageService
+}
+
+func NewFollowMqConsumerServer(conf *conf.Server, messageService *service.MessageService, logger log.Logger) *FollowMqConsumerServer {
+	l := log.NewHelper(log.With(logger, "server", "message/server/rocketmq-follow-consumer"))
+	c, err := rocketmq.NewPushConsumer(
+		consumer.WithGroupName(conf.UserMq.Follow.GroupName),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{conf.UserMq.ServerAddress})),
+		consumer.WithCredentials(primitive.Credentials{
+			SecretKey: conf.UserMq.SecretKey,
+			AccessKey: conf.UserMq.AccessKey,
+		}),
+		consumer.WithInstance("user"),
+		consumer.WithNamespace(conf.UserMq.NameSpace),
+		consumer.WithConsumeMessageBatchMaxSize(1),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset),
+		consumer.WithConsumerModel(consumer.Clustering),
+	)
+	if err != nil {
+		l.Fatalf("init consumer error: %v", err)
+	}
+
+	delayLevel := 3
+	err = c.Subscribe("follow", consumer.MessageSelector{},
+		MqRecovery(func(ctx context.Context,
+			msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+
+			concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
+			concurrentCtx.DelayLevelWhenNextConsume = delayLevel
+
+			var err error
+			m := map[string]interface{}{}
+			err = json.Unmarshal(msgs[0].Body, &m)
+			if err != nil {
+				l.Errorf("fail to unmarshal msg: %s", err.Error())
+				return consumer.ConsumeRetryLater, nil
+			}
+
+			mode := m["mode"].(string)
+			switch mode {
+			case "set_follow_db_and_cache":
+				err = messageService.SetFollowDbAndCache(ctx, m["uuid"].(string), m["userId"].(string))
+			case "cancel_follow_db_and_cache":
+				err = messageService.CancelFollowDbAndCache(ctx, m["uuid"].(string), m["userId"].(string))
+			}
+
+			if err != nil {
+				l.Error(err.Error())
+				return consumer.ConsumeRetryLater, nil
+			}
+			return consumer.ConsumeSuccess, nil
+		}))
+	if err != nil {
+		l.Fatalf("consumer subscribe error: %v", err)
+	}
+
+	return &FollowMqConsumerServer{
+		c: c,
+	}
+}
+
+func (s *FollowMqConsumerServer) Start(ctx context.Context) error {
+	log.Info("mq follow consumer starting")
+	return s.c.Start()
+}
+
+func (s *FollowMqConsumerServer) Stop(ctx context.Context) error {
+	log.Info("mq follow consumer closing")
 	return s.c.Shutdown()
 }
