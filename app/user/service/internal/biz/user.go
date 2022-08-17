8322f5dffb1900d546a58ee7676dc744208d5787
liusiyuan <945212191@qq.com>
2022-08-17 23:34:04 +0800

    feat: add method about search create and follow set

--- app/user/service/internal/biz/user.go
+++ app/user/service/internal/biz/user.go
@@ -5,6 +5,7 @@ import (
 	kerrors "github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 	v1 "github.com/the-zion/matrix-core/api/user/service/v1"
+	"golang.org/x/sync/errgroup"
 )
 
 type UserRepo interface {
@@ -12,16 +13,21 @@ type UserRepo interface {
 	GetProfile(ctx context.Context, uuid string) (*Profile, error)
 	GetProfileList(ctx context.Context, uuids []string) ([]*Profile, error)
 	GetUserFollow(ctx context.Context, uuid, userUuid string) (bool, error)
-	GetUserFollows(ctx context.Context, userId string, uuids []string) ([]*Follows, error)
+	GetUserFollows(ctx context.Context, uuid string) ([]string, error)
 	GetProfileUpdate(ctx context.Context, uuid string) (*ProfileUpdate, error)
 	GetFollowList(ctx context.Context, page int32, uuid string) ([]*Follow, error)
 	GetFollowListCount(ctx context.Context, uuid string) (int32, error)
 	GetFollowedList(ctx context.Context, page int32, uuid string) ([]*Follow, error)
 	GetFollowedListCount(ctx context.Context, uuid string) (int32, error)
+	GetUserSearch(ctx context.Context, page int32, search string) ([]*UserSearch, int32, error)
+	EditUserSearch(ctx context.Context, uuid string, profile *ProfileUpdate) error
 	SetProfile(ctx context.Context, profile *ProfileUpdate) error
 	SetProfileUpdate(ctx context.Context, profile *ProfileUpdate, status int32) (*ProfileUpdate, error)
 	SetUserFollow(ctx context.Context, uuid, userId string) error
+	SetUserFollowToCache(ctx context.Context, uuid, userId string) error
+	SetFollowToMq(ctx context.Context, follow *Follow, mode string) error
 	CancelUserFollow(ctx context.Context, uuid, userId string) error
+	CancelUserFollowFromCache(ctx context.Context, uuid, userId string) error
 	SendProfileToMq(ctx context.Context, profile *ProfileUpdate) error
 	SendUserStatisticToMq(ctx context.Context, uuid, userUuid, mode string) error
 	ModifyProfileUpdateStatus(ctx context.Context, uuid, update string) error
@@ -30,14 +36,16 @@ type UserRepo interface {
 type UserUseCase struct {
 	repo UserRepo
 	log  *log.Helper
+	re   Recovery
 	tm   Transaction
 }
 
-func NewUserUseCase(repo UserRepo, tm Transaction, logger log.Logger) *UserUseCase {
+func NewUserUseCase(repo UserRepo, re Recovery, tm Transaction, logger log.Logger) *UserUseCase {
 	return &UserUseCase{
 		repo: repo,
 		log:  log.NewHelper(log.With(logger, "module", "user/biz/userUseCase")),
 		tm:   tm,
+		re:   re,
 	}
 }
 
@@ -84,12 +92,24 @@ func (r *UserUseCase) GetUserFollow(ctx context.Context, uuid, userUuid string) 
 	return follow, nil
 }
 
-func (r *UserUseCase) GetUserFollows(ctx context.Context, userId string, uuids []string) ([]*Follows, error) {
-	follows, err := r.repo.GetUserFollows(ctx, userId, uuids)
+func (r *UserUseCase) GetUserFollows(ctx context.Context, uuid string) (map[string]bool, error) {
+	follows, err := r.repo.GetUserFollows(ctx, uuid)
 	if err != nil {
 		return nil, v1.ErrorGetUserFollowFailed("get user follows failed: %s", err.Error())
 	}
-	return follows, nil
+	followsMap := make(map[string]bool, 0)
+	for _, item := range follows {
+		followsMap[item] = true
+	}
+	return followsMap, nil
+}
+
+func (r *UserUseCase) GetUserSearch(ctx context.Context, page int32, search string) ([]*UserSearch, int32, error) {
+	userList, total, err := r.repo.GetUserSearch(ctx, page, search)
+	if err != nil {
+		return nil, 0, v1.ErrorGetUserSearchFailed("get user search failed: %s", err.Error())
+	}
+	return userList, total, nil
 }
 
 func (r *UserUseCase) GetFollowList(ctx context.Context, page int32, uuid string) ([]*Follow, error) {
@@ -146,14 +166,45 @@ func (r *UserUseCase) SetProfileUpdate(ctx context.Context, profile *ProfileUpda
 }
 
 func (r *UserUseCase) SetUserFollow(ctx context.Context, uuid, userId string) error {
-	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
-		if uuid == userId {
-			return v1.ErrorSetFollowFailed("uuid and userId are the same: %s")
+	if uuid == userId {
+		return v1.ErrorSetFollowFailed("uuid and userId are the same: %s")
+	}
+	g, _ := errgroup.WithContext(ctx)
+	g.Go(r.re.GroupRecover(ctx, func(ctx context.Context) error {
+		err := r.repo.SetUserFollowToCache(ctx, uuid, userId)
+		if err != nil {
+			return v1.ErrorSetFollowFailed("set follow to cache failed: %s", err.Error())
 		}
+		return nil
+	}))
+	g.Go(r.re.GroupRecover(ctx, func(ctx context.Context) error {
+		err := r.repo.SetFollowToMq(ctx, &Follow{
+			Follow:   uuid,
+			Followed: userId,
+		}, "set_follow_db_and_cache")
+		if err != nil {
+			return v1.ErrorSetFollowFailed("set follow failed: %s", err.Error())
+		}
+		return nil
+	}))
 
+	err := g.Wait()
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func (r *UserUseCase) SetFollowDbAndCache(ctx context.Context, uuid, userId string) error {
+	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
 		err := r.repo.SetUserFollow(ctx, uuid, userId)
 		if err != nil {
 			return v1.ErrorSetFollowFailed("set follow failed: %s", err.Error())
+		}
+
+		err = r.repo.SetUserFollowToCache(ctx, uuid, userId)
+		if err != nil {
+			return v1.ErrorSetFollowFailed("set follow to cache failed: %s", err.Error())
 		}
 
 		err = r.repo.SendUserStatisticToMq(ctx, uuid, userId, "follow")
@@ -166,8 +217,40 @@ func (r *UserUseCase) SetUserFollow(ctx context.Context, uuid, userId string) er
 }
 
 func (r *UserUseCase) CancelUserFollow(ctx context.Context, uuid, userId string) error {
+	g, _ := errgroup.WithContext(ctx)
+	g.Go(r.re.GroupRecover(ctx, func(ctx context.Context) error {
+		err := r.repo.CancelUserFollowFromCache(ctx, uuid, userId)
+		if err != nil {
+			return v1.ErrorCancelFollowFailed("cancel follow failed: %s", err.Error())
+		}
+		return nil
+	}))
+	g.Go(r.re.GroupRecover(ctx, func(ctx context.Context) error {
+		err := r.repo.SetFollowToMq(ctx, &Follow{
+			Follow:   uuid,
+			Followed: userId,
+		}, "cancel_follow_db_and_cache")
+		if err != nil {
+			return v1.ErrorSetFollowFailed("cancel follow failed: %s", err.Error())
+		}
+		return nil
+	}))
+
+	err := g.Wait()
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func (r *UserUseCase) CancelFollowDbAndCache(ctx context.Context, uuid, userId string) error {
 	return r.tm.ExecTx(ctx, func(ctx context.Context) error {
 		err := r.repo.CancelUserFollow(ctx, uuid, userId)
+		if err != nil {
+			return v1.ErrorCancelFollowFailed("cancel follow failed: %s", err.Error())
+		}
+
+		err = r.repo.CancelUserFollowFromCache(ctx, uuid, userId)
 		if err != nil {
 			return v1.ErrorCancelFollowFailed("cancel follow failed: %s", err.Error())
 		}
@@ -192,6 +275,10 @@ func (r *UserUseCase) ProfileReviewPass(ctx context.Context, uuid, update string
 			return err
 		}
 		err = r.repo.SetProfile(ctx, profile)
+		if err != nil {
+			return err
+		}
+		err = r.repo.EditUserSearch(ctx, uuid, profile)
 		if err != nil {
 			return err
 		}
