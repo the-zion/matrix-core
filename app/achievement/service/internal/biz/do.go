51e628d8158b77cdea7ae7ba48e66b477cd22551
liusiyuan <945212191@qq.com>
2022-08-18 23:22:25 +0800

    feat: add Do object about medal

--- app/achievement/service/internal/biz/do.go
+++ app/achievement/service/internal/biz/do.go
@@ -8,3 +8,32 @@ type Achievement struct {
 	Follow   int32
 	Followed int32
 }
+
+type Medal struct {
+	Creation1 int32
+	Creation2 int32
+	Creation3 int32
+	Creation4 int32
+	Creation5 int32
+	Creation6 int32
+	Creation7 int32
+	Agree1    int32
+	Agree2    int32
+	Agree3    int32
+	Agree4    int32
+	Agree5    int32
+	Agree6    int32
+	View1     int32
+	View2     int32
+	View3     int32
+	Comment1  int32
+	Comment2  int32
+	Comment3  int32
+	Collect1  int32
+	Collect2  int32
+	Collect3  int32
+}
+
+type Active struct {
+	Agree int32
+}
