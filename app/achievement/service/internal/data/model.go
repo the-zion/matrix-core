47d606e35eec1b355f27ab0c6d2d5efc10e8ea04
liusiyuan <945212191@qq.com>
2022-08-18 23:23:37 +0800

    feat: add model about "active"

--- app/achievement/service/internal/data/model.go
+++ app/achievement/service/internal/data/model.go
@@ -9,3 +9,34 @@ type Achievement struct {
 	Follow   int32  `gorm:"type:int unsigned;default:0"`
 	Followed int32  `gorm:"type:int unsigned;default:0"`
 }
+
+type Active struct {
+	Uuid  string `gorm:"primaryKey;size:36"`
+	Agree int32  `gorm:"type:int unsigned;default:0"`
+}
+
+type Medal struct {
+	Uuid      string `gorm:"primaryKey;size:36"`
+	Creation1 int32  `gorm:"type:int unsigned;default:0"`
+	Creation2 int32  `gorm:"type:int unsigned;default:0"`
+	Creation3 int32  `gorm:"type:int unsigned;default:0"`
+	Creation4 int32  `gorm:"type:int unsigned;default:0"`
+	Creation5 int32  `gorm:"type:int unsigned;default:0"`
+	Creation6 int32  `gorm:"type:int unsigned;default:0"`
+	Creation7 int32  `gorm:"type:int unsigned;default:0"`
+	Agree1    int32  `gorm:"type:int unsigned;default:0"`
+	Agree2    int32  `gorm:"type:int unsigned;default:0"`
+	Agree3    int32  `gorm:"type:int unsigned;default:0"`
+	Agree4    int32  `gorm:"type:int unsigned;default:0"`
+	Agree5    int32  `gorm:"type:int unsigned;default:0"`
+	Agree6    int32  `gorm:"type:int unsigned;default:0"`
+	View1     int32  `gorm:"type:int unsigned;default:0"`
+	View2     int32  `gorm:"type:int unsigned;default:0"`
+	View3     int32  `gorm:"type:int unsigned;default:0"`
+	Comment1  int32  `gorm:"type:int unsigned;default:0"`
+	Comment2  int32  `gorm:"type:int unsigned;default:0"`
+	Comment3  int32  `gorm:"type:int unsigned;default:0"`
+	Collect1  int32  `gorm:"type:int unsigned;default:0"`
+	Collect2  int32  `gorm:"type:int unsigned;default:0"`
+	Collect3  int32  `gorm:"type:int unsigned;default:0"`
+}
