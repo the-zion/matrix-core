7849bd0906f61df4a2d66c02a943e242c920db6f
liusiyuan <945212191@qq.com>
2022-08-19 23:47:00 +0800

    feat: add new column to Do object "CreationUser"

--- app/bff/interface/internal/biz/do.go
+++ app/bff/interface/internal/biz/do.go
@@ -289,6 +289,7 @@ type CreationUser struct {
 	Article     int32
 	Column      int32
 	Talk        int32
+	Collect     int32
 	Collections int32
 }
 
@@ -300,6 +301,45 @@ type Achievement struct {
 	View     int32
 	Follow   int32
 	Followed int32
+}
+
+type Active struct {
+	Agree int32
+}
+
+type Medal struct {
+	Creation1 int32
+	Creation2 int32
+	Creation3 int32
+	Creation4 int32
+	Creation5 int32
+	Creation6 int32
+	Creation7 int32
+	Agree1    int32
+	Agree2    int32
+	Agree3    int32
+	Agree4    int32
+	Agree5    int32
+	Agree6    int32
+	View1     int32
+	View2     int32
+	View3     int32
+	Comment1  int32
+	Comment2  int32
+	Comment3  int32
+	Collect1  int32
+	Collect2  int32
+	Collect3  int32
+}
+
+type MedalProgress struct {
+	Article     int32
+	Talk        int32
+	Agree       int32
+	ActiveAgree int32
+	View        int32
+	Comment     int32
+	Collect     int32
 }
 
 type Subscribe struct {
@@ -340,6 +380,10 @@ type SubComment struct {
 	UserName  string
 	ReplyName string
 	Agree     int32
+}
+
+type CommentUser struct {
+	Comment int32
 }
 
 type CommentStatistic struct {
