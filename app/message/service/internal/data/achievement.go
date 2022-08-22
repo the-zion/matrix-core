5396fb8f4c808d873ee8d5085e33999913eafec2
liusiyuan <945212191@qq.com>
2022-08-22 23:56:14 +0800

    feat: add repo about user medal get set cancel

--- app/message/service/internal/data/achievement.go
+++ app/message/service/internal/data/achievement.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"github.com/go-kratos/kratos/v2/log"
+	achievementV1 "github.com/the-zion/matrix-core/api/achievement/service/v1"
 	achievementv1 "github.com/the-zion/matrix-core/api/achievement/service/v1"
 	"github.com/the-zion/matrix-core/app/message/service/internal/biz"
 )
@@ -19,9 +20,10 @@ func NewAchievementRepo(data *Data, logger log.Logger) biz.AchievementRepo {
 	}
 }
 
-func (r *achievementRepo) SetAchievementAgree(ctx context.Context, uuid string) error {
+func (r *achievementRepo) SetAchievementAgree(ctx context.Context, uuid, userUuid string) error {
 	_, err := r.data.ac.SetAchievementAgree(ctx, &achievementv1.SetAchievementAgreeReq{
-		Uuid: uuid,
+		Uuid:     uuid,
+		UserUuid: userUuid,
 	})
 	if err != nil {
 		return err
@@ -29,9 +31,10 @@ func (r *achievementRepo) SetAchievementAgree(ctx context.Context, uuid string) 
 	return nil
 }
 
-func (r *achievementRepo) CancelAchievementAgree(ctx context.Context, uuid string) error {
+func (r *achievementRepo) CancelAchievementAgree(ctx context.Context, uuid, userUuid string) error {
 	_, err := r.data.ac.CancelAchievementAgree(ctx, &achievementv1.CancelAchievementAgreeReq{
-		Uuid: uuid,
+		Uuid:     uuid,
+		UserUuid: userUuid,
 	})
 	if err != nil {
 		return err
@@ -80,6 +83,17 @@ func (r *achievementRepo) SetAchievementFollow(ctx context.Context, follow, foll
 	return nil
 }
 
+func (r *achievementRepo) SetUserMedalDbAndCache(ctx context.Context, medal, uuid string) error {
+	_, err := r.data.ac.SetUserMedalDbAndCache(ctx, &achievementv1.SetUserMedalDbAndCacheReq{
+		Medal: medal,
+		Uuid:  uuid,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (r *achievementRepo) CancelAchievementFollow(ctx context.Context, follow, followed string) error {
 	_, err := r.data.ac.CancelAchievementFollow(ctx, &achievementv1.CancelAchievementFollowReq{
 		Follow:   follow,
@@ -91,10 +105,52 @@ func (r *achievementRepo) CancelAchievementFollow(ctx context.Context, follow, f
 	return nil
 }
 
+func (r *achievementRepo) CancelUserMedalDbAndCache(ctx context.Context, medal, uuid string) error {
+	_, err := r.data.ac.CancelUserMedalDbAndCache(ctx, &achievementv1.CancelUserMedalDbAndCacheReq{
+		Medal: medal,
+		Uuid:  uuid,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (r *achievementRepo) AddAchievementScore(ctx context.Context, uuid string, score int32) error {
 	_, err := r.data.ac.AddAchievementScore(ctx, &achievementv1.AddAchievementScoreReq{
 		Uuid:  uuid,
 		Score: score,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *achievementRepo) GetUserAchievement(ctx context.Context, uuid string) (int32, int32, error) {
+	achievement, err := r.data.ac.GetUserAchievement(ctx, &achievementV1.GetUserAchievementReq{
+		Uuid: uuid,
+	})
+	if err != nil {
+		return 0, 0, err
+	}
+	return achievement.Agree, achievement.View, nil
+}
+
+func (r *achievementRepo) GetUserActive(ctx context.Context, uuid string) (int32, error) {
+	active, err := r.data.ac.GetUserActive(ctx, &achievementV1.GetUserActiveReq{
+		Uuid: uuid,
+	})
+	if err != nil {
+		return 0, err
+	}
+	return active.Agree, nil
+}
+
+func (r *achievementRepo) AccessUserMedalDbAndCache(ctx context.Context, medal, uuid string) error {
+	_, err := r.data.ac.AccessUserMedalDbAndCache(ctx, &achievementv1.AccessUserMedalReq{
+		Medal: medal,
+		Uuid:  uuid,
 	})
 	if err != nil {
 		return err
