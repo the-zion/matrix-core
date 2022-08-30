9beed19b26b654c7e4f6d8a36baaffa5a8586126
liusiyuan <945212191@qq.com>
2022-08-30 18:41:59 +0800

    feat: add service discovery about creation

--- app/comment/service/internal/data/data.go
+++ app/comment/service/internal/data/data.go
@@ -5,11 +5,15 @@ import (
 	"github.com/apache/rocketmq-client-go/v2"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"github.com/apache/rocketmq-client-go/v2/producer"
+	"github.com/go-kratos/kratos/contrib/registry/nacos/v2"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-redis/redis/v8"
 	"github.com/google/wire"
 	"github.com/tencentyun/cos-go-sdk-v5"
 	_ "github.com/tencentyun/cos-go-sdk-v5"
+	creationv1 "github.com/the-zion/matrix-core/api/creation/service/v1"
 	"github.com/the-zion/matrix-core/app/comment/service/internal/biz"
 	"github.com/the-zion/matrix-core/app/comment/service/internal/conf"
 	"gorm.io/driver/mysql"
@@ -20,7 +24,7 @@ import (
 	"time"
 )
 
-var ProviderSet = wire.NewSet(NewData, NewDB, NewRedis, NewTransaction, NewCommentRepo, NewRocketmqCommentProducer, NewRocketmqReviewProducer, NewRocketmqAchievementProducer, NewCosServiceClient, NewRecovery)
+var ProviderSet = wire.NewSet(NewData, NewDB, NewRedis, NewTransaction, NewCommentRepo, NewRocketmqCommentProducer, NewRocketmqReviewProducer, NewRocketmqAchievementProducer, NewCosServiceClient, NewCreationServiceClient, NewRecovery)
 
 type ReviewMqPro struct {
 	producer rocketmq.Producer
@@ -39,6 +43,7 @@ type Data struct {
 	log              *log.Helper
 	cosCli           *cos.Client
 	redisCli         redis.Cmdable
+	cc               creationv1.CreationClient
 	reviewMqPro      *ReviewMqPro
 	commonMqPro      *CommentMqPro
 	achievementMqPro *AchievementMqPro
@@ -76,6 +81,20 @@ func (d *Data) GroupRecover(ctx context.Context, fn func(ctx context.Context) er
 			}
 		}()
 		return fn(ctx)
+	}
+}
+
+func (d *Data) Recover(ctx context.Context, fn func(ctx context.Context)) func() {
+	return func() {
+		defer func() {
+			if rerr := recover(); rerr != nil {
+				buf := make([]byte, 64<<10)
+				n := runtime.Stack(buf, false)
+				buf = buf[:n]
+				log.Context(ctx).Errorf("%v: %s\n", rerr, buf)
+			}
+		}()
+		fn(ctx)
 	}
 }
 
@@ -209,13 +228,32 @@ func NewRocketmqAchievementProducer(conf *conf.Data, logger log.Logger) *Achieve
 	}
 }
 
-func NewData(db *gorm.DB, cos *cos.Client, redisCmd redis.Cmdable, rm *ReviewMqPro, cm *CommentMqPro, ap *AchievementMqPro, logger log.Logger) (*Data, func(), error) {
+func NewCreationServiceClient(r *nacos.Registry, logger log.Logger) creationv1.CreationClient {
+	l := log.NewHelper(log.With(logger, "module", "message/data/new-creation-client"))
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("discovery:///matrix.creation.service.grpc"),
+		grpc.WithDiscovery(r),
+		grpc.WithMiddleware(
+			//tracing.Client(tracing.WithTracerProvider(tp)),
+			recovery.Recovery(),
+		),
+	)
+	if err != nil {
+		l.Fatalf(err.Error())
+	}
+	c := creationv1.NewCreationClient(conn)
+	return c
+}
+
+func NewData(db *gorm.DB, cos *cos.Client, redisCmd redis.Cmdable, rm *ReviewMqPro, cm *CommentMqPro, ap *AchievementMqPro, cc creationv1.CreationClient, logger log.Logger) (*Data, func(), error) {
 	l := log.NewHelper(log.With(logger, "module", "comment/data/new-data"))
 
 	d := &Data{
 		db:               db,
 		redisCli:         redisCmd,
 		cosCli:           cos,
+		cc:               cc,
 		reviewMqPro:      rm,
 		achievementMqPro: ap,
 		commonMqPro:      cm,
