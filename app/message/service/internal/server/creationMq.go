e592fec8e9e27b1ed99d0a49dc3c989bdd2b23fe
liusiyuan <945212191@qq.com>
2022-08-31 18:51:06 +0800

    feat: add method about article agree/view db and cache set

--- app/message/service/internal/server/creationMq.go
+++ app/message/service/internal/server/creationMq.go
@@ -130,6 +130,16 @@ func NewArticleMqConsumerServer(conf *conf.Server, messageService *service.Messa
 			err = messageService.EditArticleCosAndSearch(ctx, int32(m["id"].(float64)), int32(m["auth"].(float64)), m["uuid"].(string))
 		case "delete_article_cache_and_search":
 			err = messageService.DeleteArticleCacheAndSearch(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "set_article_view_db_and_cache":
+			err = messageService.SetArticleViewDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "set_article_agree_db_and_cache":
+			err = messageService.SetArticleAgreeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "cancel_article_agree_db_and_cache":
+			err = messageService.CancelArticleAgreeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "set_article_collect_db_and_cache":
+			err = messageService.SetArticleCollectDbAndCache(ctx, int32(m["id"].(float64)), int32(m["collectionsId"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "cancel_article_collect_db_and_cache":
+			err = messageService.CancelArticleCollectDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
 		}
 
 		if err != nil {
@@ -276,6 +286,16 @@ func NewTalkMqConsumerServer(conf *conf.Server, messageService *service.MessageS
 			err = messageService.EditTalkCosAndSearch(ctx, int32(m["id"].(float64)), int32(m["auth"].(float64)), m["uuid"].(string))
 		case "delete_talk_cache_and_search":
 			err = messageService.DeleteTalkCacheAndSearch(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "set_talk_view_db_and_cache":
+			err = messageService.SetTalkViewDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "set_talk_agree_db_and_cache":
+			err = messageService.SetTalkAgreeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "cancel_talk_agree_db_and_cache":
+			err = messageService.CancelTalkAgreeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "set_talk_collect_db_and_cache":
+			err = messageService.SetTalkCollectDbAndCache(ctx, int32(m["id"].(float64)), int32(m["collectionsId"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "cancel_talk_collect_db_and_cache":
+			err = messageService.CancelTalkCollectDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
 		}
 
 		if err != nil {
@@ -366,12 +386,12 @@ func NewColumnReviewMqConsumerServer(conf *conf.Server, messageService *service.
 }
 
 func (s *ColumnReviewMqConsumerServer) Start(_ context.Context) error {
-	log.Info("mq talk review consumer starting")
+	log.Info("mq column review consumer starting")
 	return s.c.Start()
 }
 
 func (s *ColumnReviewMqConsumerServer) Stop(_ context.Context) error {
-	log.Info("mq talk review consumer closing")
+	log.Info("mq column review consumer closing")
 	return s.c.Shutdown()
 }
 
@@ -422,6 +442,24 @@ func NewColumnMqConsumerServer(conf *conf.Server, messageService *service.Messag
 			err = messageService.EditColumnCosAndSearch(ctx, int32(m["id"].(float64)), int32(m["auth"].(float64)), m["uuid"].(string))
 		case "delete_column_cache_and_search":
 			err = messageService.DeleteColumnCacheAndSearch(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "set_column_view_db_and_cache":
+			err = messageService.SetColumnViewDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "set_column_agree_db_and_cache":
+			err = messageService.SetColumnAgreeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "cancel_column_agree_db_and_cache":
+			err = messageService.CancelColumnAgreeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "set_column_collect_db_and_cache":
+			err = messageService.SetColumnCollectDbAndCache(ctx, int32(m["id"].(float64)), int32(m["collectionsId"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "cancel_column_collect_db_and_cache":
+			err = messageService.CancelColumnCollectDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string), m["userUuid"].(string))
+		case "add_column_includes_db_and_cache":
+			err = messageService.AddColumnIncludesDbAndCache(ctx, int32(m["id"].(float64)), int32(m["articleId"].(float64)), m["uuid"].(string))
+		case "delete_column_includes_db_and_cache":
+			err = messageService.DeleteColumnIncludesDbAndCache(ctx, int32(m["id"].(float64)), int32(m["articleId"].(float64)), m["uuid"].(string))
+		case "set_column_subscribe_db_and_cache":
+			err = messageService.SetColumnSubscribeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		case "cancel_column_subscribe_db_and_cache":
+			err = messageService.CancelColumnSubscribeDbAndCache(ctx, int32(m["id"].(float64)), m["uuid"].(string))
 		}
 
 		if err != nil {
@@ -445,6 +483,152 @@ func (s *ColumnMqConsumerServer) Start(_ context.Context) error {
 }
 
 func (s *ColumnMqConsumerServer) Stop(_ context.Context) error {
+	log.Info("mq column consumer closing")
+	return s.c.Shutdown()
+}
+
+type CollectionsReviewMqConsumerServer struct {
+	c  rocketmq.PushConsumer
+	ms *service.MessageService
+}
+
+func NewCollectionsReviewMqConsumerServer(conf *conf.Server, messageService *service.MessageService, logger log.Logger) *CollectionsReviewMqConsumerServer {
+	l := log.NewHelper(log.With(logger, "server", "message/server/rocketmq-collections-review-consumer"))
+	c, err := rocketmq.NewPushConsumer(
+		consumer.WithGroupName(conf.CreationMq.CollectionsReview.GroupName),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{conf.CreationMq.ServerAddress})),
+		consumer.WithCredentials(primitive.Credentials{
+			SecretKey: conf.CreationMq.SecretKey,
+			AccessKey: conf.CreationMq.AccessKey,
+		}),
+		consumer.WithInstance("creation"),
+		consumer.WithConsumeMessageBatchMaxSize(1),
+		consumer.WithNamespace(conf.CreationMq.NameSpace),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset),
+		consumer.WithConsumerModel(consumer.Clustering),
+	)
+	if err != nil {
+		l.Fatalf("init consumer error: %v", err)
+	}
+
+	delayLevel := 3
+	err = c.Subscribe("collections_review", consumer.MessageSelector{}, MqRecovery(func(ctx context.Context,
+		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+
+		concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
+		concurrentCtx.DelayLevelWhenNextConsume = delayLevel
+
+		var err error
+		m := map[string]interface{}{}
+		err = json.Unmarshal(msgs[0].Body, &m)
+		if err != nil {
+			l.Errorf("fail to unmarshal msg: %s", err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+
+		mode := m["Mode"].(string)
+		switch mode {
+		case "create":
+			err = messageService.ToReviewCreateCollections(int32(m["Id"].(float64)), m["Uuid"].(string))
+		case "edit":
+			err = messageService.ToReviewEditCollections(int32(m["Id"].(float64)), m["Uuid"].(string))
+		}
+
+		if err != nil {
+			l.Error(err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+		return consumer.ConsumeSuccess, nil
+	}))
+	if err != nil {
+		l.Fatalf("consumer subscribe error: %v", err)
+	}
+
+	return &CollectionsReviewMqConsumerServer{
+		c: c,
+	}
+}
+
+func (s *CollectionsReviewMqConsumerServer) Start(_ context.Context) error {
+	log.Info("mq collections review consumer starting")
+	return s.c.Start()
+}
+
+func (s *CollectionsReviewMqConsumerServer) Stop(_ context.Context) error {
+	log.Info("mq collections review consumer closing")
+	return s.c.Shutdown()
+}
+
+type CollectionsMqConsumerServer struct {
+	c  rocketmq.PushConsumer
+	ms *service.MessageService
+}
+
+func NewCollectionsMqConsumerServer(conf *conf.Server, messageService *service.MessageService, logger log.Logger) *CollectionsMqConsumerServer {
+	l := log.NewHelper(log.With(logger, "server", "message/server/rocketmq-collections-consumer"))
+	c, err := rocketmq.NewPushConsumer(
+		consumer.WithGroupName(conf.CreationMq.Collections.GroupName),
+		consumer.WithNsResolver(primitive.NewPassthroughResolver([]string{conf.CreationMq.ServerAddress})),
+		consumer.WithCredentials(primitive.Credentials{
+			SecretKey: conf.CreationMq.SecretKey,
+			AccessKey: conf.CreationMq.AccessKey,
+		}),
+		consumer.WithInstance("creation"),
+		consumer.WithConsumeMessageBatchMaxSize(1),
+		consumer.WithNamespace(conf.CreationMq.NameSpace),
+		consumer.WithConsumeFromWhere(consumer.ConsumeFromFirstOffset),
+		consumer.WithConsumerModel(consumer.Clustering),
+	)
+	if err != nil {
+		l.Fatalf("init consumer error: %v", err)
+	}
+
+	delayLevel := 3
+	err = c.Subscribe("collections", consumer.MessageSelector{}, MqRecovery(func(ctx context.Context,
+		msgs ...*primitive.MessageExt) (consumer.ConsumeResult, error) {
+
+		concurrentCtx, _ := primitive.GetConcurrentlyCtx(ctx)
+		concurrentCtx.DelayLevelWhenNextConsume = delayLevel
+
+		var err error
+		m := map[string]interface{}{}
+		err = json.Unmarshal(msgs[0].Body, &m)
+		if err != nil {
+			l.Errorf("fail to unmarshal msg: %s", err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+
+		mode := m["mode"].(string)
+		switch mode {
+		case "create_collections_db_and_cache":
+			err = messageService.CreateCollectionsDbAndCache(ctx, int32(m["id"].(float64)), int32(m["auth"].(float64)), m["uuid"].(string))
+		case "edit_collections_cos":
+			err = messageService.EditCollectionsCos(ctx, int32(m["id"].(float64)), int32(m["auth"].(float64)), m["uuid"].(string))
+		case "delete_collections_cache":
+			err = messageService.DeleteCollectionsCache(ctx, int32(m["id"].(float64)), m["uuid"].(string))
+		}
+
+		if err != nil {
+			l.Error(err.Error())
+			return consumer.ConsumeRetryLater, nil
+		}
+		return consumer.ConsumeSuccess, nil
+	}))
+	if err != nil {
+		l.Fatalf("consumer subscribe error: %v", err)
+	}
+
+	return &CollectionsMqConsumerServer{
+		c: c,
+	}
+}
+
+func (s *CollectionsMqConsumerServer) Start(_ context.Context) error {
+	log.Info("mq column consumer starting")
+	return s.c.Start()
+}
+
+func (s *CollectionsMqConsumerServer) Stop(_ context.Context) error {
 	log.Info("mq column consumer closing")
 	return s.c.Shutdown()
 }
