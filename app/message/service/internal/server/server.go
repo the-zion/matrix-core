1234a426778899ca86bb09643d7692cd982ac59f
liusiyuan <945212191@qq.com>
2022-09-01 18:41:50 +0800

    feat: add collection consumer server

--- app/message/service/internal/server/server.go
+++ app/message/service/internal/server/server.go
@@ -6,4 +6,4 @@ import (
 
 // ProviderSet is user providers.
 var ProviderSet = wire.NewSet(NewHTTPServer, NewGRPCServer, NewCodeMqConsumerServer, NewProfileMqConsumerServer, NewArticleReviewMqConsumerServer, NewArticleMqConsumerServer, NewTalkReviewMqConsumerServer, NewTalkMqConsumerServer, NewColumnReviewMqConsumerServer, NewColumnMqConsumerServer,
-	NewAchievementMqConsumerServer, NewCommentReviewMqConsumerServer, NewCommentMqConsumerServer, NewFollowMqConsumerServer)
+	NewAchievementMqConsumerServer, NewCommentReviewMqConsumerServer, NewCommentMqConsumerServer, NewFollowMqConsumerServer, NewCollectionsReviewMqConsumerServer, NewCollectionsMqConsumerServer)
