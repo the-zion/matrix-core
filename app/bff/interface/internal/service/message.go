d95175d7e6f3a727ebc1e2114ea833fd27633b68
liusiyuan <945212191@qq.com>
2022-10-08 18:36:10 +0800

    feat: add method about message notification get

--- app/bff/interface/internal/service/message.go
+++ app/bff/interface/internal/service/message.go
@@ -6,6 +6,19 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+func (s *BffService) GetMessageNotification(ctx context.Context, _ *emptypb.Empty) (*v1.GetMessageNotificationReply, error) {
+	notification, err := s.mc.GetMessageNotification(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetMessageNotificationReply{
+		Timeline:   notification.Timeline,
+		Comment:    notification.Comment,
+		SubComment: notification.SubComment,
+		System:     notification.SystemNotification,
+	}, nil
+}
+
 func (s *BffService) GetMailBoxLastTime(ctx context.Context, _ *emptypb.Empty) (*v1.GetMailBoxLastTimeReply, error) {
 	mailbox, err := s.mc.GetMailBoxLastTime(ctx)
 	if err != nil {
@@ -16,8 +29,8 @@ func (s *BffService) GetMailBoxLastTime(ctx context.Context, _ *emptypb.Empty) (
 	}, nil
 }
 
-func (s *BffService) SetMailBoxLastTime(ctx context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
-	err := s.mc.SetMailBoxLastTime(ctx)
+func (s *BffService) SetMailBoxLastTime(ctx context.Context, req *v1.SetMailBoxLastTimeReq) (*emptypb.Empty, error) {
+	err := s.mc.SetMailBoxLastTime(ctx, req.Time)
 	if err != nil {
 		return nil, err
 	}
