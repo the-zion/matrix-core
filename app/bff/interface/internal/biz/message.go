210eac076de2985fb7eabe7f3d9cf72c9c3c8fc6
liusiyuan <945212191@qq.com>
2022-10-08 18:35:54 +0800

    feat: add method about message notification get

--- app/bff/interface/internal/biz/message.go
+++ app/bff/interface/internal/biz/message.go
@@ -7,21 +7,37 @@ import (
 
 type MessageRepo interface {
 	GetMailBoxLastTime(ctx context.Context, uuid string) (*MailBox, error)
-	SetMailBoxLastTime(ctx context.Context, uuid string) error
+	GetMessageNotification(ctx context.Context, uuid string, follows []string) (*Notification, error)
+	SetMailBoxLastTime(ctx context.Context, uuid string, time int32) error
 }
 
 type MessageUseCase struct {
-	repo MessageRepo
-	re   Recovery
-	log  *log.Helper
+	repo     MessageRepo
+	userRepo UserRepo
+	re       Recovery
+	log      *log.Helper
 }
 
-func NewMessageUseCase(repo MessageRepo, re Recovery, logger log.Logger) *MessageUseCase {
+func NewMessageUseCase(repo MessageRepo, userRepo UserRepo, re Recovery, logger log.Logger) *MessageUseCase {
 	return &MessageUseCase{
-		repo: repo,
-		re:   re,
-		log:  log.NewHelper(log.With(logger, "module", "bff/biz/MessageUseCase")),
+		repo:     repo,
+		userRepo: userRepo,
+		re:       re,
+		log:      log.NewHelper(log.With(logger, "module", "bff/biz/MessageUseCase")),
 	}
+}
+
+func (r *MessageUseCase) GetMessageNotification(ctx context.Context) (*Notification, error) {
+	uuid := ctx.Value("uuid").(string)
+	uuidMap, err := r.userRepo.GetUserFollows(ctx, uuid)
+	if err != nil {
+		return nil, err
+	}
+	follows := make([]string, 0)
+	for key := range uuidMap {
+		follows = append(follows, key)
+	}
+	return r.repo.GetMessageNotification(ctx, uuid, follows)
 }
 
 func (r *MessageUseCase) GetMailBoxLastTime(ctx context.Context) (*MailBox, error) {
@@ -29,7 +45,7 @@ func (r *MessageUseCase) GetMailBoxLastTime(ctx context.Context) (*MailBox, erro
 	return r.repo.GetMailBoxLastTime(ctx, uuid)
 }
 
-func (r *MessageUseCase) SetMailBoxLastTime(ctx context.Context) error {
+func (r *MessageUseCase) SetMailBoxLastTime(ctx context.Context, time int32) error {
 	uuid := ctx.Value("uuid").(string)
-	return r.repo.SetMailBoxLastTime(ctx, uuid)
+	return r.repo.SetMailBoxLastTime(ctx, uuid, time)
 }
