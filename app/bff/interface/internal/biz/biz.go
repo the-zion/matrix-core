cbd5f310adc182722d5cb76806dfdc4cbf324713
liusiyuan <945212191@qq.com>
2022-10-08 18:39:58 +0800

    feat: add message use case

--- app/bff/interface/internal/biz/biz.go
+++ app/bff/interface/internal/biz/biz.go
@@ -5,7 +5,7 @@ import (
 	"github.com/google/wire"
 )
 
-var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase, NewColumnUseCase, NewAchievementUseCase, NewNewsUseCase, NewCommentUseCase)
+var ProviderSet = wire.NewSet(NewUserUseCase, NewArticleUseCase, NewCreationUseCase, NewTalkUseCase, NewColumnUseCase, NewAchievementUseCase, NewNewsUseCase, NewCommentUseCase, NewMessageUseCase)
 
 type Recovery interface {
 	GroupRecover(context.Context, func(ctx context.Context) error) func() error
