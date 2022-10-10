4f06420d2d634c5358501c3411114be168c812f0
liusiyuan <945212191@qq.com>
2022-10-10 18:27:48 +0800

    feat: add method about mailbox comment count remove

--- app/bff/interface/internal/service/message.go
+++ app/bff/interface/internal/service/message.go
@@ -29,8 +29,47 @@ func (s *BffService) GetMailBoxLastTime(ctx context.Context, _ *emptypb.Empty) (
 	}, nil
 }
 
+func (s *BffService) GetMessageSystemNotification(ctx context.Context, req *v1.GetMessageSystemNotificationReq) (*v1.GetMessageSystemNotificationReply, error) {
+	reply := &v1.GetMessageSystemNotificationReply{List: make([]*v1.GetMessageSystemNotificationReply_List, 0)}
+	notificationList, err := s.mc.GetMessageSystemNotification(ctx, req.Page)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range notificationList {
+		reply.List = append(reply.List, &v1.GetMessageSystemNotificationReply_List{
+			Id:               item.Id,
+			ContentId:        item.ContentId,
+			CreatedAt:        item.CreatedAt,
+			NotificationType: item.NotificationType,
+			Title:            item.Title,
+			Uuid:             item.Uuid,
+			Label:            item.Label,
+			Result:           item.Result,
+			Section:          item.Section,
+			Text:             item.Text,
+		})
+	}
+	return reply, nil
+}
+
 func (s *BffService) SetMailBoxLastTime(ctx context.Context, req *v1.SetMailBoxLastTimeReq) (*emptypb.Empty, error) {
 	err := s.mc.SetMailBoxLastTime(ctx, req.Time)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *BffService) RemoveMailBoxCommentCount(ctx context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
+	err := s.mc.RemoveMailBoxCommentCount(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *BffService) RemoveMailBoxSubCommentCount(ctx context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
+	err := s.mc.RemoveMailBoxSubCommentCount(ctx)
 	if err != nil {
 		return nil, err
 	}
