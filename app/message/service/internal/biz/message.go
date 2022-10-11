43f3769d489d52ed59eadfeebd77bfec37f3cb5c
liusiyuan <945212191@qq.com>
2022-10-11 18:13:18 +0800

    feat: add method about mailbox system notification  get/remove

--- app/message/service/internal/biz/message.go
+++ app/message/service/internal/biz/message.go
@@ -9,7 +9,13 @@ import (
 type MessageRepo interface {
 	GetMailBoxLastTime(ctx context.Context, uuid string) (*MailBox, error)
 	GetMessageNotification(ctx context.Context, uuid string, follows []string) (*Notification, error)
+	GetMessageSystemNotification(ctx context.Context, page int32, uuid string) ([]*SystemNotification, error)
 	SetMailBoxLastTime(ctx context.Context, uuid string, time int32) error
+	RemoveMailBoxCommentCount(ctx context.Context, uuid string) error
+	RemoveMailBoxSubCommentCount(ctx context.Context, uuid string) error
+	RemoveMailBoxSystemNotificationCount(ctx context.Context, uuid string) error
+	AddMailBoxSystemNotification(ctx context.Context, contentId int32, notificationType string, title string, uuid string, label string, result int32, section string, text string, uid string) (*SystemNotification, error)
+	AddMailBoxSystemNotificationToCache(ctx context.Context, notification *SystemNotification) error
 }
 
 type MessageUseCase struct {
@@ -42,10 +48,42 @@ func (r *MessageUseCase) GetMessageNotification(ctx context.Context, uuid string
 	return mailbox, nil
 }
 
+func (r *MessageUseCase) GetMessageSystemNotification(ctx context.Context, page int32, uuid string) ([]*SystemNotification, error) {
+	notificationList, err := r.repo.GetMessageSystemNotification(ctx, page, uuid)
+	if err != nil {
+		return nil, v1.ErrorGetMessageNotificationFailed("get message system notification failed: %s", err.Error())
+	}
+	return notificationList, nil
+}
+
 func (r *MessageUseCase) SetMailBoxLastTime(ctx context.Context, uuid string, time int32) error {
 	err := r.repo.SetMailBoxLastTime(ctx, uuid, time)
 	if err != nil {
 		return v1.ErrorSetMailboxLastTimeFailed("set mailbox last time failed: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *MessageUseCase) RemoveMailBoxCommentCount(ctx context.Context, uuid string) error {
+	err := r.repo.RemoveMailBoxCommentCount(ctx, uuid)
+	if err != nil {
+		return v1.ErrorRemoveMailboxCommentFailed("remove mail box comment count: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *MessageUseCase) RemoveMailBoxSubCommentCount(ctx context.Context, uuid string) error {
+	err := r.repo.RemoveMailBoxSubCommentCount(ctx, uuid)
+	if err != nil {
+		return v1.ErrorRemoveMailboxCommentFailed("remove mail box sub comment count: %s", err.Error())
+	}
+	return nil
+}
+
+func (r *MessageUseCase) RemoveMailBoxSystemNotificationCount(ctx context.Context, uuid string) error {
+	err := r.repo.RemoveMailBoxSystemNotificationCount(ctx, uuid)
+	if err != nil {
+		return v1.ErrorRemoveMailboxSystemNotificationFailed("remove mail box system notification failed: %s", err.Error())
 	}
 	return nil
 }
