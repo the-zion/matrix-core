23d62d4bfa72cb53bb6baf52f65d4d90a333e860
liusiyuan <945212191@qq.com>
2022-10-11 18:13:35 +0800

    feat: add method about mailbox system notification  get/remove

--- app/message/service/internal/data/message.go
+++ app/message/service/internal/data/message.go
@@ -2,12 +2,14 @@ package data
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-redis/redis/v8"
 	"github.com/pkg/errors"
 	"github.com/the-zion/matrix-core/app/message/service/internal/biz"
 	"strconv"
+	"time"
 )
 
 type messageRepo struct {
@@ -106,10 +108,199 @@ func (r *messageRepo) GetMessageNotification(ctx context.Context, uuid string, f
 	}, nil
 }
 
+func (r *messageRepo) GetMessageSystemNotification(ctx context.Context, page int32, uuid string) ([]*biz.SystemNotification, error) {
+	key := "system_notification_" + uuid
+	review, err := r.getMessageSystemNotificationFromCache(ctx, page, key)
+	if err != nil {
+		return nil, err
+	}
+
+	size := len(review)
+	if size != 0 {
+		return review, nil
+	}
+
+	review, err = r.getMessageSystemNotificationFromDB(ctx, page, uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	size = len(review)
+	if size != 0 {
+		go r.data.Recover(context.Background(), func(ctx context.Context) {
+			r.setMessageSystemNotificationToCache(key, review)
+		})()
+	}
+	return review, nil
+}
+
+func (r *messageRepo) getMessageSystemNotificationFromCache(ctx context.Context, page int32, key string) ([]*biz.SystemNotification, error) {
+	if page < 1 {
+		page = 1
+	}
+	index := int64(page - 1)
+	list, err := r.data.redisCli.LRange(ctx, key, index*20, index*20+19).Result()
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("fail to get message system notification from cache: key(%s), page(%v)", key, page))
+	}
+
+	notificationList := make([]*biz.SystemNotification, 0)
+	for _, item := range list {
+		var notification = &biz.SystemNotification{}
+		err = json.Unmarshal([]byte(item), notification)
+		if err != nil {
+			return nil, errors.Wrapf(err, fmt.Sprintf("json unmarshal error: notification(%v)", item))
+		}
+		notificationList = append(notificationList, &biz.SystemNotification{
+			Id:               notification.Id,
+			ContentId:        notification.ContentId,
+			CreatedAt:        notification.CreatedAt,
+			NotificationType: notification.NotificationType,
+			Title:            notification.Title,
+			Uid:              notification.Uid,
+			Uuid:             notification.Uuid,
+			Label:            notification.Label,
+			Result:           notification.Result,
+			Section:          notification.Section,
+			Text:             notification.Text,
+		})
+	}
+	return notificationList, nil
+}
+
+func (r *messageRepo) getMessageSystemNotificationFromDB(ctx context.Context, page int32, uuid string) ([]*biz.SystemNotification, error) {
+	if page < 1 {
+		page = 1
+	}
+	index := int(page - 1)
+	list := make([]*SystemNotification, 0)
+	err := r.data.db.WithContext(ctx).Where("uuid", uuid).Order("id desc").Offset(index * 20).Limit(20).Find(&list).Error
+	if err != nil {
+		return nil, errors.Wrapf(err, fmt.Sprintf("fail to get message system notification from db: page(%v), uuid(%s)", page, uuid))
+	}
+
+	notification := make([]*biz.SystemNotification, 0)
+	for _, item := range list {
+		notification = append(notification, &biz.SystemNotification{
+			Id:               int32(item.ID),
+			ContentId:        item.ContentId,
+			CreatedAt:        item.CreatedAt.Format("2006-01-02"),
+			NotificationType: item.NotificationType,
+			Title:            item.Title,
+			Uid:              item.Uid,
+			Uuid:             item.Uuid,
+			Label:            item.Label,
+			Result:           item.Result,
+			Section:          item.Section,
+			Text:             item.Text,
+		})
+	}
+	return notification, nil
+}
+
+func (r *messageRepo) setMessageSystemNotificationToCache(key string, notification []*biz.SystemNotification) {
+	ctx := context.Background()
+	_, err := r.data.redisCli.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+		list := make([]interface{}, 0)
+		for _, item := range notification {
+			m, err := json.Marshal(item)
+			if err != nil {
+				return errors.Wrapf(err, fmt.Sprintf("fail to marshal avatar review: notification(%v)", item))
+			}
+			list = append(list, m)
+		}
+		pipe.RPush(ctx, key, list...)
+		pipe.Expire(ctx, key, time.Hour*8)
+		return nil
+	})
+	if err != nil {
+		r.log.Errorf("fail to set message system notification to cache: notifications(%v), err(%v)", notification, err)
+	}
+}
+
 func (r *messageRepo) SetMailBoxLastTime(ctx context.Context, uuid string, time int32) error {
 	_, err := r.data.redisCli.HSet(ctx, "mailbox_last_time", uuid, time).Result()
 	if err != nil {
 		return errors.Wrapf(err, "fail to set mailbox last time: uuid(%s)", uuid)
+	}
+	return nil
+}
+
+func (r *messageRepo) RemoveMailBoxCommentCount(ctx context.Context, uuid string) error {
+	_, err := r.data.redisCli.HSet(ctx, "message_comment", uuid, 0).Result()
+	if err != nil {
+		return errors.Wrapf(err, "fail to remove mailbox comment count: uuid(%s)", uuid)
+	}
+	return nil
+}
+
+func (r *messageRepo) RemoveMailBoxSubCommentCount(ctx context.Context, uuid string) error {
+	_, err := r.data.redisCli.HSet(ctx, "message_sub_comment", uuid, 0).Result()
+	if err != nil {
+		return errors.Wrapf(err, "fail to remove mailbox sub comment count: uuid(%s)", uuid)
+	}
+	return nil
+}
+
+func (r *messageRepo) RemoveMailBoxSystemNotificationCount(ctx context.Context, uuid string) error {
+	_, err := r.data.redisCli.HSet(ctx, "message_system", uuid, 0).Result()
+	if err != nil {
+		return errors.Wrapf(err, "fail to remove mailbox system notification count: uuid(%s)", uuid)
+	}
+	return nil
+}
+
+func (r *messageRepo) AddMailBoxSystemNotification(ctx context.Context, contentId int32, notificationType string, title string, uuid string, label string, result int32, section string, text string, uid string) (*biz.SystemNotification, error) {
+	ar := &SystemNotification{
+		ContentId:        contentId,
+		NotificationType: notificationType,
+		Title:            title,
+		Uid:              uid,
+		Uuid:             uuid,
+		Label:            label,
+		Result:           result,
+		Section:          section,
+		Text:             text,
+	}
+	err := r.data.db.WithContext(ctx).Select("ContentId", "NotificationType", "Title", "Uuid", "Uid", "Label", "Result", "Section", "Text").Create(ar).Error
+	if err != nil {
+		return nil, errors.Wrapf(err, "fail to add mail box notification: notification(%v)", ar)
+	}
+	return &biz.SystemNotification{
+		Id:               int32(ar.ID),
+		CreatedAt:        time.Now().Format("2006-01-02"),
+		ContentId:        contentId,
+		NotificationType: notificationType,
+		Title:            title,
+		Uuid:             uuid,
+		Label:            label,
+		Result:           result,
+		Section:          section,
+		Text:             text,
+	}, nil
+}
+
+func (r *messageRepo) AddMailBoxSystemNotificationToCache(ctx context.Context, notification *biz.SystemNotification) error {
+	marshal, err := json.Marshal(notification)
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to set system notification to json: json.Marshal(%v)", notification))
+	}
+	var script = redis.NewScript(`
+					local key = KEYS[1]
+					local value = ARGV[1]
+					local uuid = ARGV[2]
+					local exist = redis.call("EXISTS", key)
+					if exist == 1 then
+						redis.call("LPUSH", key, value)
+					end
+					redis.call("HINCRBY", "message_system", uuid, 1)
+					return 0
+	`)
+	keys := []string{"system_notification_" + notification.Uuid}
+	values := []interface{}{marshal, notification.Uuid}
+	_, err = script.Run(ctx, r.data.redisCli, keys, values...).Result()
+	if err != nil {
+		return errors.Wrapf(err, fmt.Sprintf("fail to set system notification to json: notification(%v)", notification))
 	}
 	return nil
 }
